8f3c913431fd62192f3c8d7856da7833e68bb369
Steven Diamond <stevend2@stanford.edu>
2013-08-07 13:46:20 -0700

    floor planning finished

--- floor_packing.py
+++ floor_packing.py
@@ -1,38 +1,46 @@
 from cvxpy import *
 import pylab
+import math
 
+# Based on http://cvxopt.org/examples/book/floorplan.html
 class Box(object):
     """ A box in a floor packing problem. """
-    def __init__(self, width, height):
-        self.height = height
-        self.width = width
+    ASPECT_RATIO = 5.0
+    def __init__(self, min_area):
+        self.min_area = min_area
+        self.height = Variable()
+        self.width = Variable()
         self.x = Variable()
         self.y = Variable()
 
     @property
     def position(self):
-        return (round(self.x.value - self.width/2,2), 
-                round(self.y.value - self.height/2,2))
+        return (round(self.x.value,2), round(self.y.value,2))
+
+    @property
+    def size(self):
+        return (round(self.width.value,2), round(self.height.value,2))
 
     @property
     def left(self):
-        return self.x - self.width/2
+        return self.x
 
     @property
     def right(self):
-        return self.x + self.width/2
+        return self.x + self.width
 
     @property
     def bottom(self):
-        return self.y - self.height/2
+        return self.y
 
     @property
     def top(self):
-        return self.y + self.height/2
+        return self.y + self.height
 
 class FloorPlan(object):
     """ A minimum perimeter floor plan. """
-    MARGIN = 1
+    MARGIN = 1.0
+    ASPECT_RATIO = 5.0
     def __init__(self, boxes):
         self.boxes = boxes
         self.height = Variable()
@@ -61,12 +69,20 @@ class FloorPlan(object):
     # Compute minimum perimeter layout.
     def layout(self):
         constraints = []
-        # Enforce that boxes lie in bounding box.
         for box in self.boxes:
+            # Enforce that boxes lie in bounding box.
             constraints += [box.bottom >= FloorPlan.MARGIN, 
                             box.top + FloorPlan.MARGIN <= self.height]
             constraints += [box.left >= FloorPlan.MARGIN, 
                             box.right + FloorPlan.MARGIN <= self.width]
+            # Enforce aspect ratios.
+            constraints += [(1/box.ASPECT_RATIO)*box.height <= box.width,
+                            box.width <= box.ASPECT_RATIO*box.height]
+            # Enforce minimum area
+            constraints += [
+            geo_mean(box.width, box.height) >= math.sqrt(box.min_area)
+            ]
+
         # Enforce the relative ordering of the boxes.
         for ordering in self.horizontal_orderings:
             constraints += self._order(ordering, True)
@@ -81,10 +97,11 @@ class FloorPlan(object):
         for k in range(len(self.boxes)):
             box = self.boxes[k]
             x,y = box.position
-            pylab.fill([x, x, x + box.width, x + box.width],
-                       [y, y+box.height, y+box.height, y],
+            w,h = box.size
+            pylab.fill([x, x, x + w, x + w],
+                       [y, y+h, y+h, y],
                        facecolor = '#D0D0D0')
-            pylab.text(x+.5*box.width, y+.5*box.height, "%d" %(k+1))
+            pylab.text(x+.5*w, y+.5*h, "%d" %(k+1))
         x,y = self.size
         pylab.axis([0, x, 0, y])
         pylab.xticks([])
@@ -92,11 +109,11 @@ class FloorPlan(object):
 
         pylab.show()
 
-boxes = [Box(10,20), Box(5,8), Box(9,3), Box(40,20), Box(9,10)]
+boxes = [Box(180), Box(80), Box(80), Box(80), Box(80)]
 fp = FloorPlan(boxes)
 fp.horizontal_orderings.append( [boxes[0], boxes[2], boxes[4]] )
 fp.horizontal_orderings.append( [boxes[1], boxes[2]] )
-fp.vertical_orderings.append( [boxes[4], boxes[3]] )
+fp.horizontal_orderings.append( [boxes[3], boxes[4]] )
 fp.vertical_orderings.append( [boxes[1], boxes[0], boxes[3]] )
 fp.vertical_orderings.append( [boxes[2], boxes[3]] )
 fp.layout()
