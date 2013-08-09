215bbf246e1c1ba264954d00918f8eeef3c61edf
Steven Diamond <stevend2@stanford.edu>
2013-08-09 15:26:15 -0700

    finished max_card for mixed integer programming. Unbelievable

---
+++ examples/mixed_integer/__init__.py
@@ -0,0 +1,2 @@
+from variable import Variable
+from problem import Problem
--- examples/mixed_integer/variable.py
+++ examples/mixed_integer/variable.py
@@ -5,11 +5,10 @@ class Variable(cvxpy.Variable):
     """ A variable with integer constraints """
     def __init__(self, *args, **kwargs):
         super(Variable, self).__init__(*args, **kwargs)
-        self.noncvx = False
-        self.z = Parameter(*self.size)
-        self.z.value = cvxopt.matrix(0, self.size)
-        self.u = Parameter(*self.size)
-        self.u.value = cvxopt.matrix(0, self.size)
+        self.z = cvxpy.Parameter(*self.size)
+        self.z.value = cvxopt.matrix(0, self.size, tc='d')
+        self.u = cvxpy.Parameter(*self.size)
+        self.u.value = cvxopt.matrix(0, self.size, tc='d')
 
     # Sets the maximum cardinality.
     def max_card(self, card):
@@ -21,22 +20,32 @@ class Variable(cvxpy.Variable):
         return sum(cvxpy.square(self - self.z + self.u))
 
     # Projection step in ADMM
+    # All values except k-largest (by magnitude) set to zero.
     def project(self):
-        # Find k-largest values.
-        ind_val = []
-        for row in self.size[0]:
-            for col in self.size[1]:
-                ind_val.append( ((row,col), 
-                    self.value[row,col] + self.u.value[row,col])
-                )
-        ind_val.sort(key=lambda tup: abs(tup[1]), reverse=True)
-        # All values set to 0 except for _max_card largest
-        # magnitudes.
-        self.z.value = cvxopt.matrix(0, self.size)
-        for i in range(self._max_card):
-            index,val = ind_val[i]
-            self.z.value[*index] = val
+        ind_val = self.sort_by_mag(self.value + self.u.value, self.size)
+        self.z.value = cvxopt.matrix(0, self.size, tc='d')
+        for index,val in ind_val[0:self._max_card]:
+            self.z.value[index[0],index[1]] = val
 
     # Update step in ADMM
     def update(self):
         self.u.value += self.value - self.z.value
+
+    # Fix the sparsity pattern by returning a constraint.
+    def fix(self):
+        ind_val = self.sort_by_mag(self.z.value, self.size)
+        constraints = []
+        for index,val in ind_val[self._max_card:]:
+            constraints.append( self[index[0],index[1]] == 0 )
+        return constraints
+
+    # Return a list of the (indices, value) for the matrix,
+    # sorted by decreasing magnitude.
+    @staticmethod
+    def sort_by_mag(matrix, size):
+        ind_val = []
+        for row in range(size[0]):
+            for col in range(size[1]):
+                ind_val.append( ((row,col), matrix[row,col]) )
+        ind_val.sort(key=lambda tup: abs(tup[1]), reverse=True)
+        return ind_val
