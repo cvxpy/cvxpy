5f4f4565cfddd7e5293fd79a77a122b994aae3b5
Eric Chu <eytchu@gmail.com>
2013-08-09 21:34:09 -0700

    added branch and bound example

---
+++ examples/integer_ls.py
@@ -0,0 +1,17 @@
+from cvxpy import *
+from ncvx.boolean import Boolean
+import ncvx.branch_and_bound
+import cvxopt
+
+x = Boolean(3, name='x')
+A = cvxopt.matrix([1,2,3,4,5,6,7,8,9], (3,3), tc='d')
+z = cvxopt.matrix([3, 7, 9])
+
+p = Problem(Minimize(sum(square(A*x - z)))).branch_and_bound()
+
+print x.value
+print p
+
+# even a simple problem like this introduces too many variables
+# y = Boolean()
+# Problem(Minimize(square(y - 0.5))).branch_and_bound()
