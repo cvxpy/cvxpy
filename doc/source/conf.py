2938adc63b5665e79a131988868ac7b2fe4282ba
Christopher Dembia <cld72@cornell.edu>
2014-02-06 12:52:37 -0800

    Use a cleaner sphinx theme.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -103,7 +103,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+on_readthedocs = os.environ.get('READTHEDOCS', None) == 'True'
+if on_readthedocs:
+    html_theme = 'default'
+else:
+    html_theme = 'f6'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -111,7 +115,7 @@ html_theme = 'default'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['../themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
