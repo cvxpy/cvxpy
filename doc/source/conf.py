4d3f7338b4e1ee71e50f88d2a0a0e1e25cfe9fa1
Steven Diamond <stevend2@stanford.edu>
2014-02-09 20:56:19 -0800

    new try with rtd

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,29 +14,12 @@
 import sys, os
 
 # Mocking out dependencies.
-
-class Mock(object):
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def __call__(self, *args, **kwargs):
-        return Mock()
-
-    @classmethod
-    def __getattr__(cls, name):
-        if name in ('__file__', '__path__'):
-            return '/dev/null'
-        elif name[0] == name[0].upper():
-            mockType = type(name, (), {})
-            mockType.__module__ = __name__
-            return mockType
-        else:
-            return Mock()
+import mock
 
 MOCK_MODULES = ['numpy', 'scipy', 'scipy.sparse',
                 'cvxopt', 'cvxopt.solvers', 'ecos']
 for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = Mock()
+	sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
