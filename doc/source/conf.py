f56e76bde415544282dd0cebf2f382b1eaeab0f5
Steven Diamond <stevend2@stanford.edu>
2014-02-09 21:05:25 -0800

    added requirements.txt for rtd

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,14 +13,6 @@
 
 import sys, os
 
-# Mocking out dependencies.
-import mock
-
-MOCK_MODULES = ['numpy', 'scipy', 'scipy.sparse',
-                'cvxopt', 'cvxopt.solvers', 'ecos']
-for mod_name in MOCK_MODULES:
-	sys.modules[mod_name] = mock.Mock()
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
