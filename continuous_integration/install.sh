b3ae4231699f47aeb73d044ebc6a86301782b4de
Steven Diamond <stevend2@stanford.edu>
2014-05-09 19:59:30 -0700

    different approach to atlas

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -26,7 +26,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
 
     # Configure the conda environment and put it in the path using the
     # provided versions
-    conda create -n testenv --yes python=$PYTHON_VERSION atlas pip nose \
+    conda create -n testenv --yes python=$PYTHON_VERSION pip nose \
         numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION cvxopt=$CVXOPT_VERSION
     source activate testenv
 
