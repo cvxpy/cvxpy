2bd4f86884a9cc717d959dfcf01dc1df7f3c381d
Steven Diamond <stevend2@stanford.edu>
2014-05-13 11:40:35 -0700

    was installing cvxpy to system instead of conda

--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -9,7 +9,7 @@ set -e
 python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
-sudo python setup.py install
+python setup.py install
 
 if [[ "$COVERAGE" == "true" ]]; then
     export WITH_COVERAGE="--with-coverage"
