f581a40fe1e11cea07f229b9ffbfbd70c42a1e06
Steven Diamond <stevend2@stanford.edu>
2014-08-11 17:11:32 -0700

    working on consensus admm example

--- examples/consensus_admm.py
+++ examples/consensus_admm.py
@@ -19,56 +19,33 @@ from multiprocessing import Pool
 
 def prox(args):
     f, v = args
-    f += (rho/2)*sum_squares(x - 1000)
-    #Problem(Minimize(f)).solve()
-    p = Problem(Minimize(f))
-    p.solve()
-    if p.status is not OPTIMAL:
-        print f
-        print v
-        p.solve(verbose=True)
+    f += (rho/2)*sum_squares(x - v)
+    Problem(Minimize(f)).solve()
     return x.value
 
 x = Variable(n)
-prox_arg = Parameter(n)
 gamma = 1.0
-rho = 1.0
+rho = 5.0
 
 # Initialize x, z, u.
 funcs = [sum_squares(A*x - b),
          gamma*norm(x, 1)]
 ui = [np.zeros((n, 1)) for func in funcs]
-z = np.zeros((n, 1))
+xbar = np.zeros((n, 1))
 pool = Pool(2)
 for i in range(200):
     # x update.
-    prox_args = [-z + u for u in ui]
+    prox_args = [xbar - u for u in ui]
     xi = map(prox, zip(funcs, prox_args))
-    # z update.
-    xi_ui = [x_ + u for x_, u in zip(xi, ui)]
-    z = sum(xi_ui)/len(xi_ui)
+    xbar = sum(xi)/len(xi)
     # u update.
-    ui = [u + x_ - z for x_, u in zip(xi, ui)]
+    ui = [u + x_ - xbar for x_, u in zip(xi, ui)]
 
 obj = sum_squares(A*x - b) + gamma*norm(x, 1)
 prob = Problem(Minimize(obj))
 result = prob.solve()
 # print x.value
-# print z
-print "ADMM best", (sum_squares(A*z - b) + gamma*norm(z, 1)).value
+# print xbar
+print "ADMM best", (sum_squares(A*xbar - b) + gamma*norm(xbar, 1)).value
 print "ECOS best", obj.value
 # Boolean least squares with prox.
-
-import numpy as np
-from cvxpy import *
-m = 4
-n = 2
-np.random.seed(1)
-A = np.random.randn(m, n)
-b = np.random.randn(m)
-x = Variable(n)
-obj = sum_squares(A*x - b) + sum_squares(x - 1000)
-prob = Problem(Minimize(obj))
-prob.solve(verbose=True)
-print prob.status
-c, G, h, dims, A, b = prob.get_problem_data(ECOS)
