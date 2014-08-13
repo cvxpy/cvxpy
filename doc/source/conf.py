f6a74b1449bc6b63a4ad39e5b1b3e9a97122606c
Steven Diamond <stevend2@stanford.edu>
2014-08-12 20:28:35 -0700

    Bump version: 0.2.9 → 0.2.10

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # To import sphinx extensions we've put in the repository:
 sys.path.insert(0, os.path.abspath('../sphinxext'))
 
-__version__ = "0.2.9"
+__version__ = "0.2.10"
 
 # -- General configuration -----------------------------------------------------
 
