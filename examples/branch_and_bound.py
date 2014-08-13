f694224570791b1a914ac79f8c4fdba034100213
Steven Diamond <stevend2@stanford.edu>
2014-08-12 19:35:28 -0700

    shortened branch and bound

--- examples/branch_and_bound.py
+++ examples/branch_and_bound.py
@@ -7,7 +7,7 @@ subject to x in {0, 1}^n
 
 from cvxpy import *
 import numpy as np
-from Queue import PriorityQueue
+from heapq import *
 
 # Problem data.
 m = 40
@@ -25,30 +25,26 @@ prob = Problem(Minimize(f(x)),
                [L <= x, x <= U])
 
 visited = 0
-best_upper = best_lower = np.inf
+best_upper = np.inf
 best_x = 0
-nodes = PriorityQueue()
-nodes.put((np.inf, 0, np.zeros(n), np.ones(n), 0))
-while not nodes.empty():
+nodes = []
+heappush(nodes, (np.inf, 0, np.zeros(n), np.ones(n), 0))
+while nodes:
     visited += 1
     # Evaluate the node with the lowest lower bound.
-    _, _, L_val, U_val, next_split = nodes.get()
+    _, _, L_val, U_val, idx = heappop(nodes)
     L.value = L_val
     U.value = U_val
-    new_lower = prob.solve()
-    new_upper = f(np.round(x.value)).value
+    lower = prob.solve()
     # Update the best upper and lower bound.
-    best_lower = min(best_lower, new_lower)
-    best_upper = min(best_upper, new_upper)
-    if new_upper == best_upper:
+    best_upper = min(best_upper, f(np.round(x.value)).value)
+    if f(np.round(x.value)).value == best_upper:
         best_x = np.round(x.value)
     # Add new nodes if not at a leaf and the branch cannot be pruned.
-    if next_split < n and new_lower < best_upper:
+    if idx < n and lower < best_upper:
         for i in [0, 1]:
-            next_L = L_val.copy()
-            next_U = U_val.copy()
-            next_L[next_split] = next_U[next_split] = i
-            nodes.put((new_lower, i, next_L, next_U, next_split + 1))
+            L_val[idx] = U_val[idx] = i
+            heappush(nodes, (lower, i, L_val.copy(), U_val.copy(), idx + 1))
 
 print("Nodes visited: %s out of %s" % (visited, 2**(n+1)-1))
 print("Optimal solution:", best_upper)
