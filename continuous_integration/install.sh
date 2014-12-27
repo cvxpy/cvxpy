7e239640d1805b7fcacbaf738efed93c99a0c8ba
Steven Diamond <stevend2@stanford.edu>
2014-12-26 21:05:59 -0500

    playing with conda

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -23,7 +23,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
     wget http://repo.continuum.io/miniconda/Miniconda-3.7.3-Linux-x86_64.sh \
         -O miniconda.sh
     chmod +x miniconda.sh && ./miniconda.sh -b
-    #export PATH=/home/travis/anaconda/bin:$PATH
+    export PATH=/home/travis/miniconda/bin:$PATH
     conda update --yes conda
 
     # Configure the conda environment and put it in the path using the
