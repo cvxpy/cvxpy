b8c5878aa08eedfaf9044ce12f1da8ff2f4bfbda
Steven Diamond <stevend2@stanford.edu>
2015-01-03 15:22:58 -0800

    got 1D convolution working

--- examples/expr_trees/1D_convolution.py
+++ examples/expr_trees/1D_convolution.py
@@ -11,21 +11,22 @@ def gauss(n=11,sigma=1, scale=1, min_val=1):
 
 np.random.seed(5)
 random.seed(5)
-n = 1000
-DENSITY = 6.0/n
+n = 100000
+NUM_SPIKES = 6.0
+DENSITY = NUM_SPIKES/n
 x = Variable(n)
 # Create sparse signal.
 HEIGHT = n/10
 true_x = np.zeros((n,1))
 nnz = 0
 for i in range(n):
-    if random.random() < DENSITY:
+    if random.random() < DENSITY and NUM_SPIKES > nnz:
         true_x[i] = random.uniform(0, HEIGHT)
         nnz += 1
 
 # Gaussian kernel.
 m = n+1
-kernel = gauss(m, m/10, 100, 0.001)
+kernel = gauss(m, m/10, 1, 1e-6)
 #kernel = np.sinc(np.linspace(-m/100, m/100, m))
 
 # Noisy signal.
@@ -37,9 +38,10 @@ print("SNR", norm(signal,2).value/norm(noise,2).value)
 noisy_signal = signal + noise
 
 gamma = Parameter(sign="positive")
+gamma.value = 0
 fit = norm(conv(kernel, x) - noisy_signal, 2)
 constraints = [x >= 0]
-prob = Problem(Minimize(fit),
+prob = Problem(Minimize(fit + gamma*sum_entries(x)),
                constraints)
 result = prob.solve(solver=ECOS, verbose=True)
 # result = prob.solve(solver=SCS,
@@ -51,22 +53,33 @@ result = prob.solve(solver=ECOS, verbose=True)
 # result = prob.solve(solver=SCS_MAT_FREE,
 #                     verbose=True,
 #                     max_iters=2000,
-#                     equil_steps=1,
-#                     eps=1e-4,
+#                     equil_steps=2,
+#                     eps=1e-3,
 #                     cg_rate=2)
 print("recovered x fit", fit.value)
 
+print("nnz =", np.sum(x.value >= 1))
+print("max =", np.max(np.max(x.value)))
 # Plot result and fit.
 # Assumes convolution kernel is centered around m/2.
 t = range(n+m-1)
 import matplotlib.pyplot as plt
 plt.figure()
-plt.subplot(2, 1, 1)
+ax1 = plt.subplot(2, 1, 1)
 true_x_padded = np.vstack([np.zeros((m/2,1)), true_x, np.zeros((m/2,1))])
 x_padded = np.vstack([np.zeros((m/2,1)), x.value[:,0], np.zeros((m/2,1))])
-plt.plot(t, true_x_padded, label="true x")
-plt.plot(t, x_padded, label="recovered x", color="red")
-plt.legend(loc='upper right')
+lns1 = ax1.plot(t, true_x_padded, label="true x")
+ax1.set_ylabel('true x')
+ax2 = ax1.twinx()
+lns2 = ax2.plot(t, x_padded, label="recovered x", color="red")
+ax2.set_ylabel('recovered x')
+ax2.set_ylim([0, np.max(x_padded)])
+
+# added these three lines
+lns = lns1+lns2
+labs = [l.get_label() for l in lns]
+ax1.legend(lns, labs, loc='upper right')
+
 plt.subplot(2, 1, 2)
 plt.plot(t, np.asarray(noisy_signal.value[:, 0]), label="signal")
 plt.legend(loc='upper right')
