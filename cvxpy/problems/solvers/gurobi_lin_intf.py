e58b969f597d455daa1a5854eb348389e85ee6c1
macklin <macklin@server>
2015-03-19 16:31:09 -0700

    Better warm start capabilities + tests for GUROBI_LIN.

--- cvxpy/problems/solvers/gurobi_lin_intf.py
+++ cvxpy/problems/solvers/gurobi_lin_intf.py
@@ -54,6 +54,7 @@ class GUROBI_LIN(Solver):
     CUSTOM_OPTS = [
                   "update_eq_constrs",
                   "update_ineq_constrs",
+                  "update_objective",
                   ]
 
     def name(self):
@@ -131,6 +132,7 @@ class GUROBI_LIN(Solver):
 
         update_eq_constrs = solver_opts.get("update_eq_constrs", True)
         update_ineq_constrs = solver_opts.get("update_ineq_constrs", True)
+        update_objective = solver_opts.get("update_objective", True)
 
         solver_cache = cached_data[self.name()]
 
@@ -139,16 +141,92 @@ class GUROBI_LIN(Solver):
             variables = solver_cache.prev_result["variables"]
             eq_constrs = solver_cache.prev_result["eq_constrs"]
             ineq_constrs = solver_cache.prev_result["ineq_constrs"]
+            c_prev = solver_cache.prev_result["c"]
+            A_prev = solver_cache.prev_result["A"]
+            b_prev = solver_cache.prev_result["b"]
+            G_prev = solver_cache.prev_result["G"]
+            h_prev = solver_cache.prev_result["h"]
+
+            if update_objective:
+                c_diff = c - c_prev
+
+                I_unique = list(set(np.where(c_diff)[0]))
+
+                for i in I_unique:
+                    variables[i].Obj = c[i]
+            else:
+                # Stay consistent with Gurobi's representation of the problem
+                c = c_prev
 
             if update_eq_constrs:
-                for constr in eq_constrs:
-                    model.remove(constr)
+                A_diff = dok_matrix(A - A_prev)
+                b_diff = b - b_prev
+
+                # Figure out which rows of A and elements of b have changed
+                try:
+                    I, _ = zip(*[x for x in A_diff.iterkeys()])
+                except ValueError:
+                    I = []
+                I_unique = list(set(I) | set(np.where(b_diff)[0]))
+
+                A_nonzero_locs = gurobipy.tuplelist([x for x in A.iterkeys()])
+
+                # Update locations which have changed
+                for i in I_unique:
+
+                    # Remove old constraint if it exists
+                    if eq_constrs[i] != None:
+                        model.remove(eq_constrs[i])
+                        eq_constrs[i] = None
+
+                    # Add new constraint
+                    if len(A_nonzero_locs.select(i, "*")):
+                        expr_list = []
+                        for loc in A_nonzero_locs.select(i, "*"):
+                            expr_list.append((A[loc], variables[loc[1]]))
+                        expr = gurobipy.LinExpr(expr_list)
+                        eq_constrs[i] = model.addConstr(expr, gurobipy.GRB.EQUAL, b[i])
+
+                model.update()
+            else:
+                # Stay consistent with Gurobi's representation of the problem
+                A = A_prev
+                b = b_prev
 
             if update_ineq_constrs:
-                for constr in ineq_constrs:
-                    model.remove(constr)
+                G_diff = dok_matrix(G - G_prev)
+                h_diff = h - h_prev
 
-            model.update()
+                # Figure out which rows of G and elements of h have changed
+                try:
+                    I, _ = zip(*[x for x in G_diff.iterkeys()])
+                except ValueError:
+                    I = []
+                I_unique = list(set(I) | set(np.where(h_diff)[0]))
+
+                G_nonzero_locs = gurobipy.tuplelist([x for x in G.iterkeys()])
+
+                # Update locations which have changed
+                for i in I_unique:
+
+                    # Remove old constraint if it exists
+                    if ineq_constrs[i] != None:
+                        model.remove(ineq_constrs[i])
+                        ineq_constrs[i] = None
+
+                    # Add new constraint
+                    if len(G_nonzero_locs.select(i, "*")):
+                        expr_list = []
+                        for loc in G_nonzero_locs.select(i, "*"):
+                            expr_list.append((G[loc], variables[loc[1]]))
+                        expr = gurobipy.LinExpr(expr_list)
+                        ineq_constrs[i] = model.addConstr(expr, gurobipy.GRB.LESS_EQUAL, h[i])
+
+                model.update()
+            else:
+                # Stay consistent with Gurobi's representation of the problem
+                G = G_prev
+                h = h_prev
 
         else:
             model = gurobipy.Model()
@@ -159,6 +237,42 @@ class GUROBI_LIN(Solver):
                     lb = -gurobipy.GRB.INFINITY,    # Gurobi's default LB is 0 (WHY???)
                     ub =  gurobipy.GRB.INFINITY)
                 for i in xrange(n)]
+            model.update()
+
+            eq_constrs = [None] * b.shape[0]
+            if A.nnz > 0 or b.any:
+                try:
+                    I, _ = zip(*[x for x in A.iterkeys()])
+                except ValueError:
+                    I = []
+                eq_constrs_nonzero_idxs = list(set(I) | set(np.where(b)[0]))
+                A_nonzero_locs = gurobipy.tuplelist([x for x in A.iterkeys()])
+
+                for i in eq_constrs_nonzero_idxs:
+                    expr_list = []
+                    for loc in A_nonzero_locs.select(i, "*"):
+                        expr_list.append((A[loc], variables[loc[1]]))
+                    expr = gurobipy.LinExpr(expr_list)
+
+                    eq_constrs[i] = model.addConstr(expr, gurobipy.GRB.EQUAL, b[i])
+
+            ineq_constrs = [None] * h.shape[0]
+            if G.nnz > 0 or h.any:
+                try:
+                    I, _ = zip(*[x for x in G.iterkeys()])
+                except ValueError:
+                    I = []
+                ineq_constrs_nonzero_idxs = list(set(I) | set(np.where(h)[0]))
+                G_nonzero_locs = gurobipy.tuplelist([x for x in G.iterkeys()])
+
+                for i in ineq_constrs_nonzero_idxs:
+                    expr_list = []
+                    for loc in G_nonzero_locs.select(i, "*"):
+                        expr_list.append((G[loc], variables[loc[1]]))
+                    expr = gurobipy.LinExpr(expr_list)
+
+                    ineq_constrs[i] = model.addConstr(expr, gurobipy.GRB.LESS_EQUAL, h[i])
+
             model.update()
 
         # Set verbosity and other parameters
@@ -172,49 +286,27 @@ class GUROBI_LIN(Solver):
                 continue
             model.setParam(key, value)
 
-        if update_eq_constrs:
-            eq_constrs = []
-            if A.nnz > 0:
-                I, J = zip(*[x for x in A.iterkeys()])
-                I_unique = list(set(I))
-                A_nonzero_locs = gurobipy.tuplelist([x for x in A.iterkeys()])
-
-                for i in I_unique:
-                    expr_list = []
-                    for loc in A_nonzero_locs.select(i, "*"):
-                        expr_list.append((A[loc], variables[loc[1]]))
-                    expr = gurobipy.LinExpr(expr_list)
-                    eq_constrs.append(model.addConstr(expr, gurobipy.GRB.EQUAL, b[i]))
-
-        if update_ineq_constrs:
-            ineq_constrs = []
-            if G.nnz > 0:
-                I, J = zip(*[x for x in G.iterkeys()])
-                I_unique = list(set(I))
-                G_nonzero_locs = gurobipy.tuplelist([x for x in G.iterkeys()])
-
-                for i in I_unique:
-                    expr_list = []
-                    for loc in G_nonzero_locs.select(i, "*"):
-                        expr_list.append((G[loc], variables[loc[1]]))
-                    expr = gurobipy.LinExpr(expr_list)
-
-                    ineq_constrs.append(model.addConstr(expr, gurobipy.GRB.LESS_EQUAL, h[i]))
-
-            model.update()
 
         try:
             model.optimize()
+
             results_dict = {
                 "model": model,
                 "variables": variables,
                 "eq_constrs": eq_constrs,
                 "ineq_constrs": ineq_constrs,
+                "c": c,
+                "A": A,
+                "b": b,
+                "G": G,
+                "h": h,
                 "status": self.STATUS_MAP.get(model.Status, "unknown"),
                 "primal objective": model.ObjVal,
                 "x": np.array([v.X for v in variables]),
-                "y": np.array([lc.Pi for lc in eq_constrs]),
-                "z": np.array([lc.Pi for lc in ineq_constrs]),
+                # Not sure why we need to negate the following,
+                # but need to in order to be consistent with other solvers.
+                "y": -np.array([lc.Pi if lc != None else 0 for lc in eq_constrs]),
+                "z": -np.array([lc.Pi if lc != None else 0 for lc in ineq_constrs]),
                 }
         except gurobipy.GurobiError:
             results_dict = {
@@ -246,13 +338,19 @@ class GUROBI_LIN(Solver):
         dict
             The solver output in standard form.
         """
-        solver_cache = cached_data[self.name()]
-        solver_cache.prev_result = {
-            "model": results_dict["model"],
-            "variables": results_dict["variables"],
-            "eq_constrs": results_dict["eq_constrs"],
-            "ineq_constrs": results_dict["ineq_constrs"],
-            }
+        if results_dict["status"] != s.SOLVER_ERROR:
+            solver_cache = cached_data[self.name()]
+            solver_cache.prev_result = {
+                "model": results_dict["model"],
+                "variables": results_dict["variables"],
+                "eq_constrs": results_dict["eq_constrs"],
+                "ineq_constrs": results_dict["ineq_constrs"],
+                "c": results_dict["c"],
+                "A": results_dict["A"],
+                "b": results_dict["b"],
+                "G": results_dict["G"],
+                "h": results_dict["h"],
+                }
         new_results = {}
         new_results[s.STATUS] = results_dict['status']
         if new_results[s.STATUS] in s.SOLUTION_PRESENT:
