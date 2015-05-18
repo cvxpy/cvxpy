b589c5aae777958f1c80e907a55affccd484614c
Steven Diamond <stevend2@stanford.edu>
2015-05-18 00:13:53 -0700

    Bump version: 0.2.20 → 0.2.21

--- cvxpy/__init__.py
+++ cvxpy/__init__.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with CVXPY.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = "0.2.20"
+__version__ = "0.2.21"
 from cvxpy.atoms import *
 from cvxpy.expressions.variables import Variable, Semidef, Bool, Int
 from cvxpy.expressions.constants import Parameter, CallbackParam, Constant
