0fe706f83dddf94969e3b0c3d767e5e00bfd502e
Steven Diamond <stevend2@stanford.edu>
2016-03-14 16:21:19 -0700

    try with gcc

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -28,7 +28,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
 
     # Configure the conda environment and put it in the path using the
     # provided versions
-    conda create -n testenv --yes python=$PYTHON_VERSION pip nose \
+    conda create -n testenv --yes python=$PYTHON_VERSION pip nose gcc \
         numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION cvxopt
     source activate testenv
 
