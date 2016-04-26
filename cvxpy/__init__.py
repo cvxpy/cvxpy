918af76b93aea0e5fa7d6a57b317170042610c14
Steven Diamond <stevend2@stanford.edu>
2016-04-26 12:24:29 -0700

    Bump version: 0.3.9 → 0.4.0

--- cvxpy/__init__.py
+++ cvxpy/__init__.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with CVXPY.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = "0.3.9"
+__version__ = "0.4.0"
 from cvxpy.atoms import *
 from cvxpy.expressions.variables import (Variable, Semidef, Symmetric, Bool,
 Int, NonNegative)
