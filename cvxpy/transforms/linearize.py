4b1ecc273654993c12c3efea60e03c2ad6bb6a3a
Steven Diamond <stevend2@stanford.edu>
2016-04-26 12:30:47 -0700

    make linearize work on numeric constants

--- cvxpy/transforms/linearize.py
+++ cvxpy/transforms/linearize.py
@@ -34,6 +34,7 @@ def linearize(expr):
     Returns:
         An affine expression or None.
     """
+    expr = Constant.cast_to_const(expr)
     if expr.is_affine():
         return expr
     else:
