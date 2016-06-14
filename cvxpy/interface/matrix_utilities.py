Fixed bug with gradients with constant terms.

cvxpy/interface/matrix_utilities.py
@@ -108,8 +108,20 @@ def scalar_value(constant):
         raise TypeError("%s is not a valid type for a Constant value." % type(constant))
 
 # Return the collective sign of the matrix entries.
-def sign(constant):
+def sign(constant, tol=1e-5):
     """Return (is positive, is negative).
+
+    Parameters
+    ----------
+    constant : numeric type
+        The numeric value to evaluate the sign of.
+    tol : float, optional
+        The largest (smallest) value considered positive (negative).
+
+    Returns
+    -------
+    tuple
+        The sign of the constant.
     """
     if isinstance(constant, numbers.Number):
         max_val = constant
@@ -124,7 +136,7 @@ def sign(constant):
         mat = INTERFACES[np.ndarray].const_to_matrix(constant)
         max_val = mat.max()
         min_val = mat.min()
-    return (min_val >= 0, max_val <= 0)
+    return (min_val >= -tol, max_val <= tol)
 
 # Get the value at the given index.
 def index(constant, key):

cvxpy/tests/test_expressions.py
@@ -19,7 +19,7 @@ along with CVXPY.  If not, see <http://www.gnu.org/licenses/>.
 
 from cvxpy.atoms.affine.add_expr import AddExpression
 from cvxpy.expressions.expression import *
-from cvxpy.expressions.variables import Variable, Semidef
+from cvxpy.expressions.variables import Variable, Semidef, NonNegative
 from cvxpy.expressions.constants import Constant
 from cvxpy.expressions.constants import Parameter
 from cvxpy import Problem, Minimize
@@ -110,6 +110,16 @@ class TestExpressions(BaseTest):
         A = Variable(3, 2)
         A.value = np.ones((3, 2))
         self.assertItemsAlmostEqual(A.value, np.ones((3, 2)))
+
+        # Test assigning negative val to nonnegative variable.
+        x = NonNegative()
+        with self.assertRaises(Exception) as cm:
+            x.value = -2
+        self.assertEqual(str(cm.exception), "Invalid sign for NonNegative value.")
+
+        # Small negative values are rounded to 0.
+        x.value = -1e-8
+        self.assertEqual(x.value, 0)
 
     # Test tranposing variables.
     def test_transpose_variable(self):

cvxpy/utilities/grad.py
@@ -22,7 +22,7 @@ along with CVXPY.  If not, see <http://www.gnu.org/licenses/>.
 import scipy.sparse as sp
 
 def constant_grad(expr):
-    """Returns the gradient of a constant expression.
+    """Returns the gradient of constant terms in an expression.
 
     Matrix expressions are vectorized, so the gradient is a matrix.
 