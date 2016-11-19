1b78d139474fab0af99beea76e460d99f6434a8e
Steven Diamond <stevend2@stanford.edu>
2016-11-19 14:37:22 -0800

    added solution object

---
+++ cvxpy/reductions/solution.py
@@ -0,0 +1,21 @@
+# Status codes.
+OPTIMAL = "OPTIMAL"
+INFEASIBLE = "INFEASIBLE"
+UNBOUNDED = "UNBOUNDED"
+USER_LIMIT = "USER_LIMIT"
+ERROR = "ERROR"
+
+class Solution(object):
+    """A solution object.
+
+    Attributes:
+        status: status code
+        opt_val: float
+        primal_vars: dict of id to NumPy ndarray
+        dual_vars: dict of id to NumPy ndarray
+    """
+    def __init__(self, status, opt_val, primal_vars, dual_vars):
+        self.status = status
+        self.opt_val = opt_val
+        self.primal_vars = primal_vars
+        self.dual_vars = dual_vars
