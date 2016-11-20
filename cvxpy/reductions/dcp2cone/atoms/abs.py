bcccca48cc7d45750fe243c8df9097a666f38d54
Nicholas Moehle <moehle@stanford.edu>
2016-11-19 16:56:45 -0800

    some work on dcp2cone

---
+++ cvxpy/reductions/dcp2cone/atoms/abs.py
@@ -0,0 +1,8 @@
+from cvxpy.expressions.variables.variable import Variable
+
+
+def abs_canon(expr, args):
+    x = args
+    t = Variable(x.size)
+    constraints = [t >= x, t >= -x]
+    return t, constraints
