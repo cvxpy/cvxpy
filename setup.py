c541a32974f4c34e4a339d64d45a6853935aa435
Steven Diamond <stevend2@stanford.edu>
2017-02-04 18:18:56 -0800

    changed license to Apache 2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
               'cvxpy.utilities'],
     package_dir={'cvxpy': 'cvxpy'},
     url='http://github.com/cvxgrp/cvxpy/',
-    license='GPLv3',
+    license='Apache v2.0',
     zip_safe=False,
     description='A domain-specific language for modeling convex optimization problems in Python.',
     install_requires=["ecos >= 2",
