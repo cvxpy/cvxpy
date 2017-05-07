82cc77d34221451eb41e19a86b04b1965db7b126
Akshay Agrawal <akshaykagrawal7@gmail.com>
2017-05-06 17:11:05 -0700

    another fix to calling SOC in quad_over_lin_canon

--- cvxpy/reductions/dcp2cone/atom_canonicalizers/quad_over_lin_canon.py
+++ cvxpy/reductions/dcp2cone/atom_canonicalizers/quad_over_lin_canon.py
@@ -1,9 +1,11 @@
+from cvxpy.atoms.affine.reshape import reshape
+from cvxpy.atoms.affine.vstack import vstack
 from cvxpy.constraints.second_order import SOC
 from cvxpy.expressions.variables.variable import Variable
 
 
 def quad_over_lin_canon(expr, args):
-    # quad_over_lin := <x, x> / y
+    # quad_over_lin := sum_{ij} X^2_{ij} / y
     x = args[0]
     y = args[1]
     shape = expr.shape
@@ -12,5 +14,9 @@ def quad_over_lin_canon(expr, args):
     # (y+t, y-t, 2*x) must lie in the second-order cone,
     # where y+t is the scalar part of the second-order
     # cone constraint.
-    constraints = [SOC(y+t, [y-t, 2*x]), y >= 0]
+    constraints = [SOC(t=y+t,
+                    X=vstack(
+                        y-t,
+                        reshape(2*x, (x.shape[0] * x.shape[1], 1))),
+                    axis=0), y >= 0]
     return t, constraints
