b3e0968196b7ee5dac7c7aac92f8b24794f434d7
Akshay Agrawal <akshaykagrawal7@gmail.com>
2017-07-31 10:37:00 -0700

    Add norm1_canon, norm_inf_canon, pnorm_canon

---
+++ cvxpy/reductions/eliminate_pwl/atom_canonicalizers/norm_inf_canon.py
@@ -0,0 +1,31 @@
+"""
+Copyright 2013 Steven Diamond
+
+This file is part of CVXPY.
+
+CVXPY is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CVXPY is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CVXPY.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from cvxpy.atoms.affine.promote import promote
+from cvxpy.expressions.variables.variable import Variable
+
+
+def norm_inf_canon(expr, args):
+    x = args[0]
+    axis = expr.axis
+    shape = expr.shape
+    t = Variable(*shape)
+
+    promoted_t = promote(t, x.shape)
+    return t, [x <= promoted_t, x + promoted_t >= 0]
