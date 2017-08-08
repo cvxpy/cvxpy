dae89181998723cc0e30d09cb00198f415999dfc
Akshay Agrawal <akshaykagrawal7@gmail.com>
2017-08-08 14:07:43 -0700

    remove extra compile args from cvxcanon setup

--- cvxpy/CVXcanon/setup.py
+++ cvxpy/CVXcanon/setup.py
@@ -14,7 +14,6 @@ canon = Extension(
     sources=['src/CVXcanon.cpp', 'src/LinOpOperations.cpp',
              'src/python/CVXcanon_wrap.cpp', 'src/Utils.cpp'],
     include_dirs=['src/', 'src/python/', 'include/Eigen', get_numpy_include()],
-    extra_compile_args = ["-O0", "-std=c++0x"], 
 )
 
 
