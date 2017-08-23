4a3daf0ff5eba428e52276bf256b3f37791c2b6a
Steven Diamond <stevend2@stanford.edu>
2017-08-23 12:29:27 -0400

    added ecos intf back for moment

---
+++ cvxpy/problems/solvers/ecos_intf.py
@@ -0,0 +1,159 @@
+"""
+Copyright 2017 Steven Diamond
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import cvxpy.interface as intf
+import cvxpy.settings as s
+from cvxpy.problems.solvers.solver import Solver
+
+
+class ECOS(Solver):
+    """An interface for the ECOS solver.
+    """
+
+    # Solver capabilities.
+    LP_CAPABLE = True
+    SOCP_CAPABLE = True
+    SDP_CAPABLE = False
+    EXP_CAPABLE = True
+    MIP_CAPABLE = False
+
+    # EXITCODES from ECOS
+    # ECOS_OPTIMAL  (0)   Problem solved to optimality
+    # ECOS_PINF     (1)   Found certificate of primal infeasibility
+    # ECOS_DINF     (2)   Found certificate of dual infeasibility
+    # ECOS_INACC_OFFSET (10)  Offset exitflag at inaccurate results
+    # ECOS_MAXIT    (-1)  Maximum number of iterations reached
+    # ECOS_NUMERICS (-2)  Search direction unreliable
+    # ECOS_OUTCONE  (-3)  s or z got outside the cone, numerics?
+    # ECOS_SIGINT   (-4)  solver interrupted by a signal/ctrl-c
+    # ECOS_FATAL    (-7)  Unknown problem in solver
+
+    # Map of ECOS status to CVXPY status.
+    STATUS_MAP = {0: s.OPTIMAL,
+                  1: s.INFEASIBLE,
+                  2: s.UNBOUNDED,
+                  10: s.OPTIMAL_INACCURATE,
+                  11: s.INFEASIBLE_INACCURATE,
+                  12: s.UNBOUNDED_INACCURATE,
+                  -1: s.SOLVER_ERROR,
+                  -2: s.SOLVER_ERROR,
+                  -3: s.SOLVER_ERROR,
+                  -4: s.SOLVER_ERROR,
+                  -7: s.SOLVER_ERROR}
+
+    def import_solver(self):
+        """Imports the solver.
+        """
+        import ecos
+        ecos  # For flake8
+
+    def name(self):
+        """The name of the solver.
+        """
+        return s.ECOS
+
+    def matrix_intf(self):
+        """The interface for matrices passed to the solver.
+        """
+        return intf.DEFAULT_SPARSE_INTF
+
+    def vec_intf(self):
+        """The interface for vectors passed to the solver.
+        """
+        return intf.DEFAULT_INTF
+
+    def split_constr(self, constr_map):
+        """Extracts the equality, inequality, and nonlinear constraints.
+
+        Parameters
+        ----------
+        constr_map : dict
+            A dict of the canonicalized constraints.
+
+        Returns
+        -------
+        tuple
+            (eq_constr, ineq_constr, nonlin_constr)
+        """
+        return (constr_map[s.EQ], constr_map[s.LEQ], [])
+
+    def solve(self, objective, constraints, cached_data,
+              warm_start, verbose, solver_opts):
+        """Returns the result of the call to the solver.
+
+        Parameters
+        ----------
+        objective : LinOp
+            The canonicalized objective.
+        constraints : list
+            The list of canonicalized cosntraints.
+        cached_data : dict
+            A map of solver name to cached problem data.
+        warm_start : bool
+            Not used.
+        verbose : bool
+            Should the solver print output?
+        solver_opts : dict
+            Additional arguments for the solver.
+
+        Returns
+        -------
+        tuple
+            (status, optimal value, primal, equality dual, inequality dual)
+        """
+        import ecos
+        data = self.get_problem_data(objective, constraints, cached_data)
+        data[s.DIMS]['e'] = data[s.DIMS][s.EXP_DIM]
+        results_dict = ecos.solve(data[s.C], data[s.G], data[s.H],
+                                  data[s.DIMS], data[s.A], data[s.B],
+                                  verbose=verbose,
+                                  **solver_opts)
+        return self.format_results(results_dict, data, cached_data)
+
+    def format_results(self, results_dict, data, cached_data):
+        """Converts the solver output into standard form.
+
+        Parameters
+        ----------
+        results_dict : dict
+            The solver output.
+        data : dict
+            Information about the problem.
+        cached_data : dict
+            A map of solver name to cached problem data.
+
+        Returns
+        -------
+        dict
+            The solver output in standard form.
+        """
+        new_results = {}
+        status = self.STATUS_MAP[results_dict['info']['exitFlag']]
+        new_results[s.STATUS] = status
+
+        # Timing data
+        new_results[s.SOLVE_TIME] = results_dict["info"]["timing"]["tsolve"]
+        new_results[s.SETUP_TIME] = results_dict["info"]["timing"]["tsetup"]
+        new_results[s.NUM_ITERS] = results_dict["info"]["iter"]
+
+        if new_results[s.STATUS] in s.SOLUTION_PRESENT:
+            primal_val = results_dict['info']['pcost']
+            new_results[s.VALUE] = primal_val + data[s.OFFSET]
+            new_results[s.PRIMAL] = results_dict['x']
+            new_results[s.EQ_DUAL] = results_dict['y']
+            new_results[s.INEQ_DUAL] = results_dict['z']
+
+        return new_results
