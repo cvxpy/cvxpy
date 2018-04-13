6992440403f68ddee47eb1a2704a885561267d3a
Riley Murray <inventor1488@gmail.com>
2018-04-13 16:03:16 -0700

    add install of lapack (via conda-forge) to travisCI builds

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -25,6 +25,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
     conda create -n testenv --yes python=$PYTHON_VERSION mkl pip nose \
               numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION
     source activate testenv
+    conda install -c conda-forge --yes lapack
     conda install -c cvxgrp --yes ecos scs multiprocess
     conda install -c anaconda --yes flake8
 
