213c95af93671f181e82691e7fb5f10d35a17a7c
Steven Diamond <stevend2@stanford.edu>
2018-05-16 15:44:11 -0700

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ canon = Extension(
 
 setup(
     name='cvxpy',
-    version='1.0.0',
+    version='1.0.1',
     author='Steven Diamond, Eric Chu, Stephen Boyd',
     author_email='stevend2@stanford.edu, echu508@stanford.edu, boyd@stanford.edu',
     ext_modules=[canon],
