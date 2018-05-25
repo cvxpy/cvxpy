a05c5a4cddfb0eeb04ebddafdb92e00ec8cdebb2
Riley Murray <inventor1488@gmail.com>
2018-05-25 10:21:34 -0700

    this should fix issue with early deletion of mosek Task objects

--- cvxpy/reductions/solvers/conic_solvers/mosek_conif.py
+++ cvxpy/reductions/solvers/conic_solvers/mosek_conif.py
@@ -250,176 +250,176 @@ class MOSEK(ConicSolver):
 
     def solve_via_data(self, data, warm_start, verbose, solver_opts, solver_cache=None):
         import mosek
-        with mosek.Env() as env:
-            with env.Task(0, 0) as task:
+        env = mosek.Env()
+        task = env.Task(0, 0)
+        # If verbose, then set default logging parameters.
+        if verbose:
+            import sys
 
-                # If verbose, then set default logging parameters.
-                if verbose:
-                    import sys
+            def streamprinter(text):
+                sys.stdout.write(text)
+                sys.stdout.flush()
+            print('\n')
+            env.set_Stream(mosek.streamtype.log, streamprinter)
+            task.set_Stream(mosek.streamtype.log, streamprinter)
+            task.putintparam(mosek.iparam.infeas_report_auto, mosek.onoffkey.on)
+            task.putintparam(mosek.iparam.log_presolve, 0)
 
-                    def streamprinter(text):
-                        sys.stdout.write(text)
-                        sys.stdout.flush()
-                    print('\n')
-                    env.set_Stream(mosek.streamtype.log, streamprinter)
-                    task.set_Stream(mosek.streamtype.log, streamprinter)
-                    task.putintparam(mosek.iparam.infeas_report_auto, mosek.onoffkey.on)
-                    task.putintparam(mosek.iparam.log_presolve, 0)
+        # Parse all user-specified parameters (override default logging
+        # parameters if applicable).
+        kwargs = sorted(solver_opts.keys())
+        if 'mosek_params' in kwargs:
+            self._handle_mosek_params(task, solver_opts['mosek_params'])
+            kwargs.remove('mosek_params')
+        if 'bfs' in kwargs:
+            kwargs.remove('bfs')
+        if kwargs:
+            raise ValueError("Invalid keyword-argument '%s'" % kwargs[0])
 
-                # Parse all user-specified parameters (override default logging
-                # parameters if applicable).
-                kwargs = sorted(solver_opts.keys())
-                if 'mosek_params' in kwargs:
-                    self._handle_mosek_params(task, solver_opts['mosek_params'])
-                    kwargs.remove('mosek_params')
-                if 'bfs' in kwargs:
-                    kwargs.remove('bfs')
-                if kwargs:
-                    raise ValueError("Invalid keyword-argument '%s'" % kwargs[0])
+        # Check if the cvxpy standard form has zero variables. If so,
+        # return a trivial solution. This is necessary because MOSEK
+        # will crash if handed a problem with zero variables.
+        if len(data[s.C]) == 0:
+            return {s.STATUS: s.OPTIMAL, s.PRIMAL: [],
+                    s.VALUE: data[s.OFFSET], s.EQ_DUAL: [], s.INEQ_DUAL: []}
 
-                # Check if the cvxpy standard form has zero variables. If so,
-                # return a trivial solution. This is necessary because MOSEK
-                # will crash if handed a problem with zero variables.
-                if len(data[s.C]) == 0:
-                    return {s.STATUS: s.OPTIMAL, s.PRIMAL: [],
-                            s.VALUE: data[s.OFFSET], s.EQ_DUAL: [], s.INEQ_DUAL: []}
+        # The following lines recover problem parameters, and define helper constants.
+        #
+        #   The problem's objective is "min c.T * z".
+        #   The problem's constraint set is "G * z <=_K h."
+        #   The rows in (G, h) are formatted in order of
+        #       (1) linear inequalities,
+        #       (2) linear equations,
+        #       (3) soc constraints,
+        #       (4) exponential cone constraints,
+        #       (5) vectorized linear matrix inequalities.
+        #   The parameter "dims" indicates the exact
+        #   dimensions of each of these cones.
+        #
+        #   MOSEK's standard form requires that we replace generalized
+        #   inequalities with slack variables and linear equations.
+        #   The parameter "n" is the size of the column-vector variable
+        #   after adding slacks for SOC and EXP constraints. To be
+        #   consistent with MOSEK documentation, subsequent comments
+        #   refer to this variable as "x".
 
-                # The following lines recover problem parameters, and define helper constants.
-                #
-                #   The problem's objective is "min c.T * z".
-                #   The problem's constraint set is "G * z <=_K h."
-                #   The rows in (G, h) are formatted in order of
-                #       (1) linear inequalities,
-                #       (2) linear equations,
-                #       (3) soc constraints,
-                #       (4) exponential cone constraints,
-                #       (5) vectorized linear matrix inequalities.
-                #   The parameter "dims" indicates the exact
-                #   dimensions of each of these cones.
-                #
-                #   MOSEK's standard form requires that we replace generalized
-                #   inequalities with slack variables and linear equations.
-                #   The parameter "n" is the size of the column-vector variable
-                #   after adding slacks for SOC and EXP constraints. To be
-                #   consistent with MOSEK documentation, subsequent comments
-                #   refer to this variable as "x".
+        c = data[s.C]
+        G, h = data[s.G], data[s.H]
+        dims = data[s.DIMS]
+        n0 = len(c)
+        n = n0 + sum(dims[s.SOC_DIM]) + sum(dims[s.EXP_DIM])
+        psd_total_dims = sum(el ** 2 for el in dims[s.PSD_DIM])
+        m = len(h)
+        num_bool = len(data[s.BOOL_IDX])
+        num_int = len(data[s.INT_IDX])
 
-                c = data[s.C]
-                G, h = data[s.G], data[s.H]
-                dims = data[s.DIMS]
-                n0 = len(c)
-                n = n0 + sum(dims[s.SOC_DIM]) + sum(dims[s.EXP_DIM])
-                psd_total_dims = sum(el ** 2 for el in dims[s.PSD_DIM])
-                m = len(h)
-                num_bool = len(data[s.BOOL_IDX])
-                num_int = len(data[s.INT_IDX])
+        # Define variables, cone constraints, and integrality constraints.
+        #
+        #   The variable "x" is a length-n block vector, with
+        #       Block 1: "z" from "G * z <=_K h",
+        #       Block 2: slacks for SOC constraints, and
+        #       Block 3: slacks for EXP cone constraints.
+        #
+        #   Once we declare x in the MOSEK model, we add the necessary
+        #   conic constraints for slack variables (Blocks 2 and 3).
+        #   The last step is to add integrality constraints.
+        #
+        #   Note that the API call for PSD variables contains the word "bar".
+        #   MOSEK documentation consistently uses "bar" as a sort of flag,
+        #   indicating that a function deals with PSD variables.
 
-                # Define variables, cone constraints, and integrality constraints.
-                #
-                #   The variable "x" is a length-n block vector, with
-                #       Block 1: "z" from "G * z <=_K h",
-                #       Block 2: slacks for SOC constraints, and
-                #       Block 3: slacks for EXP cone constraints.
-                #
-                #   Once we declare x in the MOSEK model, we add the necessary
-                #   conic constraints for slack variables (Blocks 2 and 3).
-                #   The last step is to add integrality constraints.
-                #
-                #   Note that the API call for PSD variables contains the word "bar".
-                #   MOSEK documentation consistently uses "bar" as a sort of flag,
-                #   indicating that a function deals with PSD variables.
+        task.appendvars(n)
+        task.putvarboundlist(np.arange(n, dtype=int),
+                             [mosek.boundkey.fr] * n, np.zeros(n), np.zeros(n))
+        if psd_total_dims > 0:
+            task.appendbarvars(dims[s.PSD_DIM])
+        running_idx = n0
+        for size_cone in dims[s.SOC_DIM]:
+            task.appendcone(mosek.conetype.quad,
+                            0.0,  # unused
+                            np.arange(running_idx, running_idx + size_cone))
+            running_idx += size_cone
+        for k in range(sum(dims[s.EXP_DIM]) // 3):
+            task.appendcone(mosek.conetype.pexp,
+                            0.0,  # unused
+                            np.arange(running_idx, running_idx + 3))
+            running_idx += 3
+        if num_bool + num_int > 0:
+            task.putvartypelist(data[s.BOOL_IDX], [mosek.variabletype.type_int] * num_bool)
+            task.putvarboundlist(data[s.BOOL_IDX],
+                                 [mosek.boundkey.ra] * num_bool,
+                                 [0] * num_bool, [1] * num_bool)
+            task.putvartypelist(data[s.INT_IDX], [mosek.variabletype.type_int] * num_int)
 
-                task.appendvars(n)
-                task.putvarboundlist(np.arange(n, dtype=int),
-                                     [mosek.boundkey.fr] * n, np.zeros(n), np.zeros(n))
-                if psd_total_dims > 0:
-                    task.appendbarvars(dims[s.PSD_DIM])
-                running_idx = n0
-                for size_cone in dims[s.SOC_DIM]:
-                    task.appendcone(mosek.conetype.quad,
-                                    0.0,  # unused
-                                    np.arange(running_idx, running_idx + size_cone))
-                    running_idx += size_cone
-                for k in range(sum(dims[s.EXP_DIM]) // 3):
-                    task.appendcone(mosek.conetype.pexp,
-                                    0.0,  # unused
-                                    np.arange(running_idx, running_idx + 3))
-                    running_idx += 3
-                if num_bool + num_int > 0:
-                    task.putvartypelist(data[s.BOOL_IDX], [mosek.variabletype.type_int] * num_bool)
-                    task.putvarboundlist(data[s.BOOL_IDX],
-                                         [mosek.boundkey.ra] * num_bool,
-                                         [0] * num_bool, [1] * num_bool)
-                    task.putvartypelist(data[s.INT_IDX], [mosek.variabletype.type_int] * num_int)
+        # Define linear inequality and equality constraints.
+        #
+        #   Mosek will see a total of m linear expressions, which must
+        #   define linear inequalities and equalities. The variable x
+        #   contributes to these linear expressions by standard
+        #   matrix-vector multiplication; the matrix in question is
+        #   referred to as "A" in the mosek documentation. The PSD
+        #   variables have a different means of contributing to the
+        #   linear expressions. Specifically, a PSD variable Xj contributes
+        #   "+tr( \bar{A}_{ij} * Xj )" to the i-th linear expression,
+        #   where \bar{A}_{ij} is specified by a call to putbaraij.
+        #
+        #   The following code has three phases.
+        #       (1) Build the matrix A.
+        #       (2) Specify the \bar{A}_{ij} for PSD variables.
+        #       (3) Specify the RHS of the m linear (in)equalities.
+        #
+        #   Remark : The parameter G gives every row in the first
+        #   n0 columns of A. The remaining columns of A are for SOC
+        #   and EXP slack variables. We can actually account for all
+        #   of these slack variables at once by specifying a giant
+        #   identity matrix in the appropriate position in A.
 
-                # Define linear inequality and equality constraints.
-                #
-                #   Mosek will see a total of m linear expressions, which must
-                #   define linear inequalities and equalities. The variable x
-                #   contributes to these linear expressions by standard
-                #   matrix-vector multiplication; the matrix in question is
-                #   referred to as "A" in the mosek documentation. The PSD
-                #   variables have a different means of contributing to the
-                #   linear expressions. Specifically, a PSD variable Xj contributes
-                #   "+tr( \bar{A}_{ij} * Xj )" to the i-th linear expression,
-                #   where \bar{A}_{ij} is specified by a call to putbaraij.
-                #
-                #   The following code has three phases.
-                #       (1) Build the matrix A.
-                #       (2) Specify the \bar{A}_{ij} for PSD variables.
-                #       (3) Specify the RHS of the m linear (in)equalities.
-                #
-                #   Remark : The parameter G gives every row in the first
-                #   n0 columns of A. The remaining columns of A are for SOC
-                #   and EXP slack variables. We can actually account for all
-                #   of these slack variables at once by specifying a giant
-                #   identity matrix in the appropriate position in A.
+        task.appendcons(m)
+        row, col, vals = sp.sparse.find(G)
+        task.putaijlist(row.tolist(), col.tolist(), vals.tolist())
+        total_soc_exp_slacks = sum(dims[s.SOC_DIM]) + sum(dims[s.EXP_DIM])
+        if total_soc_exp_slacks > 0:
+            i = dims[s.LEQ_DIM] + dims[s.EQ_DIM]  # constraint index in {0, ..., m - 1}
+            j = len(c)  # index of the first slack variable in the block vector "x".
+            rows = np.arange(i, i + total_soc_exp_slacks).tolist()
+            cols = np.arange(j, j + total_soc_exp_slacks).tolist()
+            task.putaijlist(rows, cols, [1] * total_soc_exp_slacks)
 
-                task.appendcons(m)
-                row, col, vals = sp.sparse.find(G)
-                task.putaijlist(row.tolist(), col.tolist(), vals.tolist())
-                total_soc_exp_slacks = sum(dims[s.SOC_DIM]) + sum(dims[s.EXP_DIM])
-                if total_soc_exp_slacks > 0:
-                    i = dims[s.LEQ_DIM] + dims[s.EQ_DIM]  # constraint index in {0, ..., m - 1}
-                    j = len(c)  # index of the first slack variable in the block vector "x".
-                    rows = np.arange(i, i + total_soc_exp_slacks).tolist()
-                    cols = np.arange(j, j + total_soc_exp_slacks).tolist()
-                    task.putaijlist(rows, cols, [1] * total_soc_exp_slacks)
+        # constraint index; start of LMIs.
+        i = dims[s.LEQ_DIM] + dims[s.EQ_DIM] + total_soc_exp_slacks
+        for j, dim in enumerate(dims[s.PSD_DIM]):  # SDP slack variable "Xj"
+            for row_idx in range(dim):
+                for col_idx in range(dim):
+                    val = 1. if row_idx == col_idx else 0.5
+                    row = max(row_idx, col_idx)
+                    col = min(row_idx, col_idx)
+                    mat = task.appendsparsesymmat(dim, [row], [col], [val])
+                    task.putbaraij(i, j, [mat], [1.0])
+                    i += 1
 
-                # constraint index; start of LMIs.
-                i = dims[s.LEQ_DIM] + dims[s.EQ_DIM] + total_soc_exp_slacks
-                for j, dim in enumerate(dims[s.PSD_DIM]):  # SDP slack variable "Xj"
-                    for row_idx in range(dim):
-                        for col_idx in range(dim):
-                            val = 1. if row_idx == col_idx else 0.5
-                            row = max(row_idx, col_idx)
-                            col = min(row_idx, col_idx)
-                            mat = task.appendsparsesymmat(dim, [row], [col], [val])
-                            task.putbaraij(i, j, [mat], [1.0])
-                            i += 1
+        num_eq = len(h) - dims[s.LEQ_DIM]
+        type_constraint = [mosek.boundkey.up] * dims[s.LEQ_DIM] + \
+                          [mosek.boundkey.fx] * num_eq
+        task.putconboundlist(np.arange(m, dtype=int), type_constraint, h, h)
 
-                num_eq = len(h) - dims[s.LEQ_DIM]
-                type_constraint = [mosek.boundkey.up] * dims[s.LEQ_DIM] + \
-                                  [mosek.boundkey.fx] * num_eq
-                task.putconboundlist(np.arange(m, dtype=int), type_constraint, h, h)
+        # Define the objective, and optimize the mosek task.
 
-                # Define the objective, and optimize the mosek task.
+        task.putclist(np.arange(len(c)), c)
+        task.putobjsense(mosek.objsense.minimize)
+        task.optimize()
 
-                task.putclist(np.arange(len(c)), c)
-                task.putobjsense(mosek.objsense.minimize)
-                task.optimize()
+        if verbose:
+            task.solutionsummary(mosek.streamtype.msg)
 
-                if verbose:
-                    task.solutionsummary(mosek.streamtype.msg)
-
-                return {'task': task, 'solver_options': solver_opts}
+        return {'env': env, 'task': task, 'solver_options': solver_opts}
 
     def invert(self, results, inverse_data):
         """
         Use information contained within "results" and "inverse_data" to properly
         define a cvxpy Solution object.
 
-        :param results: a dictionary with two key-value pairs:
+        :param results: a dictionary with three key-value pairs:
+            results['env'] == the mosek Environment object generated during solve_via_data,
             results['task'] == the mosek Task object generated during solve_via_data,
             results['solver_options'] == the dictionary of parameters passed to solve_via_data.
         :param inverse_data: data recorded during "apply".
@@ -447,6 +447,7 @@ class MOSEK(ConicSolver):
                       mosek.solsta.near_dual_infeas_cer: s.UNBOUNDED_INACCURATE}
         STATUS_MAP = defaultdict(lambda: s.SOLVER_ERROR, STATUS_MAP)
 
+        env = results['env']
         task = results['task']
         solver_opts = results['solver_options']
 
@@ -482,6 +483,10 @@ class MOSEK(ConicSolver):
                 opt_val = None
             primal_vars = None
             dual_vars = None
+
+        # Delete the mosek Task and Environment
+        task.__exit__(None, None, None)
+        env.__exit__(None, None, None)
 
         return Solution(status, opt_val, primal_vars, dual_vars, attr={})
 
