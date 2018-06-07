739273120939c9a0603089b9b857761107e0a568
rileyjmurray <inventor1488@gmail.com>
2018-06-07 13:50:17 -0700

    travis pip

--- continuous_integration/deploy_script.sh
+++ continuous_integration/deploy_script.sh
@@ -3,12 +3,12 @@
 # Consider deploying to PyPI
 if [ $DEPLOY_SOURCE = true ] && [ $TRAVIS_OS_NAME = osx ]; then
     source activate testenv
-    conda install requests
+    conda install --yes requests
     REMOTE_VERSION=`python continuous_integration/versiongetter.py`
     LOCAL_VERSION=`python -c "import cvxpy; print(cvxpy.__version__)"`
     if [ $REMOTE_VERSION -ne $LOCAL_VERSION ]; then
         # Assume the local version is ahead of remote version
-        conda install twine
+        conda install --yes twine
         python setup.py sdist
         twine upload --repository-url $PYPI_SERVER dist/* -u $PYPI_TEST_USER -p $PYPI_TEST_PASSWORD
     fi
