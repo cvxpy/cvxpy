97b6f9c6923e7ac384b8c71200812688a46ac4fc
rileyjmurray <rileyjmurray@users.noreply.github.com>
2018-06-08 14:54:48 -0700

    this should fix issue with early deletion of mosek Task objects (#502)
    * this should fix issue with early deletion of mosek Task objects
    
    * trying to get pypi deployment to work
    
    * travis pypi
    
    * travis pypi
    
    * travis pypi
    
    * travis pypi
    
    * travis pypi
    
    * travis build version change
    
    * travis version pypi thing
    
    * removed CBC from builds to speed things up.
    
    * version update
    
    * travis pypi
    
    * travis deploy source when updated
    
    * fixed a thing
    
    * test deploy
    
    * fixed a parenthesis issue
    
    * travis pip stuff
    
    * travis pip
    
    * travis pip
    
    * travis pip
    
    * travis pip
    
    * travis conda
    
    * travis conda
    
    * travis conda
    
    * travis conda
    
    * travis conda
    
    * flake8
    
    * travis conda
    
    * travis conda
    
    * travis conda
    
    * automated conda and pip deployments via travis

---
+++ continuous_integration/versiongetter.py
@@ -0,0 +1,36 @@
+import requests
+from distutils.version import StrictVersion
+
+
+def pypi_version(test=False):
+    if test:
+        url = "https://test.pypi.org/pypi/cvxpy/json"
+    else:
+        url = "https://pypi.org/pypi/cvxpy/json"
+    r = requests.get(url)
+    data = r.json()
+    versions = list(data["releases"].keys())
+    versions.sort(key=StrictVersion)
+    return versions[-1]
+
+
+def conda_version(python_version, operating_system):
+    #
+    #   python_version must be one of {2.7, 3.5, 3.6}
+    #
+    #   operating system must be one of {linux, osx, win}
+    #
+    pyvers_dict = {'2.7': 'py27', '3.5': 'py35', '3.6': 'py36'}
+    pyvers = pyvers_dict[python_version]
+    url = "https://api.anaconda.org/package/cvxgrp/cvxpy"
+    r = requests.get(url)
+    data = r.json()
+    filestrings = [str(data['files'][i]['full_name']) for i in range(len(data['files']))]
+    versions = []
+    for fs in filestrings:
+        fs = fs.split('/')
+        # fs = ['cvxgrp', 'cvxpy', '<x.y.z>', '<os>', '<filename>' ]
+        if operating_system in fs[3] and pyvers in fs[4]:
+            versions.append(fs[2])
+    versions.sort(key=StrictVersion)
+    return versions[-1]
