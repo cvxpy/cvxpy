627f1c9313f7fd1201a194f1fc3d9cf4ca770714
Steven Diamond <stevend2@stanford.edu>
2018-10-09 16:05:41 -0700

    removed unnecessary variables for QP canon

--- cvxpy/reductions/qp2quad_form/atom_canonicalizers/quad_form_canon.py
+++ cvxpy/reductions/qp2quad_form/atom_canonicalizers/quad_form_canon.py
@@ -21,5 +21,8 @@ from cvxpy.expressions.variable import Variable
 def quad_form_canon(expr, args):
     affine_expr = expr.args[0]
     P = expr.args[1]
-    t = Variable(affine_expr.shape)
-    return SymbolicQuadForm(t, P, expr), [affine_expr == t]
+    if isinstance(affine_expr, Variable):
+        return SymbolicQuadForm(affine_expr, P, expr), []
+    else:
+        t = Variable(affine_expr.shape)
+        return SymbolicQuadForm(t, P, expr), [affine_expr == t]
