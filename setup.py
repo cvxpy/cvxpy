c7674093e9c8a22ffe9de25df2caddbac8c2200e
Ali <shariat@users.noreply.github.com>
2018-10-15 18:52:17 -0700

    handle builtin when it is a dict (#602)
    * handle builtin when it is a dict
    
    closes #594
    
    * fixed spacing
    
    * added comments

--- setup.py
+++ setup.py
@@ -7,7 +7,13 @@ class build_ext_cvxpy(build_ext):
     def finalize_options(self):
         build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
+        # `__builtins__` can be a dict
+        # see https://docs.python.org/2/reference/executionmodel.html
+        if isinstance(__builtins__, dict):
+            __builtins__['__NUMPY_SETUP__'] = False
+        else:
+            __builtins__.__NUMPY_SETUP__ = False
+
         import numpy
         self.include_dirs.append(numpy.get_include())
 
