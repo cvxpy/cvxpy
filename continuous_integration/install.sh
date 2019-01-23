0175cd5bf2d2834a062209ba7b7415c4edeac350
Steven Diamond <stevend2@stanford.edu>
2019-01-23 15:25:08 -0800

    fixed getversion, no cvxopt for 3.7

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -28,21 +28,23 @@ if [[ "$DISTRIB" == "conda" ]]; then
         conda install -c default --yes flake8
 
         # Install GLPK.
-        wget http://ftp.gnu.org/gnu/glpk/glpk-4.60.tar.gz
-        tar -zxvf glpk-4.60.tar.gz
-        cd glpk-4.60
-        sudo ./configure
-        sudo make
-        sudo make check
-        sudo make install
-        sudo ldconfig
-        cd ..
+        if [[ "$CVXOPT" == "true" ]]; then
+            wget http://ftp.gnu.org/gnu/glpk/glpk-4.60.tar.gz
+            tar -zxvf glpk-4.60.tar.gz
+            cd glpk-4.60
+            sudo ./configure
+            sudo make
+            sudo make check
+            sudo make install
+            sudo ldconfig
+            cd ..
 
-        # Install CVXOPT with GLPK bindings.
-        CVXOPT_BUILD_GLPK=1
-        CVXOPT_GLPK_LIB_DIR=/usr/local/lib
-        CVXOPT_GLPK_INC_DIR=/usr/local/include
-        conda install -c conda-forge --yes cvxopt
+            # Install CVXOPT with GLPK bindings.
+            CVXOPT_BUILD_GLPK=1
+            CVXOPT_GLPK_LIB_DIR=/usr/local/lib
+            CVXOPT_GLPK_INC_DIR=/usr/local/include
+            conda install -c conda-forge --yes cvxopt
+        fi
 
     elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
         wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh \
