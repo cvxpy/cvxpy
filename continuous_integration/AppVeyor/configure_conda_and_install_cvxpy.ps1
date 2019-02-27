78b5270ce652c7468428caa8f5b7dc88a46431de
rileyjmurray <inventor1488@gmail.com>
2019-02-26 17:28:37 -0800

    The conda build is not working right now; this is for two reaons. First, I had not added cvxgrp or oxfordcontrol to the standard conda channels (this was easy to fix). The second issue is more delicate: cvxpys conda recipe has libgcc as a build requirement and a run requirement. This requirement is inherently incompatible with Windows, which does not have access to GCC. The build requirement is important, but it doesnt need to be fulfilled by libgcc per-say (Visual Studios default compilers should suffice). Its not clear to me why there is a run requirement for libgcc at all. Im removing both the build and run requirements from the conda recipe, for now.

--- continuous_integration/AppVeyor/configure_conda_and_install_cvxpy.ps1
+++ continuous_integration/AppVeyor/configure_conda_and_install_cvxpy.ps1
@@ -1,7 +1,7 @@
+conda config --add channels conda-forge cvxgrp oxfordcontrol
 conda create -n testenv --yes python=$env:PYTHON_VERSION mkl=2018.0.3 pip nose numpy scipy
 conda activate testenv
-conda config --add channels conda-forge
-conda install -c conda-forge --yes lapack
-conda install -c conda-forge --yes ecos scs multiprocess
+conda install -c conda-forge --yes lapack ecos multiprocess
+conda install -c cvxgrp --yes scs
 conda install -c anaconda --yes flake8
 python setup.py install
