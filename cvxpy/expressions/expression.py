1f64554114abd4afdd102b6fb42112727f7d86b1
Akshay <akshaykagrawal7@gmail.com>
2019-05-16 00:14:48 -0700

    Support for multiplicatiof of parameters.

--- cvxpy/expressions/expression.py
+++ cvxpy/expressions/expression.py
@@ -157,12 +157,17 @@ class Expression(u.Canonical):
     def is_constant(self):
         """Is the expression constant?
         """
-        try:
-            return self.__is_constant
-        except AttributeError:
+        return self._check_is_constant()
+
+    def _check_is_constant(self, recompute=False):
+        """Is the expression constant?
+        """
+        if recompute and hasattr(self, "__is_constant"):
+            del self.__is_constant
+        if not hasattr(self, "__is_constant"):
             self.__is_constant = 0 in self.shape or all(
-                arg.is_constant() for arg in self.args)
-            return self.__is_constant
+                arg._check_is_constant(recompute) for arg in self.args)
+        return self.__is_constant
 
     @abc.abstractmethod
     def is_dpp(self, context='CP'):
