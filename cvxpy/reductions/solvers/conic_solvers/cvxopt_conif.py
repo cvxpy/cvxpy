3a34cc2f8b36273004b9c1fdbcc0e97c41633001
Steven Diamond <stevend2@stanford.edu>
2019-05-20 19:34:57 -0700

    fixed solver codes

--- cvxpy/reductions/solvers/conic_solvers/cvxopt_conif.py
+++ cvxpy/reductions/solvers/conic_solvers/cvxopt_conif.py
@@ -52,7 +52,7 @@ class CVXOPT(ECOS):
 
     # Map of CVXOPT status to CVXPY status.
     STATUS_MAP = {"optimal": s.OPTIMAL,
-                  "feasible": s.OPTIMAL,
+                  "feasible": s.OPTIMAL_INACCURATE,
                   "infeasible": s.INFEASIBLE,
                   "primal infeasible": s.INFEASIBLE,
                   "LP relaxation is primal infeasible": s.INFEASIBLE,
