62d70b4599d10d048cd2d46c29c93ac297dc70ad
Akshay <akshaykagrawal7@gmail.com>
2019-06-02 00:29:55 -0700

    Generated gh-pages for commit 0945cfc5 Author: Akshay <akshaykagrawal7@gmail.com>
        Get PyPi version from pypi.python.org/pypi API

--- tmp/exp.py
+++
@@ -1,6 +0,0 @@
-import cvxpy as cp
-
-
-x = cp.Variable()
-prob = cp.Problem(cp.Minimize(cp.exp(x)), [cp.ceil(x) <= 10])
-prob.solve(qcp=True, verbose=True)
--- tmp/prob.py
+++
@@ -1,9 +0,0 @@
-import cvxpy as cp
-import numpy as np
-
-A = np.random.randn(20, 20)
-b = A @ np.random.randn(20)
-
-x = cp.Variable(20)
-prob = cp.Problem(cp.Minimize(cp.sum_squares(A @ x - b)))
-prob.solve(solver=cp.OSQP, max_iter=1)
