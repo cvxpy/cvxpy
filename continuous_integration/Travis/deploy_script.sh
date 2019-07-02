47bfe4338f224bb4b5d8de262b32505cc0fb9328
rileyjmurray <inventor1488@gmail.com>
2019-07-01 20:11:18 -0700

    This file had a typo. However, deployment will not succeed with this commit! Reason being, "conda install package_name_here" is sometimes resolving dependencies in such a way that it changes the local environment version of python. As a result, a Python 3.x environment is having the wheel built under a 3.y environment where y is not equal to x

--- continuous_integration/Travis/deploy_script.sh
+++ continuous_integration/Travis/deploy_script.sh
@@ -8,7 +8,7 @@ conda install --yes requests twine readme_renderer
 
 # We chose a somewhat arbitrary build configuration (a specially marked OSX configuration)
 # to be the designated uploader of source distributions.
-if [ $DEPLOY_PYPI_SOURCE == True ] && [ $TRAVIS_OS_NAME = osx ]; then
+if [ $DEPLOY_PYPI_SOURCE == "True" ] && [ $TRAVIS_OS_NAME == "osx" ]; then
     # consider deploying to PyPI
     cd continuous_integration
     UPDATE_PYPI_SOURCE=`python -c "import versiongetter as vg; print(vg.update_pypi_source('$PYPI_API_ENDPOINT'))"`
@@ -22,9 +22,9 @@ if [ $DEPLOY_PYPI_SOURCE == True ] && [ $TRAVIS_OS_NAME = osx ]; then
 fi
 
 cd continuous_integration
-UPDATE_PYPI_WHEEL=python -c "import versiongetter as vg; print(vg.update_pypi_wheel('$PYTHON_VERSION','$TRAVIS_OS_NAME','$PYPI_API_ENDPOINT'))"
+UPDATE_PYPI_WHEEL=`python -c "import versiongetter as vg; print(vg.update_pypi_wheel('$PYTHON_VERSION','$TRAVIS_OS_NAME','$PYPI_API_ENDPOINT'))"`
 cd ..
-if [ $UPDATE_PYPI_WHEEL == True ]; then
+if [ $UPDATE_PYPI_WHEEL == "True" ]; then
     python setup.py bdist_wheel
     twine upload --repository-url $PYPI_SERVER dist/* -u $PYPI_USER -p $PYPI_PASSWORD
 fi
@@ -32,7 +32,7 @@ fi
 cd continuous_integration
 UPDATE_CONDA=`python -c "import versiongetter as vg; print(vg.update_conda('$PYTHON_VERSION','$TRAVIS_OS_NAME'))"`
 cd ..
-if [ $UPDATE_CONDA == True ]; then
+if [ $UPDATE_CONDA == "True" ]; then
     conda install --yes conda-build
     conda install --yes anaconda-client
     conda config --set anaconda_upload yes
