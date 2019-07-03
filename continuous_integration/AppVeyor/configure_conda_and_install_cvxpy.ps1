3e64975c1fb02bbc253d17d9b97e722259ac6c8c
rileyjmurray <inventor1488@gmail.com>
2019-07-02 21:35:09 -0700

    debugging only - check content of "pinned" file in AppVeyor

--- continuous_integration/AppVeyor/configure_conda_and_install_cvxpy.ps1
+++ continuous_integration/AppVeyor/configure_conda_and_install_cvxpy.ps1
@@ -3,6 +3,7 @@ conda config --add channels oxfordcontrol
 conda create -n testenv --yes python=$env:PYTHON_VERSION mkl=2018.0.3 pip nose numpy scipy
 conda activate testenv
 $text = 'python=$env:PYTHON_VERSION' | Out-File $text -FilePath C:\conda\envs\testenv\conda-meta\pinned
+Get-Content -Path C:\conda\envs\testenv\conda-meta\pinned
 conda install --yes lapack ecos multiprocess
 pip install scs<=2.0
 conda install -c anaconda --yes flake8
