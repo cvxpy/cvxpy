d84f3a6e5fd1c4b4b0949284ff2e7d69840ba72c
rileyjmurray <inventor1488@gmail.com>
2019-07-02 21:44:32 -0700

    AppVeyor update write command to conda "pinned" file

--- continuous_integration/AppVeyor/configure_conda_and_install_cvxpy.ps1
+++ continuous_integration/AppVeyor/configure_conda_and_install_cvxpy.ps1
@@ -2,9 +2,12 @@ conda config --add channels conda-forge
 conda config --add channels oxfordcontrol
 conda create -n testenv --yes python=$env:PYTHON_VERSION mkl=2018.0.3 pip nose numpy scipy
 conda activate testenv
-$text = 'python=$env:PYTHON_VERSION' | Out-File $text -FilePath C:\conda\envs\testenv\conda-meta\pinned
+"python=$env:PYTHON_VERSION" | Out-File C:\conda\envs\testenv\conda-meta\pinned -encoding ascii
 Get-Content -Path C:\conda\envs\testenv\conda-meta\pinned
 conda install --yes lapack ecos multiprocess
 pip install scs<=2.0
 conda install -c anaconda --yes flake8
 python setup.py install
+
+
+"Some Text on first line" | Out-File C:\filename1.txt -encoding ascii
