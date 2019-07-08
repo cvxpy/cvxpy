dbcfd908cbf3adbcb4f058509b4f59609f64bcfb
rileyjmurray <rileyjmurray@users.noreply.github.com>
2019-07-08 20:32:34 +0200

    Resolve existing CI issues, new wheels for OSX, improvements for Windows wheels (#760)
    * AppVeyor builds are failing due to conda issues. Try moving to anaconda3.
    
    * AppVeyor builds failing. Something is going wrong with conda update. Try *not* updating conda.
    
    * attempt to resolve bugs in existing pypi uploads (getting all relevant wheels from AppVeyor, getting source distribution from Travis), and add support manylinux wheels
    
    * pypi deployments failed due to a readme-renderer (aka readme_renderer) dependency
    
    * This file had a typo. However, deployment will not succeed with this commit! Reason being, "conda install package_name_here" is sometimes resolving dependencies in such a way that it changes the local environment version of python. As a result, a Python 3.x environment is having the wheel built under a 3.y environment where y is not equal to x
    
    * Corrected conda behavior for updating dependencies during deployment.
    
    * corrected a keyword argument to conda install
    
    * switch to loose version in versiongetter.py; minor change for manylinux (dont expect this to actually help with manylinux builds)
    
    * Use conda "pinned" file to keep key dependencies at specific versions throughout the build process.
    
    * only pin python (rather than python, numpy, scipy) in conda settings
    
    * use conda "pinned" file for AppVeyor builds; remove --no-update-deps
    
    * debugging only - check content of "pinned" file in AppVeyor
    
    * AppVeyor update write command to conda "pinned" file
    
    * removed some leftover, unnecessary changes
    
    * should have removed this in previous commit
    
    * Well, that shouldnt have been there.

--- continuous_integration/Travis/install_dependencies.sh
+++ continuous_integration/Travis/install_dependencies.sh
@@ -17,12 +17,14 @@ if [[ "$DISTRIB" == "conda" ]]; then
         chmod +x miniconda.sh && ./miniconda.sh -b
         export PATH=/home/travis/miniconda3/bin:$PATH
         conda update --yes conda
-
         # Configure the conda environment and put it in the path using the
         # provided versions
         conda create -n testenv --yes python=$PYTHON_VERSION mkl pip nose \
                 numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION
         source activate testenv
+        PIN_FILE=/home/travis/miniconda3/envs/testenv/conda-meta/pinned
+        touch $PIN_FILE
+        echo "python=$PYTHON_VERSION" >> $PIN_FILE
         conda install -c conda-forge --yes lapack
         conda install -c conda-forge --yes ecos scs multiprocess
         conda install -c default --yes flake8
@@ -43,6 +45,9 @@ if [[ "$DISTRIB" == "conda" ]]; then
         conda create -n testenv --yes python=$PYTHON_VERSION mkl pip nose \
               numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION
         source activate testenv
+        PIN_FILE=/Users/travis/miniconda3/envs/testenv/conda-meta/pinned
+        touch $PIN_FILE
+        echo "python=$PYTHON_VERSION.*" >> $PIN_FILE
         conda install -c conda-forge --yes ecos scs multiprocess
         conda install -c default --yes flake8=3.5.0
     fi
