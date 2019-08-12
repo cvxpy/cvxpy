7a46af6421fccf68db29f2ae4dbed0d3c953af1c
Akshay <akshaykagrawal7@gmail.com>
2019-08-12 05:57:59 -0700

    Fix import error

--- cvxpy/reductions/dgp2dcp/atom_canonicalizers/sum_canon.py
+++ cvxpy/reductions/dgp2dcp/atom_canonicalizers/sum_canon.py
@@ -1,13 +1,13 @@
 from cvxpy.atoms.affine.hstack import hstack
 from cvxpy.atoms.affine.reshape import reshape
 from cvxpy.reductions.dgp2dcp.atom_canonicalizers.add_canon import add_canon
-from cvxpy.reductions.dgp2dcp import util
+from cvxpy.reductions.dgp2dcp.util import explicit_sum
 
 
 def sum_canon(expr, args):
     X = args[0]
     if expr.axis is None:
-        summation = util.sum(X)
+        summation = explicit_sum(X)
         canon, _ = add_canon(summation, summation.args)
         return reshape(canon, expr.shape), []
 
@@ -16,7 +16,7 @@ def sum_canon(expr, args):
 
     rows = []
     for i in range(X.shape[0]):
-        summation = util.sum(X[i])
+        summation = explicit_sum(X[i])
         canon, _ = add_canon(summation, summation.args)
         rows.append(canon)
     canon = hstack(rows)
