f03479907c3821466734b96ed3de4fc154204331
Akshay <akshaykagrawal7@gmail.com>
2019-08-12 05:41:18 -0700

    (DGP) Replace builtin `sum` with explicit summation in canonicalization
    Related to #782

--- cvxpy/reductions/dgp2dcp/atom_canonicalizers/quad_over_lin_canon.py
+++ cvxpy/reductions/dgp2dcp/atom_canonicalizers/quad_over_lin_canon.py
@@ -1,8 +1,9 @@
 from cvxpy.atoms.affine.vec import vec
+from cvxpy.reductions.dgp2dcp import util
 
 
 def quad_over_lin_canon(expr, args):
     x = vec(args[0])
     y = args[1]
-    numerator = sum(2 * xi for xi in x)
+    numerator = util.sum(2 * xi for xi in x)
     return numerator - y, []
--- cvxpy/reductions/dgp2dcp/atom_canonicalizers/trace_canon.py
+++ cvxpy/reductions/dgp2dcp/atom_canonicalizers/trace_canon.py
@@ -1,7 +1,8 @@
 from cvxpy.atoms.affine.diag import diag
 from cvxpy.reductions.dgp2dcp.atom_canonicalizers.add_canon import add_canon
+from cvxpy.reductions.dgp2dcp import util
 
 
 def trace_canon(expr, args):
-    diag_sum = sum(diag(args[0]))
+    diag_sum = util.sum(diag(args[0]))
     return add_canon(diag_sum, diag_sum.args)
