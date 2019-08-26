7f8d73edb245a2e9c9da31e6d07a9bbd2de07b9d
Steven Diamond <stevend2@stanford.edu>
2019-08-26 15:10:17 -0700

    added cummax

---
+++ cvxpy/reductions/eliminate_pwl/atom_canonicalizers/cumsum_canon.py
@@ -0,0 +1,32 @@
+"""
+Copyright 2013 Steven Diamond
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cvxpy.expressions.variable import Variable
+
+
+def cumsum_canon(expr, args):
+    """Cumulative sum.
+    """
+    X = args[0]
+    axis = expr.axis
+    # Implicit O(n) definition:
+    # X = Y[1:,:] - Y[:-1, :]
+    Y = Variable(expr.shape)
+    if axis == 0:
+        constr = [X[1:] == Y[1:] - Y[:-1], Y[0] == X[0]]
+    else:
+        constr = [X[:, 1:] == Y[:, 1:] - Y[:, :-1], Y[:, 0] == X[:, 0]]
+    return (Y, constr)
