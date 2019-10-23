2d00928669ef070f28dec9b9e6fc375c043c86c1
Akshay <akshaykagrawal7@gmail.com>
2019-10-22 22:03:53 -0700

    Revert "install diffcp from test pypi for now"
    This reverts commit 8a216e1c9b023d14896dbcbb1db1a8f38a30c75e.

--- continuous_integration/AppVeyor/configure_conda_and_install_cvxpy.ps1
+++ continuous_integration/AppVeyor/configure_conda_and_install_cvxpy.ps1
@@ -6,5 +6,5 @@ conda activate testenv
 conda install --yes lapack ecos multiprocess
 conda install -c conda-forge --yes scs
 conda install -c anaconda --yes flake8
-pip install --index-url https://test.pypi.org/simple/ --upgrade diffcp
+pip install diffcp
 python setup.py install
