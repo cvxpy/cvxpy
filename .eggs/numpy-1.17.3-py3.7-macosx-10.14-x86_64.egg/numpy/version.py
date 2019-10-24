daabf4851a61a9c50ad778991060bcca4dac5731
Akshay <akshaykagrawal7@gmail.com>
2019-10-23 19:46:43 -0700

    Generated gh-pages for commit ca412875 Author: Akshay <akshaykagrawal7@gmail.com>
        flake8

---
+++ .eggs/numpy-1.17.3-py3.7-macosx-10.14-x86_64.egg/numpy/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+#
+# To compare versions robustly, use `numpy.lib.NumpyVersion`
+short_version = '1.17.3'
+version = '1.17.3'
+full_version = '1.17.3'
+git_revision = 'ff3df08438d570b0ccdda3f8a008278d8a4ad394'
+release = True
+
+if not release:
+    version = full_version
