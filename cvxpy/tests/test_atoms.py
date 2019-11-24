5b76e5aba22ac1c089aa461de417694c48f514a9
rileyjmurray <inventor1488@gmail.com>
2019-11-23 20:21:30 -0800

    Efficient canonicalization of log_det, normNuc, sigma_max. Added a vec_to_upper_tri function.

--- cvxpy/tests/test_atoms.py
+++ cvxpy/tests/test_atoms.py
@@ -573,6 +573,28 @@ class TestAtoms(BaseTest):
         self.assertEqual(str(cm.exception),
                          "Argument to upper_tri must be a square matrix.")
 
+    def test_vec_to_upper_tri(self):
+        from cvxpy.atoms.affine.upper_tri import vec_to_upper_tri
+        x = Variable(shape=(3,))
+        X = vec_to_upper_tri(x)
+        x.value = np.array([1, 2, 3])
+        actual = X.value
+        expect = np.array([[1, 2], [0, 3]])
+        assert np.allclose(actual, expect)
+        y = Variable(shape=(1,))
+        y.value = np.array([4])
+        Y = vec_to_upper_tri(y, strict=True)
+        actual = Y.value
+        expect = np.array([[0, 4], [0, 0]])
+        assert np.allclose(actual, expect)
+        A_expect = np.array([[0, 11, 12, 13],
+                             [0, 0, 16, 17],
+                             [0, 0, 0, 21],
+                             [0, 0, 0, 0]])
+        a = np.array([11, 12, 13, 16, 17, 21])
+        A_actual = vec_to_upper_tri(a, strict=True).value
+        assert np.allclose(A_actual, A_expect)
+
     def test_huber(self):
         # Valid.
         cp.huber(self.x, 1)
