503dc0c5c93dec7c5440f73735bdcd435a376d24
rileyjmurray <inventor1488@gmail.com>
2019-11-26 20:48:32 -0800

    introduce a scalar_product atom, which wraps elementwise multiplication composed with sum

--- cvxpy/atoms/affine/binary_operators.py
+++ cvxpy/atoms/affine/binary_operators.py
@@ -21,6 +21,10 @@ import cvxpy.interface as intf
 from cvxpy.atoms.affine.affine_atom import AffAtom
 from cvxpy.atoms.affine.add_expr import AddExpression
 from cvxpy.atoms.affine.promote import promote
+from cvxpy.atoms.affine.sum import sum as cvxpy_sum
+from cvxpy.expressions.expression import Expression
+from cvxpy.atoms.affine.bmat import bmat
+from cvxpy.atoms import conj
 from cvxpy.expressions.constants.parameter import is_param_affine, is_param_free
 from cvxpy.error import DCPError
 import cvxpy.lin_ops.lin_utils as lu
@@ -411,3 +415,19 @@ class DivExpression(BinaryOperator):
             (LinOp for objective, list of constraints)
         """
         return (lu.div_expr(arg_objs[0], arg_objs[1]), [])
+
+
+def scalar_product(x, y):
+    if isinstance(x, np.ndarray):
+        x = Expression.cast_to_const(x)
+    if isinstance(y, np.ndarray):
+        y = Expression.cast_to_const(y)
+    if isinstance(x, list):
+        x = bmat(x)
+    if isinstance(y, list):
+        y = bmat(y)
+    if not (isinstance(x, Expression) and isinstance(y, Expression)):
+        raise RuntimeError('Unknown arguments to scalar_product.')
+    prod = multiply(conj(x), y)
+    res = cvxpy_sum(prod)
+    return res
