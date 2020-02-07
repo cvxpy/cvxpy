ef7d4e3aa1d7293faf0f0b86223b2b39a90b1269
Steven Diamond <stevend2@stanford.edu>
2020-02-07 12:45:43 -0800

    upping version

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ canon = Extension(
 
 setup(
     name='cvxpy',
-    version='1.1.0a2',
+    version='1.1.0a3',
     author='Steven Diamond, Eric Chu, Stephen Boyd',
     author_email='stevend2@stanford.edu, akshayka@cs.stanford.edu, '
                  'echu508@stanford.edu, boyd@stanford.edu',
