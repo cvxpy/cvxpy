871397a9e199712b8dccd24069e86f7d7d2e8d50
uelwer <tobias.uelwer@uni-duesseldorf.de>
2020-02-14 16:21:45 +0100

    Add symmetry check for lambda_max atom

--- cvxpy/atoms/lambda_max.py
+++ cvxpy/atoms/lambda_max.py
@@ -64,6 +64,9 @@ class lambda_max(Atom):
         if not self.args[0].ndim == 2 or self.args[0].shape[0] != self.args[0].shape[1]:
             raise ValueError("The argument '%s' to lambda_max must resolve to a square matrix."
                              % self.args[0].name())
+        if not np.allclose(self.args[0].value, self.args[0].value.T, rtol=1e-03, atol=1e-04):
+            raise ValueError("The argument '%s' to lambda_max must be symmetric."
+                             % self.args[0].name())
 
     def shape_from_args(self):
         """Returns the (row, col) shape of the expression.
