5ba47bbae783678f1a7b6bcab527f7af2762851b
Akshay <akshaykagrawal7@gmail.com>
2020-02-20 18:05:19 -0800

    Generated gh-pages for commit fec749b7 Author: Akshay <akshaykagrawal7@gmail.com>
        Fix documentation for .derivative() method

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -3,7 +3,6 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '1.1.0a3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
