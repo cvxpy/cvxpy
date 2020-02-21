a69217d7fc951d075b08453143be69b8a2d41f95
Pietro Belotti <pietrobelotti@fico.com>
2020-02-21 16:05:41 +0100

    Written QP solver

--- cvxpy/reductions/solvers/qp_solvers/xpress_qpif.py
+++ cvxpy/reductions/solvers/qp_solvers/xpress_qpif.py
@@ -9,7 +9,8 @@ from cvxpy.reductions.solvers.conic_solvers.xpress_conif import (
 )
 import numpy as np
 
-from .conic_solvers.xpress_conif import makeMstart
+# method to get initial indices of each column
+from ..conic_solvers.xpress_conif import makeMstart
 
 
 class XPRESS(QpSolver):
@@ -17,6 +18,9 @@ class XPRESS(QpSolver):
     """Quadratic interface for the FICO Xpress solver"""
 
     MIP_CAPABLE = True
+
+    def __init__ (self):
+        self.prob_ = None
 
     def name(self):
         return s.XPRESS
@@ -68,98 +72,121 @@ class XPRESS(QpSolver):
 
         import xpress as xp
 
-        import pdb
-        pdb.set_trace()
+        # Objective function: 1/2 x' P x + q'x
 
-        P = data[s.P].tocsr()       # objective quadratic coefficients
+        Q = data[s.P].tocoo()       # objective quadratic coefficients
         q = data[s.Q]               # objective linear coefficient (size n_var)
 
         # Equations, Ax = b
 
-        A = data[s.A].tocsr()       # linear coefficient matrix
+        A = data[s.A]               # linear coefficient matrix
         b = data[s.B]               # rhs
 
         # Inequalities, Fx <= g
 
-        F = data[s.F].tocsr()       # linear coefficient matrix
+        F = data[s.F]               # linear coefficient matrix
         g = data[s.G]               # rhs
 
         n_var = data['n_var']
         n_eq = data['n_eq']
         n_ineq = data['n_ineq']
 
-        # Constrain values between bounds
-        constrain_xpress_infty(b)
-        constrain_xpress_infty(g)
+        self.prob_ = xp.problem()
 
-        # Define XPRESS problem
-        model = xp.problem()
+        # setControl() accepts 'option':value dictionaries
+        self.prob_.setControl(solver_opts)
 
-        # Add variables and linear objective
-        var_idx = list(model.variables.add(obj=q,
-                                           lb=-xp.infinity*np.ones(n_var),
-                                           ub=xp.infinity*np.ones(n_var)))
+        # qp_solver has the following format:
+        #
+        #    minimize      1/2 x' P x + q' x
+        #    subject to    A x =  b
+        #                  F x <= g
 
-        # Constrain binary/integer variables if present
-        for i in data[s.BOOL_IDX]:
-            model.variables.set_types(var_idx[i],
-                                      model.variables.type.binary)
-        for i in data[s.INT_IDX]:
-            model.variables.set_types(var_idx[i],
-                                      model.variables.type.integer)
+        mstartA = makeMstart(A, n_var, 1)
+        mstartF = makeMstart(F, n_var, 1)
 
-        # Add constraints
-        lin_expr, rhs = [], []
-        for i in range(n_eq):  # Add equalities
-            start = A.indptr[i]
-            end = A.indptr[i+1]
-            lin_expr.append([A.indices[start:end].tolist(),
-                             A.data[start:end].tolist()])
-            rhs.append(b[i])
-        if lin_expr:
-            model.linear_constraints.add(lin_expr=lin_expr,
-                                         senses=["E"] * len(lin_expr),
-                                         rhs=rhs)
+        # index start is simply the sum of the two mstarts for A and F
+        mstart = mstartA + mstartF
 
-        lin_expr, rhs = [], []
-        for i in range(n_ineq):  # Add inequalities
-            start = F.indptr[i]
-            end = F.indptr[i+1]
-            lin_expr.append([F.indices[start:end].tolist(),
-                             F.data[start:end].tolist()])
-            rhs.append(g[i])
-        if lin_expr:
-            model.linear_constraints.add(lin_expr=lin_expr,
-                                         senses=["L"] * len(lin_expr),
-                                         rhs=rhs)
+        # In order to arrange (A;F) as a single matrix, we have to
+        # concatenate all indices. I prefer not to do a vstack of A
+        # and F as it might increase memory usage a lot.
 
-        # Set quadratic Cost
-        if P.count_nonzero():  # Only if quadratic form is not null
-            qmat = []
-            for i in range(n_var):
-                start = P.indptr[i]
-                end = P.indptr[i+1]
-                qmat.append([P.indices[start:end].tolist(),
-                            P.data[start:end].tolist()])
-            model.objective.set_quadratic(qmat)
+        # Begin by creating two vectors of zeros for indices and
+        # coefficients
+        mrwind = np.zeros(mstart[n_var], dtype=np.int64)
+        dmatval = np.zeros(mstart[n_var], dtype=np.float64)
+
+        # Fill in mrwind and dmatval by alternately drawing from
+        # indices/coeff of A and F.
+
+        for i in range(n_var):
+
+            nElemA = mstartA[i+1] - mstartA[i]  # number of elements of A in this column
+            nElemF = mstartF[i+1] - mstartF[i]  #                       F
+
+            if nElemA:
+                mrwind[mstart[i]:mstart[i] + nElemA] = A.indices[mstartA[i]:mstartA[i+1]]
+                dmatval[mstart[i]:mstart[i] + nElemA] = A.data[mstartA[i]:mstartA[i+1]]
+            if nElemF:
+                mrwind[mstart[i] + nElemA: mstart[i] + nElemA + nElemF] = F.indices[mstartF[i]:mstartF[i+1]]
+                dmatval[mstart[i] + nElemA: mstart[i] + nElemA + nElemF] = F.data[mstartF[i]:mstartF[i+1]]
+
+            mstart[i] = mstartA[i] + mstartF[i]
+
+        # The last value of mstart must be the total number of
+        # coefficients.
+        mstart[n_var] = mstartA[n_var] + mstartF[n_var]
+
+        # Q matrix is input via row/col indices and value, but only
+        # for the upper triangle. We just make it symmetric and twice
+        # itself, then, just remove all lower-triangular elements.
+        Q += Q.transpose()
+
+        mqcol1 = Q.row  [Q.row <= Q.col]
+        mqcol2 = Q.col  [Q.row <= Q.col]
+        dqe    = Q.data [Q.row <= Q.col]
+
+        colnames = ['x_{0:09d}'.format(i) for i in range(n_var)]
+        rownames = ['eq_{0:09d}'.format(i) for i in range(n_eq)] + \
+            ['ineq_{0:09d}'.format(i) for i in range(n_ineq)] 
+
+        self.prob_.loadproblem (probname='CVXPY_xpress_qp',
+                                qrtypes=['E']*n_eq + ['L']*n_ineq,
+                                rhs=b + g,
+                                range=None,
+                                obj=q,
+                                mstart=mstart,
+                                mnel=None,
+                                # linear coefficients
+                                mrwind=mrwind,
+                                dmatval=dmatval,
+                                # variable bounds
+                                dlb=[-xp.infinity]*n_var,
+                                dub=[xp.infinity]*nvar,
+                                # quadratic objective (only upper triangle)
+                                mqcol1=mqcol1,
+                                mqcol2=mqcol2,
+                                dqe=dqe,
+                                # binary and integer variables
+                                qgtype=['B']*len(data[s.BOOL_IDX]) + ['I']*len(data[s.INT_IDX]),
+                                mgcols=data[s.BOOL_IDX] + data[s.INT_IDX],
+                                # variables' and constraints' names
+                                colnames=colnames,
+                                rownames=rownames)
 
         # Set verbosity
-        #if not verbose:
-        #    hide_solver_output(model)
-
-        # Set parameters
-        # set_parameters(model, solver_opts)
+        if not verbose:
+            self.prob_.controls.outputlog = 0
 
         # Solve problem
         results_dict = {}
         try:
-            start = model.get_time()
-            model.solve()
-            end = model.get_time()
-            results_dict["cputime"] = end - start
+            self.prob_.solve()
+            results_dict["cputime"] = self.prob_.attributes.time
         except Exception:  # Error in the solution
             results_dict["status"] = s.SOLVER_ERROR
 
-        results_dict["model"] = model
+        results_dict["model"] = self.prob_
 
         return results_dict
