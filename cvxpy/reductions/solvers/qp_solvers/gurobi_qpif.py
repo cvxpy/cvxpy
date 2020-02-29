ff037b5020db80c4b8b71d1642de271de6160b38
Robert Luce <luce@gurobi.com>
2020-02-29 11:09:03 +0100

    Workaround for a bug in gurobipy 8.1.1 (#900)

--- cvxpy/reductions/solvers/qp_solvers/gurobi_qpif.py
+++ cvxpy/reductions/solvers/qp_solvers/gurobi_qpif.py
@@ -128,6 +128,7 @@ class GUROBI(QpSolver):
             elif hasattr(model, '_v811_addMConstrs'):
                 # We can pass all of A @ x == b at once, API only for Gurobi
                 # v811
+                A.eliminate_zeros() # Work around bug in gurobipy v811
                 sense = np.repeat(grb.GRB.EQUAL, A.shape[0])
                 model._v811_addMConstrs(A, sense, b)
             else:
@@ -150,6 +151,7 @@ class GUROBI(QpSolver):
             elif hasattr(model, '_v811_addMConstrs'):
                 # We can pass all of F @ x <= g at once, API only for Gurobi
                 # v811.
+                F.eliminate_zeros() # Work around bug in gurobipy v811
                 sense = np.repeat(grb.GRB.LESS_EQUAL, F.shape[0])
                 model._v811_addMConstrs(F, sense, g)
             else:
