0ebce88adb5b129cfc64194c5587ce658a3007b3
Steven Diamond <stevend2@stanford.edu>
2020-04-20 17:57:35 -0700

    oopsie

--- test.py
+++
@@ -1,9 +0,0 @@
-import cvxpy as c
-
-x = [[4144.30127531]]
-y = [[7202.52114311]]
-z = c.Variable(shape=(1, 1))
-objective = c.Minimize(c.quad_form(z, x) - 2 * z.T @ y) 
-
-prob = c.Problem(objective)
-prob.solve('OSQP')
