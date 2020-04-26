cfed6e2250101097e1b7b44a161ded211d2e9c84
Akshay <akshaykagrawal7@gmail.com>
2020-04-26 11:56:44 -0700

    Generated gh-pages for commit 3e3ccbcf Author: Akshay <akshaykagrawal7@gmail.com>
        Version to 1.1.0a4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,9 +1,8 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.0a3',
+    VERSION: '1.1.0a4',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
