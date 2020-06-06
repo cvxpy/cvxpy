f234ad422e9c35d35b8397ea37b4d4375fb9fa92
Albert Shieh <adshieh@gmail.com>
2020-06-06 10:25:23 -0400

    Remove default eps override

--- cvxpy/reductions/solvers/conic_solvers/scs_conif.py
+++ cvxpy/reductions/solvers/conic_solvers/scs_conif.py
@@ -300,8 +300,6 @@ class SCS(ConicSolver):
             args["y"] = solver_cache[self.name()]["y"]
             args["s"] = solver_cache[self.name()]["s"]
         cones = dims_to_solver_dict(data[ConicSolver.DIMS])
-        # Default to eps = 1e-4 instead of 1e-3.
-        solver_opts["eps"] = solver_opts.get("eps", 1e-4)
 
         results = scs.solve(args, cones, verbose=verbose, **solver_opts)
         status = self.STATUS_MAP[results["info"]["status"]]
