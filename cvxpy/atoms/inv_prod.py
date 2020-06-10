131904abe7c77ad61b3f114288524dec38f1d96a
Guillermo Angeris <angeris@stanford.edu>
2020-06-10 11:57:30 -0700

    Adding `inv_prod` and fixing numeric evaluation of `geo_mean`

---
+++ cvxpy/atoms/inv_prod.py
@@ -0,0 +1,10 @@
+
+from cvxpy.expressions.expression import Expression
+from cvxpy.atoms.geo_mean import geo_mean
+from cvxpy.atoms.elementwise.power import power
+from cvxpy.atoms.elementwise.inv_pos import inv_pos
+
+def inv_prod(value):
+    """The reciprocal of a product of the entries of a vector ``x``.
+    """
+    return power(inv_pos(geo_mean(value)), len(value))
