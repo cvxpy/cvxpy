cb74cae2a5fa89504b9a1643acad0deeb9b55fc7
Robert Luce <luce@gurobi.com>
2020-06-12 12:01:51 -0700

    Fetch duals in bulk

--- cvxpy/reductions/solvers/conic_solvers/gurobi_conif.py
+++ cvxpy/reductions/solvers/conic_solvers/gurobi_conif.py
@@ -212,9 +212,6 @@ class GUROBI(SCS):
             variables += new_vars
             soc_start += constr_len
 
-        gur_constrs = eq_constrs + ineq_constrs + new_leq_constrs + soc_constrs
-        model.update()
-
         # Set parameters
         # TODO user option to not compute duals.
         model.setParam("QCPDual", True)
@@ -234,16 +231,11 @@ class GUROBI(SCS):
             # Only add duals if not a MIP.
             # Not sure why we need to negate the following,
             # but need to in order to be consistent with other solvers.
+            vals = []
             if not (data[s.BOOL_IDX] or data[s.INT_IDX]):
-                vals = []
-                for lc in gur_constrs:
-                    if lc is not None:
-                        if isinstance(lc, gurobipy.QConstr):
-                            vals.append(lc.QCPi)
-                        else:
-                            vals.append(lc.Pi)
-                    else:
-                        vals.append(0)
+                lin_constrs = eq_constrs + ineq_constrs + new_leq_constrs
+                vals += model.getAttr('Pi', lin_constrs)
+                vals += model.getAttr('QCPi', soc_constrs)
                 solution["y"] = -np.array(vals)
                 solution[s.EQ_DUAL] = solution["y"][0:dims[s.EQ_DIM]]
                 solution[s.INEQ_DUAL] = solution["y"][dims[s.EQ_DIM]:]
