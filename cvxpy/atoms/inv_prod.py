bf8ddf45d04253978a6bd45663751f3ed991c7ba
Guillermo Angeris <angeris@stanford.edu>
2020-07-15 09:16:29 -0700

    adding documentation

--- cvxpy/atoms/inv_prod.py
+++ cvxpy/atoms/inv_prod.py
@@ -20,5 +20,19 @@ from cvxpy.atoms.elementwise.inv_pos import inv_pos
 
 def inv_prod(value):
     """The reciprocal of a product of the entries of a vector ``x``.
+
+    Parameters
+    ----------
+    x : Expression or numeric
+        The expression whose reciprocal product is to be computed. Must have
+        positive entries.
+
+    Returns
+    -------
+    Expression
+        .. math::
+            \\left(\\prod_{i=1}^n x_i\\right)^{-1},
+
+        where :math:`n` is the length of :math:`x`.
     """
     return power(inv_pos(geo_mean(value)), sum(value.shape))
