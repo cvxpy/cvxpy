c19547dcd38b9aaf8da599338203223896f039b4
Tomas Krejci <tomas@krej.ci>
2020-11-01 21:12:57 +0100

    f-strings

--- cvxpy/utilities/cplex.py
+++ cvxpy/utilities/cplex.py
@@ -1,7 +1,7 @@
-from cvxpy.settings import CPLEX
+from cvxpy.settings import CPLEX, INFEASIBLE
 
 
-def _refine_conflicts_if_infeasible(soln):
+def _refine_conflicts_if_infeasible(problem, soln):
     """
     Run Cplex's conflict refiner if the solution is infeasible and print the results.
 
@@ -10,11 +10,7 @@ def _refine_conflicts_if_infeasible(soln):
         If you know how to use it properly, feel free to iterate on this.
     """
     c = soln['model']
-    solution_status = c.solution.status[c.solution.get_status()]
-    infeasible_statuses = [
-        'infeasible', 'MIP_infeasible', 'infeasible_or_unbounded', 'optimal_infeasible'
-    ]
-    if solution_status not in infeasible_statuses:
+    if problem.status != INFEASIBLE:
         return
 
     print("Starting solution refiner.")
@@ -25,13 +21,13 @@ def _refine_conflicts_if_infeasible(soln):
         try:
             return c.variables.get_names(idx)
         except:
-            return f"x{idx}"
+            return "x" + str(idx)
 
     def linear_constraint_name(idx):
         try:
             return c.linear_constraints.get_names(idx)
         except:
-            return f"c{idx}"
+            return "c" + str(idx)
 
     conflict_groups = c.conflict.get_groups()
     group_status_inds = c.conflict.get()
@@ -40,7 +36,7 @@ def _refine_conflicts_if_infeasible(soln):
         group_status = c.conflict.group_status[group_status_ind]
         if group_status == 'excluded':
             continue
-        print(f"CONFLICT GROUP: {group_status}")
+        print("CONFLICT GROUP: {}".format(group_status))
         for constr_type, constr_id in constrs:
             if constr_type == c.conflict.constraint_type.linear:
                 constraint_name = linear_constraint_name(constr_id)
@@ -48,18 +44,23 @@ def _refine_conflicts_if_infeasible(soln):
                 rhs = c.linear_constraints.get_rhs(constr_id)
                 sense = c.linear_constraints.get_senses(constr_id)
                 sense_dict = {'E': '=', 'G': '≥', 'L': '≤'}
-                lhs_str = " + ".join([f"{val}*{var_name(ind)}" for ind, val in zip(lhs.ind, lhs.val)])
-                print(f"Linear constraint: {constraint_name}:\n    {lhs_str} {sense_dict[sense]} {rhs}")
+                lhs_str = " + ".join([
+                    str(val) + "*" + str(var_name(ind)) 
+                    for ind, val in zip(lhs.ind, lhs.val)
+                ])
+                print(
+                    "Linear constraint: {}:\n    {} {} {}".format(constraint_name, lhs_str, sense_dict[sense], rhs)
+                )
             elif constr_type == c.conflict.constraint_type.upper_bound:
                 ub = c.variables.get_upper_bounds(constr_id)
                 var_name = var_name(constr_id)
-                print(f"Upper bound:\n    {var_name} ≤ {ub}")
+                print("Upper bound:\n    {} ≤ {}".format(var_name, ub))
             elif constr_type == c.conflict.constraint_type.lower_bound:
                 lb = c.variables.get_lower_bounds(constr_id)
                 var_name = var_name(constr_id)
-                print(f"Lower bound:\n    {var_name} ≥ {lb}")
+                print("Lower bound:\n    {} ≥ {}".format(var_name, lb))
             else:
-                raise Exception(f"Unknown constraint type {constr_type}.")
+                raise Exception("Unknown constraint type {}.".format(constr_type))
         print()
 
 
@@ -89,4 +90,4 @@ def solve_with_conflict_refiner(problem, warm_start=False, verbose=False, solver
     data, chain, inverse_data = problem.get_problem_data(solver=CPLEX, gp=gp, enforce_dpp=enforce_dpp)
     soln = chain.solve_via_data(problem, data, warm_start, verbose, solver_opts)
     problem.unpack_results(soln, chain, inverse_data)
-    _refine_conflicts_if_infeasible(soln)
+    _refine_conflicts_if_infeasible(problem, soln)
