628437fce124259cb2c53b73e9e712eda4130703
rileyjmurray <rjmurray@berkeley.edu>
2020-12-26 12:35:49 -0500

    remove unnecessary if statement

--- continuous_integration/Travis/install_dependencies.sh
+++ continuous_integration/Travis/install_dependencies.sh
@@ -6,51 +6,49 @@
 
 set -e
 
-if [[ "$DISTRIB" == "conda" ]]; then
-    # Use miniconda
-    if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-        sudo apt-get update -qq
-        sudo apt-get install -qq gfortran libgfortran3
-        LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgfortran.so.3
-        wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-           -O miniconda.sh
-        chmod +x miniconda.sh && ./miniconda.sh -b
-        export PATH=/home/travis/miniconda3/bin:$PATH
-        conda update --yes conda
-        # Configure the conda environment and put it in the path using the
-        # provided versions
-        conda create -n testenv --yes python=$PYTHON_VERSION mkl pip pytest \
-                numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION
-        source activate testenv
-        PIN_FILE=/home/travis/miniconda3/envs/testenv/conda-meta/pinned
-        touch $PIN_FILE
-        echo "python=$PYTHON_VERSION" >> $PIN_FILE
-        conda install -c conda-forge --yes lapack
-        conda install -c conda-forge --yes ecos scs
-        conda install -c oxfordcontrol --yes osqp
-        conda install -c default --yes flake8
-        pip install diffcp
-        pip install cvxopt
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    sudo apt-get update -qq
+    sudo apt-get install -qq gfortran libgfortran3
+    LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgfortran.so.3
+    wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+       -O miniconda.sh
+    chmod +x miniconda.sh && ./miniconda.sh -b
+    export PATH=/home/travis/miniconda3/bin:$PATH
+    conda update --yes conda
+    # Configure the conda environment and put it in the path using the
+    # provided versions
+    conda create -n testenv --yes python=$PYTHON_VERSION mkl pip pytest \
+            numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION
+    source activate testenv
+    PIN_FILE=/home/travis/miniconda3/envs/testenv/conda-meta/pinned
+    touch $PIN_FILE
+    echo "python=$PYTHON_VERSION" >> $PIN_FILE
+    conda install -c conda-forge --yes lapack
+    conda install -c conda-forge --yes ecos scs
+    conda install -c oxfordcontrol --yes osqp
+    conda install -c default --yes flake8
+    pip install diffcp
+    pip install cvxopt
 
-    elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-        wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh \
-             -O miniconda.sh
-        chmod +x miniconda.sh && ./miniconda.sh -b
-        export PATH=/Users/travis/miniconda3/bin:$PATH
-        conda update --yes conda
-        # Configure the conda environment and put it in the path using the
-        # provided versions
-        conda create -n testenv --yes python=$PYTHON_VERSION mkl pip pytest \
-              numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION
-        source activate testenv
-        PIN_FILE=/Users/travis/miniconda3/envs/testenv/conda-meta/pinned
-        touch $PIN_FILE
-        echo "python=$PYTHON_VERSION.*" >> $PIN_FILE
-        conda install -c conda-forge --yes ecos scs
-        conda install -c oxfordcontrol --yes osqp
-        conda install -c default --yes flake8=3.5.0
-        pip install diffcp
-    fi
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh \
+         -O miniconda.sh
+    chmod +x miniconda.sh && ./miniconda.sh -b
+    export PATH=/Users/travis/miniconda3/bin:$PATH
+    conda update --yes conda
+    # Configure the conda environment and put it in the path using the
+    # provided versions
+    conda create -n testenv --yes python=$PYTHON_VERSION mkl pip pytest \
+          numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION
+    source activate testenv
+    PIN_FILE=/Users/travis/miniconda3/envs/testenv/conda-meta/pinned
+    touch $PIN_FILE
+    echo "python=$PYTHON_VERSION.*" >> $PIN_FILE
+    conda install -c conda-forge --yes ecos scs
+    conda install -c oxfordcontrol --yes osqp
+    conda install -c default --yes flake8=3.5.0
+    pip install diffcp
+fi
 
 if [[ "$COVERAGE" == "true" ]]; then
     pip install coverage coveralls
