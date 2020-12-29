0fd9745471bbee8a3b1370c79dcccb0d1e16a4b6
rileyjmurray <rjmurray@berkeley.edu>
2020-12-29 08:58:43 -0500

    travis, fix typo

--- continuous_integration/Travis/install_dependencies.sh
+++ continuous_integration/Travis/install_dependencies.sh
@@ -28,20 +28,20 @@ conda config --add channels anaconda
 if [[ "$PYTHON_VERSION" == "3.7" ]]; then
   conda create -n testenv --yes python=3.7 mkl pip pytest \
    scipy=1.1 numpy=1.15 lapack ecos scs osqp flake8 cvxopt
-  conda activate testenv
+  source activate testenv
 elif [[ "$PYTHON_VERSION" == "3.8" ]]; then
   # There is a config that works with numpy 1.14, but not 1.15!
   # So we fix things at 1.16.
   # Assuming we use numpy 1.16, the earliest version of scipy we can use is 1.3.
   conda create -n testenv --yes python=3.8 mkl pip pytest \
    scipy=1.3 numpy=1.16 lapack ecos scs osqp flake8 cvxopt
-  conda activate testenv
+  source activate testenv
 elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
   # The earliest version of numpy that works is 1.19.
   # Given numpy 1.19, the earliest version of scipy we can use is 1.5.
   conda create -n testenv --yes python=3.9 mkl pip pytest \
    scipy=1.5 numpy=1.19 lapack ecos scs flake8 cvxopt
-  conda activate testenv
+  source activate testenv
   pip install osqp
 fi
 
