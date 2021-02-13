1327e0129ed65fcf42155fec48dc26d31a3f458f
Akshay Agrawal <akshayka@cs.stanford.edu>
2021-02-12 16:35:25 -0800

    Don't reset warnings filters
    Libraries should change warning filters, or reset them.
    
    Additionally, change the UserWarning for using * to a
    DeprecationWarning, and modify the stacklevel to point to the calling
    code (so users can see where the deprecated code is being used).

--- cvxpy/expressions/expression.py
+++ cvxpy/expressions/expression.py
@@ -546,9 +546,8 @@ class Expression(u.Canonical):
                     warnings.warn("Forming a nonconvex expression.")
             # Because we want to discourage using ``*`` to call matmul, we
             # raise a warning to the user.
-            warnings.resetwarnings()
-            warnings.warn(__STAR_MATMUL_WARNING__, UserWarning)
-            warnings.resetwarnings()
+            warnings.warn(
+                    __STAR_MATMUL_WARNING__, DeprecationWarning, stacklevel=2)
             return cvxtypes.matmul_expr()(self, other)
 
     @_cast_other
