ebb7bba8606fd9316888cab4278fce44f08af1c0
Akshay Agrawal <akshayka@cs.stanford.edu>
2021-02-28 09:22:31 -0800

    -std=c++11

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ canon = Extension(
     include_dirs=['cvxpy/cvxcore/src/',
                   'cvxpy/cvxcore/python/',
                   'cvxpy/cvxcore/include/'],
-    extra_compile_args=['-O3'],
+    extra_compile_args=['-O3', '-std=c++11'],
     extra_link_args=['-O3'],
 )
 
