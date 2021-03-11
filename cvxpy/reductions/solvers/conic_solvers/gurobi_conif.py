966a6d92650c88b3bb643ff5b286337192f13a82
Alexandra Shamraeva <62992014+ashamraeva@users.noreply.github.com>
2021-03-11 22:11:27 +0100

    remove obsolete return

--- cvxpy/reductions/solvers/conic_solvers/gurobi_conif.py
+++ cvxpy/reductions/solvers/conic_solvers/gurobi_conif.py
@@ -126,8 +126,6 @@ class GUROBI(SCS):
         else:
             return failure_solution(status, attr)
 
-        return Solution(status, opt_val, primal_vars, dual_vars, attr)
-
     def solve_via_data(self, data, warm_start, verbose, solver_opts, solver_cache=None):
         """Returns the result of the call to the solver.
 
