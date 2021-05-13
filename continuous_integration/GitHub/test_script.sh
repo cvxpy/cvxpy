ddb54995d0a73a36f9928150c067ef97e659e7f3
phschiele <44360364+phschiele@users.noreply.github.com>
2021-05-13 20:23:24 +0200

    Uppercase linux in test_script (#2)

--- continuous_integration/GitHub/test_script.sh
+++ continuous_integration/GitHub/test_script.sh
@@ -9,7 +9,7 @@ python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
 
-if [ $USE_OPENMP == "True" ] && [ $RUNNER_OS == "linux" ]; then
+if [ $USE_OPENMP == "True" ] && [ $RUNNER_OS == "Linux" ]; then
     CFLAGS="-fopenmp" LDFLAGS="-lgomp" python setup.py install
     export OMP_NUM_THREADS=4
 else
