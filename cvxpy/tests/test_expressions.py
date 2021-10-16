1dfa803f25e928ab121c18b1f93c6335a09865a0
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2021-10-17 03:33:44 +0530

    Refactor deprecated unittest aliases for Python 3.11 compatibility. (#1500)

--- cvxpy/tests/test_expressions.py
+++ cvxpy/tests/test_expressions.py
@@ -743,7 +743,7 @@ class TestExpressions(BaseTest):
         with self.assertRaises(Exception) as cm:
             (self.x/[2, 2, 3])
         print(cm.exception)
-        self.assertRegexpMatches(str(cm.exception),
+        self.assertRegex(str(cm.exception),
                                  "Incompatible shapes for division.*")
 
         c = Constant([3.0, 4.0, 12.0])
@@ -784,7 +784,7 @@ class TestExpressions(BaseTest):
         with self.assertRaises(Exception) as cm:
             (x/c[:, 0])
         print(cm.exception)
-        self.assertRegexpMatches(str(cm.exception),
+        self.assertRegex(str(cm.exception),
                                  "Incompatible shapes for division.*")
 
     # Test the NegExpression class.
