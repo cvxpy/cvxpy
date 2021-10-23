8a56dad45fcbccdd610e2c2b3db3db6c13171f9d
phschiele <phschiele@gmail.com>
2021-10-23 23:04:30 +0200

    Set in install_dependencies.sh

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -5,6 +5,11 @@
 
 set -e
 
+conda config --set remote_connect_timeout_secs 30.0
+conda config --set remote_max_retries 10
+conda config --set remote_backoff_factor 2
+conda config --set remote_read_timeout_secs 12ß.0
+
 conda info
 conda list
 conda config --show-sources
