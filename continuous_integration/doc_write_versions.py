396beaf28a04b84312dce366d942c59fd5511bba
phschiele <phschiele@gmail.com>
2022-01-30 15:22:06 +0100

    allow for no current deployed versions

--- continuous_integration/doc_write_versions.py
+++ continuous_integration/doc_write_versions.py
@@ -3,7 +3,15 @@ import ast
 import json
 import os
 
-versions = reversed(sorted(version.parse(v) for v in ast.literal_eval(os.environ["ALL_DEPLOYED_VERSIONS"])))
+if os.environ["ALL_DEPLOYED_VERSIONS"]:
+    deployed_versions = [version.parse(v) for v in ast.literal_eval(os.environ["ALL_DEPLOYED_VERSIONS"])]
+else:
+    deployed_versions = []
+this_version = version.parse(os.environ["CURRENT_VERSION"])
+
+all_versions = {this_version} | set(deployed_versions)
+
+versions = reversed(sorted(all_versions))
 version_str = [str(v) for v in versions]
 
 with open("versions.json", "w") as f:
