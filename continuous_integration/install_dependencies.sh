07dbd00bb4b89d1468b2055dffed2fb662b38cce
phschiele <44360364+phschiele@users.noreply.github.com>
2022-02-01 13:19:22 +0100

    Local PR (#28)
    * use quotes
    
    * Specify exactly
    
    * include cplex on 3.9
    
    * Add comment

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -11,16 +11,16 @@ conda config --set remote_backoff_factor 2
 conda config --set remote_read_timeout_secs 120.0
 
 if [[ "$PYTHON_VERSION" == "3.6" ]]; then
-  conda install dataclasses
-fi
-
-if [[ "$PYTHON_VERSION" == "3.6" ]] || [[ "$PYTHON_VERSION" == "3.7" ]] || [[ "$PYTHON_VERSION" == "3.8" ]]; then
+  conda install scipy=1.3 numpy=1.16 mkl pip=21.3.1 pytest pytest-cov lapack ecos scs osqp cvxopt
+  python -m pip install cplex  # CPLEX is not available yet on 3.10
+elif [[ "$PYTHON_VERSION" == "3.7" ]] || [[ "$PYTHON_VERSION" == "3.8" ]]; then
   conda install scipy=1.3 numpy=1.16 mkl pip pytest pytest-cov lapack ecos scs osqp cvxopt
-  python -m pip install cplex  # CPLEX is not available yet on 3.9
+  python -m pip install cplex  # CPLEX is not available yet on 3.10
 elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
   # The earliest version of numpy that works is 1.19.
   # Given numpy 1.19, the earliest version of scipy we can use is 1.5.
   conda install scipy=1.5 numpy=1.19 mkl pip pytest lapack ecos scs osqp cvxopt
+  python -m pip install cplex  # CPLEX is not available yet on 3.10
 elif [[ "$PYTHON_VERSION" == "3.10" ]]; then
     # The earliest version of numpy that works is 1.21.
     # Given numpy 1.21, the earliest version of scipy we can use is 1.7.
@@ -38,7 +38,7 @@ fi
 
 # SCIP only works with scipy >= 1.5 due to dependency conflicts when installing on Linux/macOS
 if [[ "$PYTHON_VERSION" == "3.9" ]] || [[ "$RUNNER_OS" == "Windows" ]]; then
-  conda install pyscipopt<4.0.0
+  conda install pyscipopt"<4.0"  # TODO: update interface https://github.com/cvxpy/cvxpy/pull/1628
 fi
 
 if [[ "$PYTHON_VERSION" == "3.10" ]]; then
