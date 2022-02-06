dd8171bd534df8eeadb0e0df20e2c39aebbdb9ee
parthb83 <parthbansal1536@gmail.com>
2022-02-06 16:40:39 +0530

    Added new atom function xexp()

---
+++ cvxpy/atoms/elementwise/xexp.py
@@ -0,0 +1,86 @@
+"""
+Copyright 2013 Steven Diamond, Eric Chu
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from typing import Tuple
+
+import numpy as np
+
+from cvxpy.atoms.elementwise.elementwise import Elementwise
+
+
+class xexp(Elementwise):
+    """Elementwise :math:`{x}*e^{x}`.
+    """
+
+    def __init__(self, x) -> None:
+        super(xexp, self).__init__(x)
+
+    # Returns the matrix x[i, j]*e^x[i, j].
+    @Elementwise.numpy_numeric
+    def numeric(self, values):
+        return values[0] * np.exp(values[0])
+
+    def sign_from_args(self) -> Tuple[bool, bool]:
+        """Returns sign (is positive, is negative) of the expression.
+        """
+        # Always positive.
+        return (True, False)
+
+    def is_atom_convex(self) -> bool:
+        """Is the atom convex?
+        """
+        return True
+
+    def is_atom_concave(self) -> bool:
+        """Is the atom concave?
+        """
+        return False
+
+    def is_atom_log_log_convex(self) -> bool:
+        """Is the atom log-log convex?
+        """
+        return True
+
+    def is_atom_log_log_concave(self) -> bool:
+        """Is the atom log-log concave?
+        """
+        return False
+
+    def is_incr(self, idx) -> bool:
+        """Is the composition non-decreasing in argument idx?
+        """
+        return True
+
+    def is_decr(self, idx) -> bool:
+        """Is the composition non-increasing in argument idx?
+        """
+        return False
+
+    def _grad(self, values):
+        """Gives the (sub/super)gradient of the atom w.r.t. each argument.
+
+        Matrix expressions are vectorized, so the gradient is a matrix.
+
+        Args:
+            values: A list of numeric values for the arguments.
+
+        Returns:
+            A list of SciPy CSC sparse matrices or None.
+        """
+        rows = self.args[0].size
+        cols = self.size
+        grad_vals = np.exp(values[0])
+        return [xexp.elemwise_grad_to_diag(grad_vals, rows, cols)]
