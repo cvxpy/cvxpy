886b8b42cea77dfa59e9f09856b2e951c3c9df10
rileyjmurray <rjmurray@berkeley.edu>
2022-02-12 17:53:57 -0800

    clearer example

--- kron_debug_scripts/explore_kron_canon.py
+++ kron_debug_scripts/explore_kron_canon.py
@@ -10,7 +10,36 @@ kron(M, N) = [M[0,0] * N   , ..., M[0, end] * N  ]
 """
 
 
+def print_array_indented(mat, indent_level=1):
+    tab_str = indent_level * '\t'
+    mat_str = str(mat)
+    mat_str = tab_str + mat_str
+    mat_str = mat_str.replace('\n ', f'\n{tab_str} ')
+    print(mat_str)
+
+
 def random_problem(z_dims, c_dims, var_left, param, seed=0):
+    """
+    Construct random nonnegative matrices (C, L) of shapes
+    (c_dims, z_dims) respectively. Define an optimization
+    problem with a matrix variable of shape z_dims:
+
+        min sum(Z)
+        s.t.  kron(Z, C) >= kron(L, C)   ---   if var_left is True
+              kron(C, Z) >= kron(C, L)   ---   if var_left is False
+              Z >= 0
+
+    Regardless of whether var_left is True or False, the optimal
+    solution to that problem is Z = L.
+
+    If param is True, then C is defined as a CVXPY Parameter.
+    If param is False, then C is a CVXPY Constant.
+
+    A small remark: the constraint that Z >= 0 is redundant.
+    It's there because it's easier to set break points that distinguish
+    objective canonicalization and constraint canonicalization
+    when there's more than one constraint.
+    """
     np.random.seed(seed)
     _C_value = np.random.rand(*c_dims).round(decimals=2)
     if param:
@@ -24,20 +53,39 @@ def random_problem(z_dims, c_dims, var_left, param, seed=0):
         _constraints = [cp.kron(_Z, _C) >= cp.kron(_L, _C), _Z >= 0]
     else:
         _constraints = [cp.kron(_C, _Z) >= cp.kron(_C, _L), _Z >= 0]
-    # ^ Only the first constraint matters. We use two constraints because that
-    # makes it easier to set conditional breakpoints in canonInterface.py.
-    #
-    #   Specifically, canonInterface.py:get_problem_matrix is called once for
-    #   the objective function and once for the constraint matrix. When it's
-    #   called for the objective function the linOps argument is a list of length
-    #   one, and when it's called for the constraint matrix the linOps argument
-    #   is a list of length equal to the number of constraints.
-    #
     _obj_expr = cp.sum(_Z)
-
     _prob = cp.Problem(cp.Minimize(_obj_expr), _constraints)
-    # The optimal solution is Z.value == L.
     return _Z, _C, _L, _prob
+
+
+def run_example(z_dims, c_dims, var_left=True, param=True, solve=True, seed=0):
+    print('\nContext for this run ...')
+    print(f'\tshape of Variable  Z: {z_dims}')
+    c_desc = 'Parameter' if param else 'Constant '
+    print(f'\tshape of {c_desc} C: {c_dims}')
+    side = 'LEFT' if var_left else 'RIGHT'
+    when = 'NEW' if var_left else 'OLD'
+    print(f'\tZ is the {side} operand to kron; this is {when} functionality.')
+    Z, C, L, prob = random_problem(z_dims, c_dims, var_left, param, seed=seed)
+
+    if solve:
+        prob.solve(solver='ECOS')
+        print('\nSolving with ECOS ...')
+        violations = prob.constraints[0].violation()
+        print(f'\tProblem status: {prob.status}')
+        print(f'\tZ.value = ...\n')
+        print_array_indented(Z.value, indent_level=2)
+        print(f'\n\tConstraint violation: {np.max(violations)}')
+
+    data = prob.get_problem_data(solver='ECOS', enforce_dpp=True)[0]
+    # ^ Changing to enforce_dpp=False doesn't make a difference
+    G = data['G'].A
+    h = data['h']
+    print('\nThe matrix G in ECOS data "G x <= h" is \n')
+    print_array_indented(G, indent_level=1)
+    print('\nThe vector h in ECOS data "G x <= h" is \n')
+    print_array_indented(h, indent_level=1)
+    return Z, C, L, prob, G, h
 
 
 if __name__ == '__main__':
@@ -48,58 +96,15 @@ if __name__ == '__main__':
     You can find a set of screenshots at:
     https://docs.google.com/presentation/d/1ETKlAkz1XrSfikvnF27T-JGYF5niLoA936fOSduvy6U/edit?usp=sharing
     feel free to edit by adding more slides.
-    As of Feb 11, the relevant slides start on page 18.
     """
     seed = 0
-    z_dims = (2, 2)
+    z_dims = (1, 1)
     c_dims = (1, 2)
 
-    solve = False  # I haven't found much value in setting this to True
+    solve = True
     var_left = False  # True means we test the new functionality, with a Variable in left argument
     param = True  # tests pass when param=False. But maybe worth looking at ...
-    #   (param, LEFT) = (False, False) shows what kron(const, var) canonicalizes to w/o parameters
-    #   (param, LEFT) = (False, True) shows what kron(var, const) canonicalizes to w/o parameters
+    #   (param, var_left) = (False, False) shows what kron(const, var) canonicalizes to w/o parameters
+    #   (param, var_left) = (False, True) shows what kron(var, const) canonicalizes to w/o parameters
 
-    Z, C, L, prob = random_problem(z_dims, c_dims, var_left, param, seed=seed)
-    if solve:
-        prob.solve(solver='ECOS')
-    else:
-        print('\nDimensions')
-        print(f'\tz_dims = {z_dims}')
-        print(f'\tc_dims = {c_dims}')
-        data = prob.get_problem_data(solver='ECOS', enforce_dpp=True)[0]
-        G = data['G'].A
-        h = data['h']
-        print(f'\nData for param={param} and var_left={var_left}')
-        print(f'\tnnz(G) = {np.count_nonzero(G)}')
-        print(f'\tnnz(h) = {np.count_nonzero(h)}\n')
-        print(G)
-        print(h)
-        print()
-
-        data = prob.get_problem_data(solver='ECOS', enforce_dpp=True)[0]
-        G = data['G'].A
-        h = data['h']
-        print(f'\nData for param={param} and var_left={var_left}')
-        print(f'\tnnz(G) = {np.count_nonzero(G)}')
-        print(f'\tnnz(h) = {np.count_nonzero(h)}\n')
-        print(G)
-        print(h)
-        print()
-
-    Z, C, L, prob = random_problem(z_dims, c_dims, var_left, param, seed=seed)
-    if solve:
-        prob.solve(solver='ECOS')
-    else:
-        print('\nDimensions')
-        print(f'\tz_dims = {z_dims}')
-        print(f'\tc_dims = {c_dims}')
-        data = prob.get_problem_data(solver='ECOS', enforce_dpp=True)[0]
-        G = data['G'].A
-        h = data['h']
-        print(f'\nData for param={param} and var_left={var_left}')
-        print(f'\tnnz(G) = {np.count_nonzero(G)}')
-        print(f'\tnnz(h) = {np.count_nonzero(h)}\n')
-        print(G)
-        print(h)
-        print()
+    Z, C, L, prob, G, h = run_example(z_dims, c_dims, var_left, param, solve, seed)
