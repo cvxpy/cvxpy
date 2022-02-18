f559359082e856ac8e2fc13e30facc33d7b50b12
Xinyue Shen <xinyueshen@xinyues-mbp.lan>
2022-02-17 21:19:28 -0800

    remove whitespace in blank lines

--- cvxpy/tests/test_grad.py
+++ cvxpy/tests/test_grad.py
@@ -97,12 +97,12 @@ class TestGrad(BaseTest):
         self.A.value = np.array([[3, -4], [4, 3]])
         self.assertItemsAlmostEqual(expr.grad[self.A].toarray(),
                                     np.array([[0.6, 0], [0, 0.8], [-0.8, 0], [0, 0.6]]))
-        
+
         expr = cp.pnorm(self.A, 2, axis=1)
         self.A.value = np.array([[0, 0], [10, 0]])
         self.assertItemsAlmostEqual(expr.grad[self.A].toarray(),
                                     np.array([[0, 0], [0, 1], [0, 0], [0, 0]]))
-        
+
         expr = cp.pnorm(self.A, 1, axis=1)
         self.A.value = np.array([[0, 0], [10, 0]])
         self.assertItemsAlmostEqual(expr.grad[self.A].toarray(),
