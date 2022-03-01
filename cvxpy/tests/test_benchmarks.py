5a79db98c256e09159c8a338cf8ebb203e6950d2
Hannah McLaughlin <h@mcla.ug>
2022-03-01 23:11:00 +0000

    Remove pruning added by 9672acf (#1677)
    * Add benchmark test for #1668, takes ~8.4 seconds
    
    * Remove pruning added by 9672acf
    
    See discussion on https://github.com/cvxpy/cvxpy/issues/1668. The
    pruning is causing order of magnitude slow-downs on some problems.
    
    Benchmark test now takes ~0.7 seconds instead of ~8.4

--- cvxpy/tests/test_benchmarks.py
+++ cvxpy/tests/test_benchmarks.py
@@ -274,3 +274,28 @@ class TestBenchmarks(BaseTest):
         print('Quadratic canonicalization')
         print('(OSQP) solver time: ', prob.solver_stats.solve_time)
         print('cvxpy time: ', (end - start) - prob.solver_stats.solve_time)
+
+    def test_issue_1668_slow_pruning(self) -> None:
+        """Regression test for https://github.com/cvxpy/cvxpy/issues/1668
+
+        Pruning matrices caused order-of-magnitude slow downs in compilation times.
+        """
+        s = 2000
+        t = 10
+        x = np.linspace(-100.0, 100.0, s)
+        rows = 50
+        var = cp.Variable(shape=(rows, t))
+
+        cost = cp.sum_squares(
+            var @ np.tile(np.array([x]), t).reshape((t, x.shape[0]))
+            - np.tile(x, rows).reshape((rows, s))
+        )
+        objective = cp.Minimize(cost)
+        problem = cp.Problem(objective)
+
+        start = time.time()
+        problem.get_problem_data(cp.ECOS, verbose=True)
+        end = time.time()
+
+        print("Issue #1668 regression test")
+        print("Compilation time: ", end - start)
