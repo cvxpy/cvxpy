55c2ddeeccb6e422f40e8e132dd4da9ea89bd3ce
Parth Bansal <parthbansal1536@gmail.com>
2022-03-06 00:31:38 +0530

    Fix cummmax (#1683)

--- cvxpy/reductions/eliminate_pwl/atom_canonicalizers/cumsum_canon.py
+++ cvxpy/reductions/eliminate_pwl/atom_canonicalizers/cumsum_canon.py
@@ -27,12 +27,12 @@ def cumsum_canon(expr, args):
     Y = Variable(expr.shape)
     if axis == 0:
         if (expr.shape[0] == 1):
-            return (X, [])
+            return X, []
         else:
             constr = [X[1:] == Y[1:] - Y[:-1], Y[0] == X[0]]
     else:
         if (expr.shape[1] == 1):
-            return (X, [])
+            return X, []
         else:
             constr = [X[:, 1:] == Y[:, 1:] - Y[:, :-1], Y[:, 0] == X[:, 0]]
-    return (Y, constr)
+    return Y, constr
