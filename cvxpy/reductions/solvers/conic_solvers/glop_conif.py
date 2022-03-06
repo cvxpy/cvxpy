b793066dfd7b6881d5cc28c7112f7bddb19d50c5
Miles Lubin <mlubin@google.com>
2022-03-05 22:17:35 -0500

    missing import Version

--- cvxpy/reductions/solvers/conic_solvers/glop_conif.py
+++ cvxpy/reductions/solvers/conic_solvers/glop_conif.py
@@ -27,6 +27,7 @@ from cvxpy.reductions.dcp2cone.cone_matrix_stuffing import ParamConeProg
 from cvxpy.reductions.solution import Solution, failure_solution
 from cvxpy.reductions.solvers import utilities
 from cvxpy.reductions.solvers.conic_solvers.conic_solver import ConicSolver
+from cvxpy.utilities.versioning import Version
 
 log = logging.getLogger(__name__)
 
