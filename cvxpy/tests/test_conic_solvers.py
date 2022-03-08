235f3b9ff17757f14111528f11fea8323a8402bf
rileyjmurray <rjmurray@berkeley.edu>
2022-03-07 20:42:26 -0800

    move location of tests introduced in PR #1684

--- cvxpy/tests/test_conic_solvers.py
+++ cvxpy/tests/test_conic_solvers.py
@@ -662,9 +662,6 @@ class TestGLPK(unittest.TestCase):
     def test_glpk_lp_6(self) -> None:
         StandardTestLPs.test_lp_6(solver='GLPK')
 
-    def test_glpk_resetting_options(self) -> None:
-        StandardTestLPs.test_lp_reset_options(solver='GLPK')
-
     def test_glpk_mi_lp_0(self) -> None:
         StandardTestLPs.test_mi_lp_0(solver='GLPK_MI')
 
@@ -680,8 +677,25 @@ class TestGLPK(unittest.TestCase):
     def test_glpk_mi_lp_4(self) -> None:
         StandardTestLPs.test_mi_lp_4(solver='GLPK_MI')
 
-    def test_glpk_mi_resetting_options(self) -> None:
-        StandardTestLPs.test_mi_lp_reset_options(solver='GLPK_MI')
+    def test_glpk_options(self) -> None:
+        sth = sths.lp_1()
+        import cvxopt
+        assert "tm_lim" not in cvxopt.glpk.options
+        sth.solve(solver='GLPK', tm_lim=100)
+        assert "tm_lim" not in cvxopt.glpk.options
+        sth.verify_objective(places=4)
+        sth.check_primal_feasibility(places=4)
+        sth.check_complementarity(places=4)
+        sth.check_dual_domains(places=4)
+
+    def test_glpk_mi_options(self) -> None:
+        sth = sths.mi_lp_1()
+        import cvxopt
+        assert "tm_lim" not in cvxopt.glpk.options
+        sth.solve(solver='GLPK_MI', tm_lim=100, verbose=True)
+        assert "tm_lim" not in cvxopt.glpk.options
+        sth.verify_objective(places=4)
+        sth.verify_primal_values(places=4)
 
 
 @unittest.skipUnless('GLOP' in INSTALLED_SOLVERS, 'GLOP is not installed.')
