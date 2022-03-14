5628fd5170653dbf2ec3aafe46e6195d6325eb60
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-03-14 13:38:43 -0700

    cvxpy/tests/test_mip_vars.py: Add test for infeasible boolean problem, fails with CBC

--- cvxpy/tests/test_mip_vars.py
+++ cvxpy/tests/test_mip_vars.py
@@ -80,6 +80,29 @@ class TestMIPVariable(BaseTest):
 
         self.assertItemsAlmostEqual(self.A_bool.value, C, places=4)
 
+        # Infeasible boolean problem.
+        # https://trac.sagemath.org/ticket/31962#comment:48
+        z = cp.Variable(11, boolean=True)
+        constraints = [z[2] + z[1] == 1,
+                       z[4] + z[3] == 1,
+                       z[6] + z[5] == 1,
+                       z[8] + z[7] == 1,
+                       z[10] + z[9] == 1,
+                       z[4] + z[1] <= 1,
+                       z[2] + z[3] <= 1,
+                       z[6] + z[2] <= 1,
+                       z[1] + z[5] <= 1,
+                       z[8] + z[6] <= 1,
+                       z[5] + z[7] <= 1,
+                       z[10] + z[8] <= 1,
+                       z[7] + z[9] <= 1,
+                       z[9] + z[4] <= 1,
+                       z[3] + z[10] <= 1]
+        obj = cp.Minimize(0)
+        p = cp.Problem(obj, constraints)
+        result = p.solve(solver=solver)
+        self.assertEqual(p.status in s.INF_OR_UNB, True)
+
     def int_prob(self, solver) -> None:
         # Int in objective.
         obj = cp.Minimize(cp.abs(self.y_int - 0.2))
