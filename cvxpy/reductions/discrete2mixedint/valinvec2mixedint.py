6f00ad743a40327a007db065c226e7c50d0edbdf
phschiele <phschiele@gmail.com>
2022-04-11 16:43:48 +0200

    Follow-ups

--- cvxpy/reductions/discrete2mixedint/valinvec2mixedint.py
+++ cvxpy/reductions/discrete2mixedint/valinvec2mixedint.py
@@ -6,7 +6,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+    https://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,32 +14,33 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+from typing import Callable, List, Tuple
+
 import numpy as np
 
 import cvxpy as cp
 from cvxpy.constraints import FiniteSet
+from cvxpy.constraints.constraint import Constraint
 from cvxpy.expressions.variable import Variable
 from cvxpy.reductions.canonicalization import Canonicalization
 
 
-def exprval_in_vec1(expr, vec):
-    assert expr.is_affine()
-    assert expr.size == 1
-    if vec.size == 1:
-        # handling for when vec only has a single element
-        cons = [expr == vec[0]]
-        return cons
+def exprval_in_vec_ineq(expr, vec):
+
     vec = np.sort(vec)
     d = np.diff(vec)
     z = Variable(shape=(d.size,), boolean=True)
     cons = [
-        z[1:] <= z[:-1],
-        expr == vec[0] + d @ z
+        expr == vec[0] + d @ z,
     ]
+    if d.size > 1:
+        cons.append(z[1:] <= z[:-1])
     return cons
 
 
-def exprval_in_vec2(expr, vec):
+def exprval_in_vec_eq(expr, vec):
+    # Reference: https://docs.mosek.com/modeling-cookbook/mio.html#fixed-set-of-values
+
     z = Variable(len(vec), boolean=True)
     constraints = [
         cp.sum(cp.multiply(vec, z)) == expr,
@@ -48,36 +49,33 @@ def exprval_in_vec2(expr, vec):
     return constraints
 
 
-def finite_set_canon1(con, args):
-    cons = []
-    vec = con.vec.value
-    expre = con.expre.flatten()
-    for i in range(expre.size):
-        cons += exprval_in_vec1(expre[i], vec)
-    main_con = cons[-1]
-    aux_cons = cons[:-1]
-    return main_con, aux_cons
+def get_exprval_in_vec_func(ineq_form: bool) -> Callable:
+    if ineq_form:
+        return exprval_in_vec_ineq
+    else:
+        return exprval_in_vec_eq
 
 
-def finite_set_canon2(con, args):
+def finite_set_canon(con, _args) -> Tuple[Constraint, List]:
     cons = []
     vec = con.vec.value
+    if vec.size == 1:
+        # handling for when vec only has a single element
+        return con.expre == vec[0], []
+
     expre = con.expre.flatten()
     for i in range(expre.size):
-        cons += exprval_in_vec2(expre[i], vec)
+        exprval_in_vec = get_exprval_in_vec_func(con.ineq_form)
+        cons += exprval_in_vec(expre[i], vec)
     main_con = cons[0]
     aux_cons = cons[1:]
     return main_con, aux_cons
 
 
-def finite_set_canon(con, args):
-    if con.ineq_form:
-        return finite_set_canon1(con, args)
-    else:
-        return finite_set_canon2(con, args)
-
-
 class Valinvec2mixedint(Canonicalization):
+    def accepts(self, problem) -> bool:
+        return any(FiniteSet in {type(c) for c in problem.constraints})
+
     CANON_METHODS = {
         FiniteSet: finite_set_canon
     }
