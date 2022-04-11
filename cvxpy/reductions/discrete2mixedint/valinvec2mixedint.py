ffc05bb6237fbf70644998635e4e24e792c12a30
phschiele <44360364+phschiele@users.noreply.github.com>
2022-04-12 00:02:29 +0200

    Follow-up FiniteSet (#1748)
    * Follow-ups
    
    * Remove copy/paste test description
    
    * Change default back to ineq_form=False
    
    * Add test with independent entries
    
    * Vectorize

--- cvxpy/reductions/discrete2mixedint/valinvec2mixedint.py
+++ cvxpy/reductions/discrete2mixedint/valinvec2mixedint.py
@@ -6,7 +6,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+    https://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,70 +14,70 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+from typing import Callable, List, Tuple
+
 import numpy as np
 
 import cvxpy as cp
 from cvxpy.constraints import FiniteSet
+from cvxpy.constraints.constraint import Constraint
 from cvxpy.expressions.variable import Variable
 from cvxpy.reductions.canonicalization import Canonicalization
 
 
-def exprval_in_vec1(expr, vec):
-    assert expr.is_affine()
-    assert expr.size == 1
-    if vec.size == 1:
-        # handling for when vec only has a single element
-        cons = [expr == vec[0]]
-        return cons
+def exprval_in_vec_ineq(expr, vec):
+
+    assert len(expr.shape) == 1
+    n_entries = expr.shape[0]
+
     vec = np.sort(vec)
     d = np.diff(vec)
-    z = Variable(shape=(d.size,), boolean=True)
-    cons = [
-        z[1:] <= z[:-1],
-        expr == vec[0] + d @ z
-    ]
-    return cons
-
-
-def exprval_in_vec2(expr, vec):
-    z = Variable(len(vec), boolean=True)
-    constraints = [
-        cp.sum(cp.multiply(vec, z)) == expr,
-        cp.sum(z) == 1
-    ]
-    return constraints
-
-
-def finite_set_canon1(con, args):
-    cons = []
-    vec = con.vec.value
-    expre = con.expre.flatten()
-    for i in range(expre.size):
-        cons += exprval_in_vec1(expre[i], vec)
-    main_con = cons[-1]
-    aux_cons = cons[:-1]
-    return main_con, aux_cons
-
-
-def finite_set_canon2(con, args):
-    cons = []
-    vec = con.vec.value
-    expre = con.expre.flatten()
-    for i in range(expre.size):
-        cons += exprval_in_vec2(expre[i], vec)
-    main_con = cons[0]
-    aux_cons = cons[1:]
-    return main_con, aux_cons
-
-
-def finite_set_canon(con, args):
-    if con.ineq_form:
-        return finite_set_canon1(con, args)
+    repeated_d = np.broadcast_to(d, (n_entries, len(d)))
+    z = Variable(shape=repeated_d.shape, boolean=True)
+    main_con = expr == vec[0] + cp.sum(cp.multiply(repeated_d, z), axis=1)
+    if d.size > 1:
+        aux_cons = [z[:, 1:] <= z[:, :-1]]
     else:
-        return finite_set_canon2(con, args)
+        aux_cons = []
+    return main_con, aux_cons
+
+
+def exprval_in_vec_eq(expr, vec):
+    # Reference: https://docs.mosek.com/modeling-cookbook/mio.html#fixed-set-of-values
+
+    assert len(expr.shape) == 1
+    n_entries = expr.shape[0]
+    repeated_vec = np.broadcast_to(vec, (n_entries, len(vec)))
+    z = Variable(repeated_vec.shape, boolean=True)
+
+    main_con = cp.sum(cp.multiply(repeated_vec, z), axis=1) == expr
+    aux_cons = [cp.sum(z, axis=1) == 1]
+    return main_con, aux_cons
+
+
+def get_exprval_in_vec_func(ineq_form: bool) -> Callable:
+    if ineq_form:
+        return exprval_in_vec_ineq
+    else:
+        return exprval_in_vec_eq
+
+
+def finite_set_canon(con: FiniteSet, _args) -> Tuple[Constraint, List]:
+    vec = con.vec.value
+    if vec.size == 1:
+        # handling for when vec only has a single element
+        return con.expre == vec[0], []
+
+    flat_expr = con.expre.flatten()
+    exprval_in_vec = get_exprval_in_vec_func(con.ineq_form)
+    main_con, aux_cons = exprval_in_vec(flat_expr, vec)
+    return main_con, aux_cons
 
 
 class Valinvec2mixedint(Canonicalization):
+    def accepts(self, problem) -> bool:
+        return any(FiniteSet in {type(c) for c in problem.constraints})
+
     CANON_METHODS = {
         FiniteSet: finite_set_canon
     }
