0c7678e08e79c4e26b4fa08fd735d742507c3fd0
parthb83 <parthbansal1536@gmail.com>
2022-05-18 01:30:13 +0530

    Modified tests

--- cvxpy/tests/test_dqcp.py
+++ cvxpy/tests/test_dqcp.py
@@ -500,8 +500,8 @@ class TestDqcp(base_test.BaseTest):
         constr = [
             A[0][0] == 2.0,
             A[1][1] == 3.0,
-            A[0][1] <= 2, A[0][1] >= 0,
-            A[1][0] <= 2, A[1][0] >= 0,
+            A[0][1] <= 2, A[0][1] >= 1,
+            A[1][0] <= 2, A[1][0] >= 1,
         ]
         prob = cp.Problem(cp.Minimize(con_num), constr)
         self.assertTrue(prob.is_dqcp())
