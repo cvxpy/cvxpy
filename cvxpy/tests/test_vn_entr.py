845d161d1a45d46c471b4a90fe56ecf85e701fdb
aryamanJgl <jeendgararyaman@gmail.com>
2022-05-28 10:33:23 +0530

    cosmetic changes

--- cvxpy/tests/test_vn_entr.py
+++ cvxpy/tests/test_vn_entr.py
@@ -15,10 +15,8 @@ limitations under the License.
 """
 
 import numpy as np
-import pytest
 
 import cvxpy as cp
-from cvxpy import trace
 from cvxpy.atoms import vn_entr
 from cvxpy.tests import solver_test_helpers as STH
 
@@ -28,10 +26,9 @@ class Test_vn_entr:
     def make_test_1():
         """(2,2) matrix, 100 largest ev, 1e-3 off-diagonal element"""
         n = 2
-        N = cp.Variable(shape= (n,n), PSD= True)
-        expect_N = np.array(
-                [[0.36787973, 0.00099987],
-                [0.00099987, 0.36787973]])
+        N = cp.Variable(shape=(n, n), PSD=True)
+        expect_N = np.array([[0.36787973, 0.00099987],
+                             [0.00099987, 0.36787973]])
         eps = 1e2
         cons1 = N >> 0
         cons2 = N << eps*np.eye(N.shape[0])
@@ -59,11 +56,10 @@ class Test_vn_entr:
     def make_test_2():
         """(3,3) matrix, 1000 largest ev, 1e-2 off-diagonal element"""
         n = 3
-        N = cp.Variable(shape= (n,n), PSD= True)
-        expect_N = np.array(
-       [[ 3.66497984e-01, -9.66999473e-14,  9.99852746e-03],
-       [-9.66999473e-14,  3.70615053e-01, -3.41058370e-13],
-       [ 9.99852746e-03, -3.41058370e-13,  3.66497984e-01]])
+        N = cp.Variable(shape=(n, n), PSD=True)
+        expect_N = np.array([[3.66497984e-01, -9.66999473e-14, 9.99852746e-03],
+                             [-9.66999473e-14, 3.70615053e-01, -3.41058370e-13],
+                             [9.99852746e-03, -3.41058370e-13, 3.66497984e-01]])
         eps = 1e3
         cons1 = N >> 0
         cons2 = N << eps*np.eye(N.shape[0])
@@ -90,14 +86,13 @@ class Test_vn_entr:
     def make_test_3():
         """(3,3) matrix, 1000 largest ev, 1e-2 off diagonal element under a unitary transform"""
         n = 3
-        N = cp.Variable(shape= (n,n), PSD= True)
-        expect_N = np.array([
-        [ 3.70615097e-01, -6.50164940e-15, -2.51416162e-13],
-        [-6.50164940e-15,  3.66498027e-01, -9.99852741e-03],
-        [-2.51416162e-13, -9.99852741e-03,  3.66498027e-01]])
+        N = cp.Variable(shape=(n, n), PSD=True)
+        expect_N = np.array([[3.70615097e-01, -6.50164940e-15, -2.51416162e-13],
+                             [-6.50164940e-15, 3.66498027e-01, -9.99852741e-03],
+                             [-2.51416162e-13, -9.99852741e-03, 3.66498027e-01]])
         U = cp.Constant(np.array([[0, -1, 0],
-                    [1, 0, 0],
-                    [0, 0, 1]]))
+                                  [1, 0, 0],
+                                  [0, 0, 1]]))
         eps = 1e3
         cons1 = N >> 0
         cons2 = N << eps*np.eye(N.shape[0])
@@ -126,10 +121,10 @@ class Test_vn_entr:
     def make_test_4():
         """(3,3) matrix, [1,0,1] EV, 3 ev, 1e-5 off-diagonal value"""
         n = 3
-        N = cp.Variable(shape= (n,n), PSD= True)
-        expect_N = np.array([[ 2.99999997e+00, -1.59980927e-15,  1.81681520e-08],
-       [-1.59980927e-15,  2.99990732e+00,  1.61179597e-15],
-       [ 1.81681520e-08,  1.61179597e-15,  2.99999997e+00]])
+        N = cp.Variable(shape=(n, n), PSD=True)
+        expect_N = np.array([[2.99999997e+00, -1.59980927e-15, 1.81681520e-08],
+                             [-1.59980927e-15, 2.99990732e+00, 1.61179597e-15],
+                             [1.81681520e-08, 1.61179597e-15, 2.99999997e+00]])
         cons1 = N >> 0
         v = cp.Constant(np.array([1, 0, 1]))
         mu = 3
