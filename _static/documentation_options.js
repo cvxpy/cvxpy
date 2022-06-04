265b17ac31f5987d68cc84bfd8d069fd8ab654f5
phschiele <phschiele@gmail.com>
2022-06-03 17:07:01 -0700

    Revert "Deploying to gh-pages from @ cvxpy/cvxpy@0d354b1450b0bbc8b462dc50221c3206a957bb5e 🚀"
    This reverts commit 74482f4847417ba8ebeeb612827552eb7232ed7d.

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.3.0.dev0+0.0d354b1',
+    VERSION: '1.2.1.dev0+0.d724a21',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
