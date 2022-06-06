01e9f6fc36d6a20f98f61221913f10bb930fce0d
aryamanJgl <jeendgararyaman@gmail.com>
2022-06-06 21:40:31 +0530

    new test case + name fix

--- cvxpy/tests/test_von_neumann_entr.py
+++ cvxpy/tests/test_von_neumann_entr.py
@@ -15,11 +15,9 @@ limitations under the License.
 """
 
 import numpy as np
-from numpy.linalg import LinAlgError
 
 import cvxpy as cp
 from cvxpy import trace
-from scipy.linalg import logm
 from cvxpy.atoms import von_neumann_entr
 from cvxpy.tests import solver_test_helpers as STH
 
@@ -32,23 +30,6 @@ class Test_von_neumann_entr:
         SOLVE_ARGS = {'solver': 'SCS', 'eps': 1e-6, 'max_iters': 500_000,
                       'verbose': True}
 
-    # @staticmethod
-    # def projector(A):
-    #     """
-    #     Helper function for computing $n-th$ eigenvector, given the other $(n-1)$
-    #     A: matrix with dimensions (n, n-1), n-1 eigenvectors arranged column-wise
-    #     """
-    #     # Generate an orthogonal basis for the (n-1) dimensional space
-    #     Q = np.linalg.qr(A)[0]
-    #     # grab the projector for this space
-    #     P = Q @ Q.T
-    #     # grab the projector for it's orthogonal space
-    #     P_orth = np.identity(Q.shape[0]) - P
-    #     x = np.random.rand(Q.shape[0])
-    #     # take the projection of some arbitrary vector in R^{n} onto this space to get v_{n}
-    #     v_n = P_orth @ x / np.linalg.norm(P_orth @ x)
-    #     return np.resize(v_n, (Q.shape[0],1))
-
     @staticmethod
     def make_test_1():
         """Expect un-specified EV to be 0.2"""
@@ -60,8 +41,8 @@ class Test_von_neumann_entr:
         v1 = np.array([[-0.26726124],
                        [-0.53452248],
                        [-0.80178373]])
-        v2 = np.array([[ 0.87287156],
-                       [ 0.21821789],
+        v2 = np.array([[0.87287156],
+                       [0.21821789],
                        [-0.43643578]])
         lambda1 = 0.5
         lambda2 = 0.1
@@ -93,14 +74,14 @@ class Test_von_neumann_entr:
         """expect unspecified EV to be 0.4"""
         n = 3
         N = cp.Variable(shape=(n, n), PSD=True)
-        expect_N = np.array([[ 0.23484857, -0.06060623,  0.04393948],
-                             [-0.06060623,  0.3575761 , -0.0242426 ],
-                             [ 0.04393948, -0.0242426 ,  0.30757584]])
+        expect_N = np.array([[0.23484857, -0.06060623, 0.04393948],
+                             [-0.06060623, 0.3575761, -0.0242426],
+                             [0.04393948, -0.0242426, 0.30757584]])
         v1 = np.array([[-0.12309149],
                        [-0.49236596],
                        [-0.86164044]])
-        v2 = np.array([[ 0.90453403],
-                       [ 0.30151134],
+        v2 = np.array([[0.90453403],
+                       [0.30151134],
                        [-0.30151134]])
         lambda1 = 0.3
         lambda2 = 0.2
@@ -131,22 +112,22 @@ class Test_von_neumann_entr:
         """Expect unspecified EV to be 0.35"""
         n = 4
         N = cp.Variable(shape=(n, n), PSD=True)
-        expect_N = np.array([[ 0.15567716, -0.05194798, -0.04646885,  0.05940634],
-                             [-0.05194798,  0.18639235,  0.01639256, -0.01750361],
-                             [-0.04646885,  0.01639256,  0.25662141, -0.07654513],
-                             [ 0.05940634, -0.01750361, -0.07654513,  0.20130907]])
+        expect_N = np.array([[0.15567716, -0.05194798, -0.04646885, 0.05940634],
+                             [-0.05194798, 0.18639235, 0.01639256, -0.01750361],
+                             [-0.04646885, 0.01639256, 0.25662141, -0.07654513],
+                             [0.05940634, -0.01750361, -0.07654513, 0.20130907]])
         v1 = np.array([[-0.18257419],
-                    [-0.36514837],
-                    [-0.54772256],
-                    [-0.73029674]])
+                       [-0.36514837],
+                       [-0.54772256],
+                       [-0.73029674]])
         v2 = np.array([[-8.16496581e-01],
-                    [-4.08248290e-01],
-                    [ 2.22044605e-16],
-                    [ 4.08248290e-01]])
+                       [-4.08248290e-01],
+                       [2.22044605e-16],
+                       [4.08248290e-01]])
         v3 = np.array([[-0.37407225],
-                    [ 0.79697056],
-                    [-0.47172438],
-                    [ 0.04882607]])
+                       [0.79697056],
+                       [-0.47172438],
+                       [0.04882607]])
         lambda1 = 0.15
         lambda2 = 0.1
         lambda3 = 0.2
@@ -177,62 +158,29 @@ class Test_von_neumann_entr:
 
     @staticmethod
     def make_test_4():
-        m = 4
-        A1 = np.array([[8.38972 ,1.02671 ,0.87991],
-                       [1.02671 ,8.41455 ,7.31307],
-                       [0.87991 ,7.31307 ,2.35915]])
+        A1 = np.array([[8.38972, 1.02671, 0.87991],
+                       [1.02671, 8.41455, 7.31307],
+                       [0.87991, 7.31307, 2.35915]])
 
-        A2 = np.array([[6.92907 ,4.37713 ,5.11915],
-                       [4.37713 ,7.96725 ,4.42217],
-                       [5.11915 ,4.42217 ,2.72919]])
+        A2 = np.array([[6.92907, 4.37713, 5.11915],
+                       [4.37713, 7.96725, 4.42217],
+                       [5.11915, 4.42217, 2.72919]])
 
-        A3 = np.array([[1.59465 ,6.97858 ,1.19771],
-                       [6.97858 ,1.95050 ,1.16576],
-                       [1.19771 ,1.16576 ,7.04837]])
+        b = np.array([19.16342, 17.62551])
 
-        A4 = np.array([[-3,  0,  0],
-                       [ 0, -2,  0],
-                       [ 0,  0, -1]])
-
-        X_ = np.array([[42, 0, 0],
-                       [0, 20, 0],
-                       [0, 0, 69]])
-
-        A = [A1, A2, A3, A4]
-        y_ = np.array([[0.56625887],
-                       [0.82940523],
-                       [0.92765576],
-                       [16]])
-
-        running = np.zeros(A[0].shape)
-        for index in range(m):
-            running += A[index] * y_[index]
-        C = logm(X_) + np.eye(3)
-        S = C - running
-        try:
-            tmp = np.linalg.cholesky(S)
-        except LinAlgError:
-            print("If S is not PSD, then the above test case is invalid")
-
-        b = np.array([np.trace(A[index] @ X_) for index in range(m)]).reshape((4, 1))
-
-        N = cp.Variable(shape = (3,3), PSD = True)
-        expect_N = np.array([[ 4.20000004e+01, -1.02890475e-08,  1.11583350e-08],
-                             [-1.02890475e-08,  1.99999995e+01,  1.14357171e-08],
-                             [ 1.11583350e-08,  1.14357171e-08,  6.90000002e+01]])
+        N = cp.Variable(shape=(3, 3), PSD=True)
+        expect_N = np.array([[0.82564479, 0., 0.],
+                             [0., 1.27395299, 0.],
+                             [0., 0., 0.64292239]])
         objective = cp.Minimize(-von_neumann_entr(N))
-        obj_pair = (objective, 509.05011909369927)
+        obj_pair = (objective, -0.13372929515039528)
         cons1 = trace(A1 @ N) == b[0]
         cons2 = trace(A2 @ N) == b[1]
-        cons3 = trace(A3 @ N) == b[2]
-        cons4 = trace(A4 @ N) == b[3]
-        cons5 = N - cp.diag(cp.diag(N)) == 0
+        cons3 = N - cp.diag(cp.diag(N)) == 0
         con_pairs = [
             (cons1, None),
             (cons2, None),
             (cons3, None),
-            (cons4, None),
-            (cons5, None)
         ]
         var_pairs = [
             (N, expect_N)
@@ -245,3 +193,4 @@ class Test_von_neumann_entr:
         sth.solve(**self.SOLVE_ARGS)
         sth.verify_objective(places=3)
         sth.verify_primal_values(places=3)
+        sth.check_primal_feasibility(places=3)
