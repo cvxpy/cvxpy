1554672edfe6499be6ebe33fb1a5ec24d2c5e557
Steven Diamond <diamond@cs.stanford.edu>
2022-07-01 11:51:49 -0700

    upper tri

--- cvxpy/reductions/dqcp2dcp/sets.py
+++ cvxpy/reductions/dqcp2dcp/sets.py
@@ -17,6 +17,7 @@ from cvxpy import atoms
 from cvxpy.atoms.affine import binary_operators as bin_op
 from cvxpy.atoms.affine.diag import diag_vec
 from cvxpy.atoms.affine.promote import promote
+from cvxpy.atoms.affine.upper_tri import upper_tri
 from cvxpy.constraints.psd import PSD
 from cvxpy.expressions.constants.parameter import Parameter
 from cvxpy.expressions.variable import Variable
@@ -138,7 +139,7 @@ def condition_number_sub(expr, t):
     tmp_expr1 = A - diag_vec(prom_u)
     tmp_expr2 = diag_vec(prom_ut) - A
 
-    return [A == A.T,
+    return [upper_tri(A) == upper_tri(A.T),
             PSD(A),
             PSD(tmp_expr1),
             PSD(tmp_expr2)]
