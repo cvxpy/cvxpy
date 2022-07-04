20114cbccb7d11a065d1dd612456d3cb02dd5786
Steven Diamond <diamond@cs.stanford.edu>
2022-07-04 11:28:58 -0700

    fix MOSEK, Gurobi, Cbc

--- cvxpy/reductions/solvers/conic_solvers/mosek_conif.py
+++ cvxpy/reductions/solvers/conic_solvers/mosek_conif.py
@@ -86,6 +86,8 @@ class MOSEK(ConicSolver):
         if hasattr(mosek.conetype, 'pexp') and ExpCone not in MOSEK.SUPPORTED_CONSTRAINTS:
             MOSEK.SUPPORTED_CONSTRAINTS.append(ExpCone)
             MOSEK.SUPPORTED_CONSTRAINTS.append(PowCone3D)
+            MOSEK.MI_SUPPORTED_CONSTRAINTS.append(ExpCone)
+            MOSEK.MI_SUPPORTED_CONSTRAINTS.append(PowCone3D)
 
     def name(self):
         """The name of the solver.
