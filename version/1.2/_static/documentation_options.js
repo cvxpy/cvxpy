f78a208180f2de0b929d529e2113acfa1db77584
SteveDiamond <SteveDiamond@users.noreply.github.com>
2022-07-30 23:53:47 +0000

    Deploying to gh-pages from @ cvxpy/cvxpy@aa36e445e0f65996fd997a5eb333a171762ba021 🚀

--- version/1.2/_static/documentation_options.js
+++ version/1.2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1',
+    VERSION: '1.2.2.dev0+0.aa36e44',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -10,5 +10,5 @@ var DOCUMENTATION_OPTIONS = {
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: false,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
