6eab49c786c66ac16ac4480f08bd7dc9aedc4aa0
Steven Diamond <diamond@cs.stanford.edu>
2022-08-07 14:30:57 -0700

    one more test for scs >= 3

--- cvxpy/tests/test_conic_solvers.py
+++ cvxpy/tests/test_conic_solvers.py
@@ -323,22 +323,25 @@ class TestSCS(BaseTest):
     def test_quad_obj(self) -> None:
         """Test SCS canonicalization with a quadratic objective.
         """
-        x = cp.Variable(2)
-        expr = cp.sum_squares(x)
-        constr = [x >= 1]
-        prob = cp.Problem(cp.Minimize(expr), constr)
-        data = prob.get_problem_data(solver=cp.SCS)
-        self.assertItemsAlmostEqual(data[0]["P"].A, 2*np.eye(2))
-        solution1 = prob.solve(solver=cp.SCS)
+        # Only relevant for SCS >= 3.0.0.
+        import scs
+        if Version(scs.__version__) >= Version('3.0.0'):
+            x = cp.Variable(2)
+            expr = cp.sum_squares(x)
+            constr = [x >= 1]
+            prob = cp.Problem(cp.Minimize(expr), constr)
+            data = prob.get_problem_data(solver=cp.SCS)
+            self.assertItemsAlmostEqual(data[0]["P"].A, 2*np.eye(2))
+            solution1 = prob.solve(solver=cp.SCS)
 
-        # When use_quad_obj = False, the quadratic objective is
-        # canonicalized to a SOC constraint.
-        prob = cp.Problem(cp.Minimize(expr), constr)
-        data = prob.get_problem_data(solver=cp.SCS, use_quad_obj=False)
-        assert "P" not in data[0]
-        solution2 = prob.solve(solver=cp.SCS, use_quad_obj=False)
+            # When use_quad_obj = False, the quadratic objective is
+            # canonicalized to a SOC constraint.
+            prob = cp.Problem(cp.Minimize(expr), constr)
+            data = prob.get_problem_data(solver=cp.SCS, use_quad_obj=False)
+            assert "P" not in data[0]
+            solution2 = prob.solve(solver=cp.SCS, use_quad_obj=False)
 
-        assert np.isclose(solution1, solution2)
+            assert np.isclose(solution1, solution2)
 
     def test_scs_lp_3(self) -> None:
         StandardTestLPs.test_lp_3(solver='SCS')
