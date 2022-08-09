b4dfcc8c1989359389c8be75ffc909134035c448
phschiele <phschiele@gmail.com>
2022-08-08 21:26:46 -0700

    remove unused function

--- cvxpy/utilities/deterministic.py
+++ cvxpy/utilities/deterministic.py
@@ -6,14 +6,3 @@ def unique_list(duplicates_list):
     used = set()
     unique = [x for x in duplicates_list if not (x in used or used.add(x))]
     return unique
-
-
-def unique_expressions(duplicates_list):
-    """
-    Return unique list of expressions preserving the order.
-    This function only considers the expression.id and thus is invariant under deepcopy of a
-    subset of list elements.
-    """
-    used = set()
-    unique = [x for x in duplicates_list if not (x.id in used or used.add(x.id))]
-    return unique
