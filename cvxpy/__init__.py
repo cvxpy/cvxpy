737aff15b97a9d04e79f84edf597c991702a69d1
phschiele <phschiele@gmail.com>
2022-11-04 01:15:29 -0700

    Add rust backend to init

--- cvxpy/__init__.py
+++ cvxpy/__init__.py
@@ -31,7 +31,7 @@ from cvxpy.reductions.solvers.defines import installed_solvers
 from cvxpy.settings import (CBC, CLARABEL, COPT, CPLEX, CPP_CANON_BACKEND, CVXOPT, DIFFCP, ECOS,
                             ECOS_BB, GLPK, GLPK_MI, GUROBI, INFEASIBLE, INFEASIBLE_INACCURATE,
                             MOSEK, NAG, PDLP, OPTIMAL, OPTIMAL_INACCURATE, OSQP,
-                            ROBUST_KKTSOLVER, GLOP, SCIP, SCIPY, SCIPY_CANON_BACKEND, SCS, SDPA,
-                            PROXQP, SOLVER_ERROR, UNBOUNDED, UNBOUNDED_INACCURATE,
-                            USER_LIMIT, XPRESS, get_num_threads,
+                            ROBUST_KKTSOLVER, GLOP, RUST_CANON_BACKEND, SCIP, SCIPY,
+                            SCIPY_CANON_BACKEND, SCS, SDPA, PROXQP, SOLVER_ERROR, UNBOUNDED,
+                            UNBOUNDED_INACCURATE, USER_LIMIT, XPRESS, get_num_threads,
                             set_num_threads,)
