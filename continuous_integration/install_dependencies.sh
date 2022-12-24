2ddcd20eaece9421559decdbc341639f23be69cd
Steven Diamond <steven@gridmatic.com>
2022-12-24 14:19:36 -0800

    fixing windows python 3.8

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -45,7 +45,7 @@ if [[ "$PYTHON_VERSION" == "3.10" ]]; then
   python -m pip install diffcp gurobipy
 # Python 3.8 on Windows will uninstall NumPy 1.16 and install NumPy 1.24 without the exception.
 elif [[ "$RUNNER_OS" == "Windows" ]] && [[ "$PYTHON_VERSION" == "3.8" ]]; then
-  python -m pip install xpress
+  :
 elif [[ "$PYTHON_VERSION" == "3.6" ]]; then
   python -m pip install xpress
 else
