cd87149a7a53c6febd60d9cac87268df36508081
Steven Diamond <steven@gridmatic.com>
2022-12-27 11:05:39 -0800

    prep for next release

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ from setuptools.command.build_ext import build_ext
 
 MAJOR = 1
 MINOR = 1
-MICRO = 22
-IS_RELEASED = True
+MICRO = 23
+IS_RELEASED = False
 IS_RELEASE_BRANCH = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
