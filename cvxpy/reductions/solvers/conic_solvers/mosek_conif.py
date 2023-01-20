84118116f5479436e1431c06d636118952780c5d
aszekMosek <michal.adamaszek@mosek.com>
2023-01-20 13:59:36 +0100

    Attach Mosek log handler before inputting data

--- cvxpy/reductions/solvers/conic_solvers/mosek_conif.py
+++ cvxpy/reductions/solvers/conic_solvers/mosek_conif.py
@@ -225,6 +225,7 @@ class MOSEK(ConicSolver):
             else:
                 env = mosek.Env()
                 task = env.Task(0, 0)
+                save_file = MOSEK.handle_options(env, task, verbose, solver_opts)
                 task = MOSEK._build_dualized_task(task, data)
         else:
             if len(data[s.C]) == 0:
@@ -233,10 +234,10 @@ class MOSEK(ConicSolver):
             else:
                 env = mosek.Env()
                 task = env.Task(0, 0)
+                save_file = MOSEK.handle_options(env, task, verbose, solver_opts)                
                 task = MOSEK._build_slack_task(task, data)
 
-        # Set parameters, optimize the Mosek Task, and return the result.
-        save_file = MOSEK.handle_options(env, task, verbose, solver_opts)
+        # Optimize the Mosek Task, and return the result.
         if save_file:
             task.writedata(save_file)
         task.optimize()
