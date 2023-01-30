e9821587119479e6a92daf9c8ca88b7ed6778715
Jeffrey <jlchen0@ucla.edu>
2023-01-30 13:55:45 -0800

    Add var test for pnorm input

--- cvxpy/tests/test_dgp2dcp.py
+++ cvxpy/tests/test_dgp2dcp.py
@@ -649,3 +649,11 @@ class TestDgp2Dcp(BaseTest):
         self.assertAlmostEqual(p.solve(gp=True), l2_norm)
         self.assertAlmostEqual(p.solution.opt_val, l2_norm)
         self.assertAlmostEqual(x.value, 1.0)
+
+        # Test on a variable
+        x = cvxpy.Variable(2, pos=True)
+        c = cvxpy.Constant([3, 4])
+        p = cvxpy.Problem(cvxpy.Minimize(cvxpy.norm(c, p=2)), [x == c])
+        self.assertAlmostEqual(p.solve(gp=True), 5)
+        self.assertAlmostEqual(p.solution.opt_val, 5)
+        self.assertItemsAlmostEqual(x.value, c.value)
