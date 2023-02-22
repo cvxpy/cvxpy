687f45c7fe9493db480fbbbc234a4bf89f96f6ca
Steven Diamond <diamond@cs.stanford.edu>
2023-02-21 20:07:09 -0800

    Fix conv and add convolve (#2047)
    * make conv more like np.convolve
    
    * more tests
    
    * make backwards compatible
    
    * add convolve, deprecate conv
    
    * added convolve, deprecated conv
    
    * update docs

--- cvxpy/tests/test_convolution.py
+++ cvxpy/tests/test_convolution.py
@@ -15,6 +15,7 @@ limitations under the License.
 """
 
 import numpy as np
+import pytest
 
 import cvxpy as cvx
 import cvxpy.problems.iterative as iterative
@@ -31,17 +32,19 @@ class TestConvolution(BaseTest):
         """
         n = 3
         x = cvx.Variable(n)
-        f = [1, 2, 3]
-        g = [0, 1, 0.5]
-        f_conv_g = [0., 1., 2.5,  4., 1.5]
-        expr = cvx.conv(f, g)
+        f = np.array([1, 2, 3])
+        g = np.array([0, 1, 0.5])
+        f_conv_g = np.array([0., 1., 2.5,  4., 1.5])
+        with pytest.warns(DeprecationWarning, match="Use convolve"):
+            expr = cvx.conv(f, g)
         assert expr.is_constant()
-        self.assertEqual(expr.shape, (5, 1))
+        self.assertEqual(expr.shape, (5,))
+        self.assertEqual(expr.shape, expr.value.shape)
         self.assertItemsAlmostEqual(expr.value, f_conv_g)
 
         expr = cvx.conv(f, x)
         assert expr.is_affine()
-        self.assertEqual(expr.shape, (5, 1))
+        self.assertEqual(expr.shape, (5,))
         # Matrix stuffing.
         prob = cvx.Problem(cvx.Minimize(cvx.norm(expr, 1)),
                            [x == g])
@@ -49,11 +52,75 @@ class TestConvolution(BaseTest):
         self.assertAlmostEqual(result, sum(f_conv_g), places=3)
         self.assertItemsAlmostEqual(expr.value, f_conv_g)
 
-        # # Expression trees.
-        # prob = Problem(Minimize(norm(expr, 1)))
-        # self.prob_mat_vs_mul_funcs(prob)
-        # result = prob.solve(solver=SCS, expr_tree=True, verbose=True)
-        # self.assertAlmostEqual(result, 0, places=1)
+        # Test other shape configurations.
+        expr = cvx.conv(2, g)
+        self.assertEqual(expr.shape, (3,))
+        self.assertEqual(expr.shape, expr.value.shape)
+        self.assertItemsAlmostEqual(expr.value, 2 * g)
+
+        expr = cvx.conv(f, 2)
+        self.assertEqual(expr.shape, (3,))
+        self.assertEqual(expr.shape, expr.value.shape)
+        self.assertItemsAlmostEqual(expr.value, 2 * f)
+
+        expr = cvx.conv(f, g[:, None])
+        self.assertEqual(expr.shape, (5, 1))
+        self.assertEqual(expr.shape, expr.value.shape)
+        self.assertItemsAlmostEqual(expr.value, f_conv_g)
+
+        expr = cvx.conv(f[:, None], g)
+        self.assertEqual(expr.shape, (5, 1))
+        self.assertEqual(expr.shape, expr.value.shape)
+        self.assertItemsAlmostEqual(expr.value, f_conv_g)
+
+        expr = cvx.conv(f[:, None], g[:, None])
+        self.assertEqual(expr.shape, (5, 1))
+        self.assertEqual(expr.shape, expr.value.shape)
+        self.assertItemsAlmostEqual(expr.value, f_conv_g)
+
+    def test_convolve(self) -> None:
+        """Test convolve.
+        """
+        n = 3
+        x = cvx.Variable(n)
+        f = np.array([1, 2, 3])
+        g = np.array([0, 1, 0.5])
+        f_conv_g = np.array([0., 1., 2.5,  4., 1.5])
+        expr = cvx.convolve(f, g)
+        assert expr.is_constant()
+        self.assertEqual(expr.shape, (5,))
+        self.assertEqual(expr.shape, expr.value.shape)
+        self.assertItemsAlmostEqual(expr.value, f_conv_g)
+
+        expr = cvx.convolve(f, x)
+        assert expr.is_affine()
+        self.assertEqual(expr.shape, (5,))
+        # Matrix stuffing.
+        prob = cvx.Problem(cvx.Minimize(cvx.norm(expr, 1)),
+                           [x == g])
+        result = prob.solve(solver=cvx.SCS)
+        self.assertAlmostEqual(result, sum(f_conv_g), places=3)
+        self.assertItemsAlmostEqual(expr.value, f_conv_g)
+
+        # Test other shape configurations.
+        expr = cvx.convolve(2, g)
+        self.assertEqual(expr.shape, (3,))
+        self.assertEqual(expr.shape, expr.value.shape)
+        self.assertItemsAlmostEqual(expr.value, 2 * g)
+
+        expr = cvx.convolve(f, 2)
+        self.assertEqual(expr.shape, (3,))
+        self.assertEqual(expr.shape, expr.value.shape)
+        self.assertItemsAlmostEqual(expr.value, 2 * f)
+
+        with pytest.raises(ValueError, match="must be scalar or 1D"):
+            expr = cvx.convolve(f, g[:, None])
+
+        with pytest.raises(ValueError, match="must be scalar or 1D"):
+            expr = cvx.convolve(f[:, None], g)
+
+        with pytest.raises(ValueError, match="must be scalar or 1D"):
+            expr = cvx.convolve(f[:, None], g[:, None])
 
     def prob_mat_vs_mul_funcs(self, prob) -> None:
         data, dims = prob.get_problem_data(solver=cvx.SCS)
@@ -101,24 +168,37 @@ class TestConvolution(BaseTest):
     def test_conv_prob(self) -> None:
         """Test a problem with convolution.
         """
-        import numpy as np
         N = 5
+        # Test conv.
         y = np.random.randn(N, 1)
         h = np.random.randn(2, 1)
-        x = cvx.Variable(N)
+        x = cvx.Variable((N, 1))
         v = cvx.conv(h, x)
         obj = cvx.Minimize(cvx.sum(cvx.multiply(y, v[0:N])))
-        print(cvx.Problem(obj, []).solve(solver=cvx.ECOS))
+        prob = cvx.Problem(obj, [])
+        prob.solve(solver=cvx.ECOS)
+        assert prob.status is cvx.UNBOUNDED
+
+        # Test convolve.
+        y = np.random.randn(N)
+        h = np.random.randn(2)
+        x = cvx.Variable((N))
+        v = cvx.convolve(h, x)
+        obj = cvx.Minimize(cvx.sum(cvx.multiply(y, v[0:N])))
+        prob = cvx.Problem(obj, [])
+        prob.solve(solver=cvx.ECOS)
+        assert prob.status is cvx.UNBOUNDED
 
     def test_0D_conv(self) -> None:
         """Convolution with 0D input.
         """
-        x = cvx.Variable((1,))  # or cvx.Variable((1,1))
-        problem = cvx.Problem(
-            cvx.Minimize(
-                cvx.max(cvx.conv([1.], cvx.multiply(1., x)))
-            ),
-            [x >= 0]
-        )
-        problem.solve(cvx.ECOS)
-        assert problem.status == cvx.OPTIMAL
+        for func in [cvx.conv, cvx.convolve]:
+            x = cvx.Variable((1,))  # or cvx.Variable((1,1))
+            problem = cvx.Problem(
+                cvx.Minimize(
+                    cvx.max(func(1., cvx.multiply(1., x)))
+                ),
+                [x >= 0]
+            )
+            problem.solve(cvx.ECOS)
+            assert problem.status == cvx.OPTIMAL
