bca8118a0626c8df6f58a813a957ab29051532cd
Philipp Schiele <44360364+phschiele@users.noreply.github.com>
2023-03-22 23:13:50 +0000

    Remove proxqp

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -14,17 +14,17 @@ if [[ "$PYTHON_VERSION" == "3.6" ]]; then
   conda install scipy=1.3 numpy=1.16 mkl pip=21.3.1 pytest pytest-cov lapack ecos scs osqp cvxopt "setuptools>65.5.1"
 # Issue with installing setuptools > 65.5.1 through conda on mac with Python 3.7.
 elif [[ "$PYTHON_VERSION" == "3.7" ]] && [[ "$RUNNER_OS" == "macos-11" ]]; then
-  conda install scipy=1.3 numpy=1.16 mkl pip pytest pytest-cov lapack ecos scs osqp cvxopt proxsuite setuptools
+  conda install scipy=1.3 numpy=1.16 mkl pip pytest pytest-cov lapack ecos scs osqp cvxopt setuptools
 elif [[ "$PYTHON_VERSION" == "3.7" ]] || [[ "$PYTHON_VERSION" == "3.8" ]]; then
-  conda install scipy=1.3 numpy=1.16 mkl pip pytest pytest-cov lapack ecos scs osqp cvxopt proxsuite "setuptools>65.5.1"
+  conda install scipy=1.3 numpy=1.16 mkl pip pytest pytest-cov lapack ecos scs osqp cvxopt "setuptools>65.5.1"
 elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
   # The earliest version of numpy that works is 1.19.
   # Given numpy 1.19, the earliest version of scipy we can use is 1.5.
-  conda install scipy=1.5 numpy=1.19 mkl pip pytest lapack ecos scs osqp cvxopt proxsuite "setuptools>65.5.1"
+  conda install scipy=1.5 numpy=1.19 mkl pip pytest lapack ecos scs osqp cvxopt "setuptools>65.5.1"
 elif [[ "$PYTHON_VERSION" == "3.10" ]]; then
     # The earliest version of numpy that works is 1.21.
     # Given numpy 1.21, the earliest version of scipy we can use is 1.7.
-    conda install scipy=1.7 numpy=1.21 mkl pip pytest lapack ecos scs osqp cvxopt proxsuite "setuptools>65.5.1"
+    conda install scipy=1.7 numpy=1.21 mkl pip pytest lapack ecos scs osqp cvxopt "setuptools>65.5.1"
 fi
 
 # CBC comes with wheels for windows and needs coin-or-cbc to compile otherwise
