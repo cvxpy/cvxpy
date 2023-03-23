0ad0d5159d4e910517bfd9ea36452a2290155565
Philipp Schiele <44360364+phschiele@users.noreply.github.com>
2023-03-23 21:15:27 +0000

    bring back 3.8

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -12,7 +12,7 @@ conda config --set remote_read_timeout_secs 120.0
 
 if [[ "$PYTHON_VERSION" == "3.6" ]]; then
   conda install scipy=1.3 numpy=1.16 mkl pip=21.3.1 pytest pytest-cov lapack ecos scs osqp cvxopt
-elif [[ "$PYTHON_VERSION" == "3.7" ]]; then
+elif [[ "$PYTHON_VERSION" == "3.7" ]] || [[ "$PYTHON_VERSION" == "3.8" ]]; then
   conda install scipy=1.3 numpy=1.16 mkl pip pytest pytest-cov lapack ecos scs osqp cvxopt
 elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
   # The earliest version of numpy that works is 1.19.
