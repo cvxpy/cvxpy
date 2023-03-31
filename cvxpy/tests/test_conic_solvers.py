5ef2c746f794dc6c2666ba27697734c6bbcb113c
Paul <93285862+Paulnkk@users.noreply.github.com>
2023-04-01 00:34:15 +0200

    Fix scip timelimit no solution (#2084)
    * Add condition timelimit and no solution
    
    * Add timelimit example
    
    * Add unit test timelimit and no solution
    
    * attempt
    
    * Update timelimit
    
    * try to fix windows
    
    * remove failing test
    
    ---------
    
    Co-authored-by: Steven Diamond <steven@gridmatic.com>

--- cvxpy/tests/test_conic_solvers.py
+++ cvxpy/tests/test_conic_solvers.py
@@ -24,7 +24,6 @@ import scipy.stats as st
 
 import cvxpy as cp
 import cvxpy.tests.solver_test_helpers as sths
-from cvxpy import SolverError
 from cvxpy.reductions.solvers.defines import (
     INSTALLED_MI_SOLVERS,
     INSTALLED_SOLVERS,
@@ -1562,6 +1561,22 @@ class TestSCIP(unittest.TestCase):
             exc = "One or more scip params in ['a'] are not valid: 'Not a valid parameter name'"
             assert ke.exception == exc
 
+    def test_scip_time_limit_reached(self) -> None:
+        sth = sths.mi_lp_7()
+
+        # TODO doesn't work on windows.
+        # run without enough time to find optimum
+        # sth.solve(solver="SCIP", scip_params={"limits/time": 0.01})
+        # assert sth.prob.status == cp.OPTIMAL_INACCURATE
+        # assert all([v.value is not None for v in sth.prob.variables()])
+
+        # run without enough time to do anything
+        with pytest.raises(cp.error.SolverError) as se:
+            sth.solve(solver="SCIP", scip_params={"limits/time": 0.0})
+            exc = "Solver 'SCIP' failed. " \
+                  "Try another solver, or solve with verbose=True for more information."
+            assert str(se.value) == exc
+
 
 class TestAllSolvers(BaseTest):
 
@@ -1726,5 +1741,5 @@ class TestSCIPY(unittest.TestCase):
         assert sth.objective.value > 0
 
         # run without enough time to do anything
-        with pytest.raises(SolverError):
+        with pytest.raises(cp.error.SolverError):
             sth.solve(solver='SCIPY', scipy_options={"time_limit": 0.})
