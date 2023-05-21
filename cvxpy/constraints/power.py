962ad17bc8fe26be8dfb00c0fd9e0a794c0f84bc
Riskfolio <dany.cajas.n@uni.pe>
2023-05-21 17:19:27 -0500

    Fixes to PowCone3D and updates to test_conic_solvers.py (#2131)
    Update solver_test_helpers.py to include a cone program with SDP and power cone constraints.
    
    Correct a bug in PowCone3D.residual, which should have been declared as a property but was not.
    
    ---------
    
    Co-authored-by: Steven Diamond <steven@gridmatic.com>

--- cvxpy/constraints/power.py
+++ cvxpy/constraints/power.py
@@ -66,6 +66,7 @@ class PowCone3D(Constraint):
     def __str__(self) -> str:
         return "Pow3D(%s, %s, %s; %s)" % (self.x, self.y, self.z, self.alpha)
 
+    @property
     def residual(self):
         # TODO: The projection should be implemented directly.
         from cvxpy import Minimize, Problem, Variable, hstack, norm2
