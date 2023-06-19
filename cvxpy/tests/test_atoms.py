34a4776a5cc5a2e9fca6766cb2b119a1212b44b1
Philipp Schiele <44360364+phschiele@users.noreply.github.com>
2023-06-19 23:51:23 +0000

    Adds top level import

--- cvxpy/tests/test_atoms.py
+++ cvxpy/tests/test_atoms.py
@@ -736,16 +736,15 @@ class TestAtoms(BaseTest):
                          "Argument to upper_tri must be a square matrix.")
 
     def test_vec_to_upper_tri(self) -> None:
-        from cvxpy.atoms.affine.upper_tri import vec_to_upper_tri
         x = Variable(shape=(3,))
-        X = vec_to_upper_tri(x)
+        X = cp.vec_to_upper_tri(x)
         x.value = np.array([1, 2, 3])
         actual = X.value
         expect = np.array([[1, 2], [0, 3]])
         assert np.allclose(actual, expect)
         y = Variable(shape=(1,))
         y.value = np.array([4])
-        Y = vec_to_upper_tri(y, strict=True)
+        Y = cp.vec_to_upper_tri(y, strict=True)
         actual = Y.value
         expect = np.array([[0, 4], [0, 0]])
         assert np.allclose(actual, expect)
@@ -754,26 +753,26 @@ class TestAtoms(BaseTest):
                              [0, 0, 0, 21],
                              [0, 0, 0, 0]])
         a = np.array([11, 12, 13, 16, 17, 21])
-        A_actual = vec_to_upper_tri(a, strict=True).value
+        A_actual = cp.vec_to_upper_tri(a, strict=True).value
         assert np.allclose(A_actual, A_expect)
 
         with pytest.raises(ValueError, match="must be a triangular number"):
-            vec_to_upper_tri(cp.Variable(shape=(4)))
+            cp.vec_to_upper_tri(cp.Variable(shape=(4)))
 
         with pytest.raises(ValueError, match="must be a triangular number"):
-            vec_to_upper_tri(cp.Variable(shape=(4)), strict=True)
+            cp.vec_to_upper_tri(cp.Variable(shape=(4)), strict=True)
 
         with pytest.raises(ValueError, match="must be a vector"):
-            vec_to_upper_tri(cp.Variable(shape=(2, 2)))
+            cp.vec_to_upper_tri(cp.Variable(shape=(2, 2)))
 
         # works with row vectors
         assert np.allclose(
-            vec_to_upper_tri(np.arange(6)).value, 
-            vec_to_upper_tri(np.arange(6).reshape(1, 6)).value
+            cp.vec_to_upper_tri(np.arange(6)).value, 
+            cp.vec_to_upper_tri(np.arange(6).reshape(1, 6)).value
         )
 
         # works with scalars
-        assert np.allclose(vec_to_upper_tri(1, strict=True).value, np.array([[0, 1], [0, 0]]))
+        assert np.allclose(cp.vec_to_upper_tri(1, strict=True).value, np.array([[0, 1], [0, 0]]))
 
 
     def test_huber(self) -> None:
