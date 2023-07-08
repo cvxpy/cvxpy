41921df08a120d9732ca869332fa599bcfcf347d
cbonet <bpnet.b3@gmail.com>
2023-07-08 00:11:48 -0400

    Fix bug in incidence_matrix example

--- examples/flows/incidence_matrix.py
+++ examples/flows/incidence_matrix.py
@@ -15,41 +15,44 @@ limitations under the License.
 """
 
 # Incidence matrix approach.
+import numpy as np
 import pickle
 
 import create_graph as g
-import cvxopt
 
-from cvxpy import Maximize, Problem, Variable, vstack
+from cvxpy import Maximize, Parameter, Problem, Variable, vstack
 
 # Read a graph from a file.
-f = open(g.FILE, 'r')
+f = open(g.FILE, 'rb')
 data = pickle.load(f)
 f.close()
 
 # Construct incidence matrix and capacities vector.
 node_count = data[g.NODE_COUNT_KEY]
 edges = data[g.EDGES_KEY]
-E = 2*len(edges)
-A = cvxopt.matrix(0,(node_count, E+2), tc='d')
-c = cvxopt.matrix(1000,(E,1), tc='d')
-for i,(n1,n2,capacity) in enumerate(edges):
-    A[n1,2*i] = -1
-    A[n2,2*i] = 1
-    A[n1,2*i+1] = 1
-    A[n2,2*i+1] = -1
-    c[2*i] = capacity
-    c[2*i+1] = capacity
+E = 2 * len(edges)
+
+A = Parameter((node_count, E + 2))
+A.value = np.zeros((node_count, E + 2))
+c = Parameter((E))
+c.value = np.full((E), 1000)
+for i, (n1, n2, capacity) in enumerate(edges):
+    A.value[n1, 2 * i] = -1
+    A.value[n2, 2 * i] = 1
+    A.value[n1, 2 * i + 1] = 1
+    A.value[n2, 2 * i + 1] = -1
+    c.value[2 * i] = capacity
+    c.value[2 * i + 1] = capacity
 # Add source.
-A[0,E] = 1
+A.value[0, E] = 1
 # Add sink.
-A[-1,E+1] = -1
+A.value[-1, E + 1] = -1
 # Construct the problem.
 flows = Variable(E)
 source = Variable()
 sink = Variable()
 p = Problem(Maximize(source),
-            [A*vstack([flows,source,sink]) == 0,
+            [A @ vstack([f for f in flows] + [source, sink]) == 0,
              0 <= flows,
              flows <= c])
 result = p.solve()
