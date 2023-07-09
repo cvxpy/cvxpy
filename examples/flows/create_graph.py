a791ba0ded93bfd0caa46eeeb93a5c8ea5a14b84
cbonet <carloshbonet@gmail.com>
2023-07-08 20:25:16 -0400

    Fixed two TypeError and minor style issues.

--- examples/flows/create_graph.py
+++ examples/flows/create_graph.py
@@ -26,19 +26,19 @@ EDGES_KEY = "edges"
 
 if __name__ == "__main__":
     N = 20
-    E = N*(N-1)/2
+    E = N * (N - 1) / 2
     c = 10
     edges = []
     # Start with a line.
-    for i in range(1,N):
-        edges.append( (i-1,i,c*random()) )
+    for i in range(1, N):
+        edges.append((i - 1, i, c * random()))
     # Add additional edges.
-    for i in range(N,E):
-        n1,n2 = sample(range(N), 2)
-        edges.append( (n1,n2,c) )
+    for i in range(N, int(E)):
+        n1, n2 = sample(range(N), 2)
+        edges.append((n1, n2, c))
     # Pickle the graph data.
     data = {NODE_COUNT_KEY: N,
             EDGES_KEY: edges}
-    f = open(FILE, 'w')
+    f = open(FILE, 'wb')
     pickle.dump(data, f)
     f.close()
