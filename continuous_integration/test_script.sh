6566f37f452849d6c11d158b0eb824d3b60a9153
Philipp Schiele <44360364+phschiele@users.noreply.github.com>
2023-07-27 11:56:08 +0000

    Try to fix openmp build

--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -10,7 +10,7 @@ python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
 
 if [ $USE_OPENMP == "True" ] && [ $RUNNER_OS == "Linux" ]; then
-    CFLAGS="-fopenmp" LDFLAGS="-lgomp" python setup.py install
+    CFLAGS="-fopenmp" LDFLAGS="-lgomp" python -m pip install .
     export OMP_NUM_THREADS=4
 else
     python -m pip list
