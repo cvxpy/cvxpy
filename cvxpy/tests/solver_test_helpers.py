8d92d608eebdb25d88a6665f095a48329dde555a
Philipp Schiele <44360364+phschiele@users.noreply.github.com>
2023-08-07 23:54:03 +0000

    Adds mi_lp_5

--- cvxpy/tests/solver_test_helpers.py
+++ cvxpy/tests/solver_test_helpers.py
@@ -760,6 +760,32 @@ def mi_lp_4() -> SolverTestHelper:
     return sth
 
 
+def mi_lp_5() -> SolverTestHelper:
+    # infeasible boolean problem - https://trac.sagemath.org/ticket/31962#comment:48
+    z = cp.Variable(11, boolean=True)
+    constraints = [z[2] + z[1] == 1,
+                   z[4] + z[3] == 1,
+                   z[6] + z[5] == 1,
+                   z[8] + z[7] == 1,
+                   z[10] + z[9] == 1,
+                   z[4] + z[1] <= 1,
+                   z[2] + z[3] <= 1,
+                   z[6] + z[2] <= 1,
+                   z[1] + z[5] <= 1,
+                   z[8] + z[6] <= 1,
+                   z[5] + z[7] <= 1,
+                   z[10] + z[8] <= 1,
+                   z[7] + z[9] <= 1,
+                   z[9] + z[4] <= 1,
+                   z[3] + z[10] <= 1]
+    obj = cp.Minimize(0)
+    obj_pair = (obj, np.inf)
+    con_pairs = [(c, None) for c in constraints]
+    var_pairs = [(z, None)]
+    sth = SolverTestHelper(obj_pair, var_pairs, con_pairs)
+    return sth
+
+
 def mi_lp_6() -> SolverTestHelper:
     "Test MILP for timelimit and no feasible solution"
     n = 70
@@ -1010,6 +1036,14 @@ class StandardTestLPs:
     @staticmethod
     def test_mi_lp_4(solver, places: int = 4, **kwargs) -> SolverTestHelper:
         sth = mi_lp_4()
+        sth.solve(solver, **kwargs)
+        sth.verify_objective(places)
+        sth.verify_primal_values(places)
+        return sth
+
+    @staticmethod
+    def test_mi_lp_5(solver, places: int = 4, **kwargs) -> SolverTestHelper:
+        sth = mi_lp_5()
         sth.solve(solver, **kwargs)
         sth.verify_objective(places)
         sth.verify_primal_values(places)
