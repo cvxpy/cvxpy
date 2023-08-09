7ebc69a35cc61c2fa7cfd5a2b939f092b4508158
phschiele <phschiele@users.noreply.github.com>
2023-08-09 06:58:06 +0000

    Deploying to gh-pages from @ cvxpy/cvxpy@c9cd44aaada8b6bf0fc7929d8685be42301c637d 🚀

--- version/1.1/_static/documentation_options.js
+++ version/1.1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.23',
+    VERSION: '1.1.24',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
