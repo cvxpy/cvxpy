688ad7a9a6d09533799dcfc445613971fb57c4e3
Parth Nobel <parthnobel@berkeley.edu>
2023-08-10 23:46:08 -0700

    Adds geo_mean error

---
+++ cvxpy/atoms/errormsg.py
@@ -0,0 +1,5 @@
+SECOND_ARG_SHOULD_NOT_BE_EXPRESSION_ERROR_MESSAGE = """
+The second argument has type cvxpy.Expression. However, that is not allowed.
+Most likely, you want to call this atom by using cvxpy.hstack to combine the
+two arguemnts into a vector.
+"""
