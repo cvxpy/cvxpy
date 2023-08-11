4b065d8ae3c97ee1f3124ffc365109166a8b2213
William Zijie Zhang <89562186+Transurgeon@users.noreply.github.com>
2023-08-11 18:54:06 -0400

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -17,7 +17,7 @@ import mock
  
 MOCK_MODULES = ['numpy', 'scipy', 'cvxpy']
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = mock.Mock()
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
