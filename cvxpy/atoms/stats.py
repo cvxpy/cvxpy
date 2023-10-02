81546ad8932dd0a4a26dfd41373bf6ac6b984c1c
Parth Nobel <parthnobel@berkeley.edu>
2023-10-02 16:42:10 -0700

    Addresses doc comment concerns

--- cvxpy/atoms/stats.py
+++ cvxpy/atoms/stats.py
@@ -23,19 +23,21 @@ from cvxpy.atoms.sum_squares import sum_squares
 
 def mean(x, axis=None, keepdims=False):
     """
-    Returns the mean of x
+    Returns the mean of x.
     """
     if axis is None:
         return cvxpy_sum(x, axis, keepdims) / x.size
     elif axis in (0, 1):
         return cvxpy_sum(x, axis, keepdims) / x.shape[axis]
     else:
-        raise RuntimeError("Invalid axis value.")
+        raise ValueError("Invalid axis value.")
 
 
 def std(x, axis=None, keepdims=False, ddof=0):
     """
-    Returns the standard deviation of x
+    Returns the standard deviation of x.
+
+    `ddof` is the quantity to use in the Bessel correction.
     """
     if axis is None:
         return norm((x - mean(x)).flatten(), 2) / np.sqrt(x.size - ddof)
@@ -43,11 +45,13 @@ def std(x, axis=None, keepdims=False, ddof=0):
         return norm(x - mean(x, axis, True), 2, axis=axis, keepdims=keepdims) \
                 / np.sqrt(x.shape[axis] - ddof)
     else:
-        raise RuntimeError("Invalid axis value.")
+        raise ValueError("Invalid axis value.")
 
 def var(x, axis=None, keepdims=False, ddof=0):
     """
-    Returns the standard deviation of x
+    Returns the variance of x.
+
+    `ddof` is the quantity to use in the Bessel correction.
     """
     if axis is None:
         return sum_squares(x - mean(x)) / (x.size - ddof)
@@ -61,4 +65,4 @@ def var(x, axis=None, keepdims=False, ddof=0):
         # return sum_squares(x - mean(x, axis, True), 2, axis=axis, keepdims=keepdims) \
         #         / (x.shape[axis] - ddof)
     else:
-        raise RuntimeError("Invalid axis value.")
+        raise ValueError("Invalid axis value.")
