8deef853ef15776caea2c1d13ed82d7785c4c372
Transurgeon <peter.zijie@gmail.com>
2023-10-27 01:18:05 -0400

    completing first draft of all tables

--- doc/source/_static/main.js
+++ doc/source/_static/main.js
@@ -83,10 +83,36 @@ function createMatrixDCPTable(tableClass, typeValue) {
   });
 }
 
+function createDGPTable(tableClass, typeValue) {
+  var newTable = $(`table.${tableClass}`);
+  const curvatures = ['log-log affine', 'log-log convex', 'log-log concave', 'constant'];
+
+  const dcpData = $('table.atomic-functions').DataTable().data().toArray().filter(row => {
+    const curvatureValue = $(row[4]).text().trim();
+    const rowTypeValue = $(row[6]).text().trim();
+    return curvatures.includes(curvatureValue) && rowTypeValue === typeValue;
+  });
+
+  newTable.DataTable({
+    data: dcpData,
+    columns: [
+      { title: "Function" },
+      { title: "Meaning" },
+      { title: "Domain" },
+      { title: "Sign", visible: false },
+      { title: "Curvature" },
+      { title: "Monotonicity" },
+    ]
+  });
+}
+
   createRadioButtons();
   initializeMainTable();
   createDCPTable("scalar-dcp", "scalar");
   createDCPTable("element-dcp", "elementwise");
   createMatrixDCPTable("matrix-dcp", "matrix");
+  createDGPTable("scalar-dgp", "scalar");
+  createDGPTable("element-dgp", "elementwise");
+  createDGPTable("matrix-dgp", "matrix");
 
 });
