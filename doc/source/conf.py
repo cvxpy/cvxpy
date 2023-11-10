e6e05e41cd3fd306e42cd5325801b43f610e5dde
Transurgeon <peter.zijie@gmail.com>
2023-11-09 19:43:08 -0500

    adding css and other source files

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -248,17 +248,17 @@ html_title = ""  # we are using a logo.
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "_static/cvxpy-wordmark-light.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "_static/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['/_static']
+html_static_path = ['_static']
 html_css_files = ['css/extra_css.css']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
