439def734eb9faec4b8bc88edae88d78974bf457
Philipp Schiele <44360364+phschiele@users.noreply.github.com>
2023-11-22 11:23:32 -0800

    Test SCS Python 3.8 fix (#2292)
    * Use SCS master branch
    
    * Install openblas
    
    * drop pyscipopt on mac and linux
    
    * try fixing cvxpy-base
    
    * update scs requirement
    
    * Install openblas
    
    * Build wheels
    
    * Removes unnecessary change to install openblas
    
    * Revert temp change
    
    ---------
    
    Co-authored-by: Steven Diamond <steven@gridmatic.com>

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -11,19 +11,19 @@ conda config --set remote_backoff_factor 2
 conda config --set remote_read_timeout_secs 120.0
 
 if [[ "$PYTHON_VERSION" == "3.8" ]]; then
-  conda install scipy=1.3 numpy=1.16 mkl pip pytest pytest-cov lapack ecos scs osqp cvxopt proxsuite "setuptools>65.5.1"
+  conda install scipy=1.3 numpy=1.16 mkl pip pytest pytest-cov openblas ecos scs osqp cvxopt proxsuite "setuptools>65.5.1"
 elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
   # The earliest version of numpy that works is 1.19.
   # Given numpy 1.19, the earliest version of scipy we can use is 1.5.
-  conda install scipy=1.5 numpy=1.19 mkl pip pytest lapack ecos scs osqp cvxopt proxsuite "setuptools>65.5.1"
+  conda install scipy=1.5 numpy=1.19 mkl pip pytest openblas ecos scs osqp cvxopt proxsuite "setuptools>65.5.1"
 elif [[ "$PYTHON_VERSION" == "3.10" ]]; then
     # The earliest version of numpy that works is 1.21.
     # Given numpy 1.21, the earliest version of scipy we can use is 1.7.
-    conda install scipy=1.7 numpy=1.21 mkl pip pytest lapack ecos scs osqp cvxopt proxsuite "setuptools>65.5.1"
+    conda install scipy=1.7 numpy=1.21 mkl pip pytest openblas ecos scs osqp cvxopt proxsuite "setuptools>65.5.1"
 elif [[ "$PYTHON_VERSION" == "3.11" ]]; then
     # The earliest version of numpy that works is 1.23.4.
     # Given numpy 1.23.4, the earliest version of scipy we can use is 1.9.3.
-    conda install scipy=1.9.3 numpy=1.23.4 mkl pip pytest lapack ecos scs cvxopt proxsuite "setuptools>65.5.1"
+    conda install scipy=1.9.3 numpy=1.23.4 mkl pip pytest openblas ecos scs cvxopt proxsuite "setuptools>65.5.1"
 fi
 
 if [[ "$PYTHON_VERSION" == "3.11" ]]; then
@@ -52,7 +52,8 @@ if [[ "$RUNNER_OS" != "Windows" ]]; then
 fi
 
 # SCIP only works with scipy >= 1.5 due to dependency conflicts when installing on Linux/macOS
-if [[ "$PYTHON_VERSION" == "3.9" ]] || [[ "$RUNNER_OS" == "Windows" ]]; then
+# TODO make work on linux and mac.
+if [[ "$PYTHON_VERSION" == "3.9" ]] && [[ "$RUNNER_OS" == "Windows" ]]; then
   conda install pyscipopt
 fi
 
