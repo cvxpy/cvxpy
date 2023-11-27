5d4a2645b308a5f198645d2ce23492501276179a
Amit Solomon <as3993@princeton.edu>
2023-11-27 16:14:03 -0500

    Moved gen_torch_exp from Atom to Expression. Moved VariablesDict and the VAR_TYPE enum from atom.py to a new utilities file called torch_utils.py. The user may provide an order in which the varialbes will be read, or they can use the default one used in previous commits. Added a relevant test to test_gen_torch_exp.py (test11 and its unordered variant).

--- cvxpy/expressions/expression.py
+++ cvxpy/expressions/expression.py
@@ -16,7 +16,7 @@ limitations under the License.
 
 import abc
 import warnings
-from functools import wraps
+from functools import partial, wraps
 from typing import List, Tuple
 
 import numpy as np
@@ -30,6 +30,7 @@ from cvxpy.constraints import PSD, Equality, Inequality
 from cvxpy.expressions import cvxtypes
 from cvxpy.utilities import scopes
 from cvxpy.utilities.shape import size_from_shape
+from cvxpy.utilities.torch_utils import VAR_TYPE, VariablesDict
 
 
 def _cast_other(binary_op):
@@ -884,3 +885,258 @@ class Expression(u.Canonical):
         """
         from cvxpy import var
         return var(self, ddof=ddof)
+
+    def conj(self):
+        """
+        Equivalent to `cp.conj(self)`.
+        """
+        from cvxpy import conj
+        return conj(self)
+
+    def conjugate(self):
+        """
+        Equivalent to `cp.conj(self)`.
+        """
+        from cvxpy import conj
+        return conj(self)
+
+    def cumsum(self, axis=0):
+        """
+        Equivalent to `cp.cumsum(self, axis)`.
+        """
+        from cvxpy import cumsum
+        return cumsum(self, axis)
+
+    def max(self, axis=None, *, keepdims=False):
+        """
+        Equivalent to `cp.max(self, axis, keepdims)`.
+        """
+        from cvxpy import max as max_
+        return max_(self, axis, keepdims)
+
+    def mean(self, axis=None, *, keepdims=False):
+        """
+        Equivalent to `cp.mean(self, axis, keepdims)`.
+        """
+        from cvxpy import mean
+        return mean(self, axis, keepdims)
+
+    def min(self, axis=None, *, keepdims=False):
+        """
+        Equivalent to `cp.min(self, axis, keepdims)`.
+        """
+        from cvxpy import min as min_
+        return min_(self, axis, keepdims)
+
+    def prod(self, axis=None, *, keepdims=False):
+        """
+        Equivalent to `cp.prod(self, axis, keepdims)`.
+        """
+        from cvxpy import prod
+        return prod(self, axis, keepdims)
+
+    def ptp(self, axis=None, *, keepdims=False):
+        """
+        Equivalent to `cp.ptp(self, axis, keepdims)`.
+        """
+        from cvxpy import ptp
+        return ptp(self, axis, keepdims)
+
+    def reshape(self, shape, order='F'):
+        """
+        Equivalent to `cp.reshape(self, shape, order)`.
+        """
+        from cvxpy import reshape
+        return reshape(self, shape, order)
+
+    def std(self, axis=None, *, ddof=0, keepdims=False):
+        """
+        Equivalent to `cp.std(self, axis, keepdims)`.
+        """
+        from cvxpy import std
+        return std(self, axis=axis, ddof=ddof, keepdims=keepdims)
+ 
+    def sum(self, axis=None, *, keepdims=False):
+        """
+        Equivalent to `cp.sum(self, axis, keepdims)`.
+        """
+        from cvxpy import sum as sum_
+        return sum_(self, axis, keepdims)
+
+    def trace(self):
+        """
+        Equivalent to `cp.trace(self)`.
+        """
+        from cvxpy import trace
+        return trace(self)
+
+    def var(self, *, ddof=0):
+        """
+        Equivalent to `cp.var(self)`.
+        """
+        from cvxpy import var
+        return var(self, ddof=ddof)
+
+    def apply_torch_numeric(self, values):
+        """
+        This function returns self.torch_numeric(values) if it exists,
+        and self.numeric(values) otherwise.
+        """
+        if hasattr(self, "torch_numeric"):
+            return self.torch_numeric(values)
+        else:
+            return self.numeric(values)
+
+    def gen_torch_exp(self, provided_vars_list:list = []) -> tuple[callable, VariablesDict]:
+        """ This function generates a torch expression.
+        Also returns vars_dict to help keep track of the order of the variables.
+        If no vars_dict is provided, this function will generate one automatically.
+        The order of the arguments is the same as in self.args (left to right).
+        """
+        import torch
+
+        from cvxpy.expressions.constants.constant import Constant
+        from cvxpy.expressions.constants.parameter import Parameter
+        from cvxpy.expressions.variable import Variable
+
+        def _gen_var_type(arg) -> VAR_TYPE:
+            """
+            This is a helper function that generates a VAR_TYPE from an arg.
+            """
+            if isinstance(arg, Constant):
+                return VAR_TYPE.CONSTANT
+            elif isinstance(arg, Parameter) or isinstance(arg, Variable):
+                return VAR_TYPE.VARIABLE_PARAMETER
+            else:
+                return VAR_TYPE.EXPRESSION
+
+        def _gen_consts_vars(self, vars_dict: VariablesDict) -> dict:
+            """ This is a helper function that generates the index -> (value, type) dictionary. """
+            ind_to_value_type = dict() #Local dictionary
+            for i, arg in enumerate(self.args):
+                var_type = _gen_var_type(arg)
+                if isinstance(arg, Constant):
+                    ind_to_value_type[i] = (torch.tensor(arg.value, dtype=torch.float64), var_type)
+                elif isinstance(arg, Parameter) or isinstance(arg, Variable):
+                    ind_to_value_type[i] = (arg, var_type)
+                    vars_dict.add_var(arg)
+                else:
+                    ind_to_value_type[i] = (arg, var_type)
+                    _gen_consts_vars(arg, vars_dict)
+            return ind_to_value_type
+        
+        def wrapped_func(self, ind_to_value_type: dict, vars_dict: VariablesDict, *args):
+            def transpose_if_matmul(self, res: list, should_transpose: list) -> None:
+                """
+                This function transposes the second element if the wrapped function is a dot product
+                between two vectors. While transposing a vector in CVXPY does nothing, this function
+                is important because it helps overloading this function to the case where the
+                one of the elements is a matrix, where each row is a vector to be multiplied with.
+                """
+
+                def is_matmul(self) -> bool:
+                    """
+                    This function checks if self is a valid matrix multiplication
+                    """
+                    from cvxpy.atoms.affine.binary_operators import MulExpression
+                    if not isinstance(self, MulExpression):
+                        return False
+                    #Some subclasses of MulExpressions are not Matrix multiplications, so check
+                    #if the current object is a MulExpression but not a subclass
+                    if issubclass(type(self), MulExpression) and type(self) != MulExpression:
+                        return False
+                    return True
+
+                def check_should_transpose(should_transpose: list) -> bool:
+                    """
+                    This is a helper function that determines if transpose should happen, based
+                    on the CVXPY types.
+                    """
+                    if len(should_transpose) != 2: #Only support a dot product, hence two elements
+                        return False
+                    return all(should_transpose)
+                
+                def is_valid_input(res: list) -> tuple[bool, list]:
+                    """
+                    This function checks if res is valid for transpose, and if it does, returns
+                    the ndims list.
+                    """
+                    ndims = None
+                    valid = False
+
+                    #######################
+                    #Check input validity
+                    #######################
+                    #Transpose only a dot product between two elements
+                    if len(res) != 2:
+                        return valid, ndims
+                    
+                    #Work only on objects with ndim (torch/numpy/cvxpy etc.) and ndim==1
+                    ndims = [0]*2
+                    for i, vec in enumerate(res):
+                        if not hasattr(vec, "ndim"):
+                            return valid, ndims
+                        if vec.ndim >2:
+                            return #Only deal with scalars, vectors, or matrices
+                        ndims[i] = vec.ndim
+
+                    if max(ndims)<2:
+                        #No need to transpose scalars and vectors
+                        #If both elements are matrices, do not transpose -
+                        # we only make a vector-by-matrix compatible.
+                        return valid, ndims 
+                    if min(ndims)==2:
+                        return valid, ndims
+                    valid = True
+                    return valid, ndims
+
+                if not is_matmul(self):
+                    return
+                if not check_should_transpose(should_transpose):
+                    return
+                valid, ndims = is_valid_input(res)
+                if not valid:
+                    return
+                matrix_ind = ndims.index(2)
+                #Since ndims is a vector with 2 elements, 1-matrix_ind returns the other index
+                vector_ind = 1-matrix_ind
+                if res[vector_ind].shape[0] == res[matrix_ind].shape[matrix_ind]:
+                    res[matrix_ind] = res[matrix_ind].T
+
+            def should_transpose(curr_arg):
+                """
+                This is a helper function that determines if an element should be transposed for
+                matrix multiplication (allows broadcasting vectors into matrices)
+                """
+                if curr_arg[1]==VAR_TYPE.CONSTANT:
+                    return False
+                return curr_arg[0].ndim==1
+            res =  []
+            #In order to support dot products of vectors,
+            #where one vector is represented by a matrix, we need to see if:
+            #1. The operation is matmul
+            #2. between two vectors (variables/parameters with ndim==1)
+            #These checks should happen on the original variables/parameters,
+            #and NOT on the elements of args.
+            #So the transpose happens only if should_tranpose contains two Trues.
+            transposable_elements = []
+            #Iterate over the range instead of the dictionary directly:
+            #dictionaries have no order, but we must iterate in order
+            for ind in range(len(ind_to_value_type)): 
+                curr_arg = ind_to_value_type[ind]
+                if curr_arg[1]==VAR_TYPE.CONSTANT:
+                    res.append(curr_arg[0])
+                elif curr_arg[1]==VAR_TYPE.VARIABLE_PARAMETER:
+                    res.append(args[vars_dict.vars_dict[curr_arg[0]]])
+                else:
+                    rec_ind_to_value_type = _gen_consts_vars(curr_arg[0], vars_dict)
+                    res.append(wrapped_func(curr_arg[0], rec_ind_to_value_type, vars_dict, *args))
+                transposable_elements.append(should_transpose(curr_arg))
+            #If this is a matrix multiplicaiton operation between 2 elements, transpose the second.
+            #This helps with overloading this function to be used with matrices.
+            transpose_if_matmul(self, res, transposable_elements)
+            return self.apply_torch_numeric(res)
+        
+        vars_dict = VariablesDict(provided_vars_list=provided_vars_list)
+        ind_to_value_type = _gen_consts_vars(self, vars_dict)
+        return partial(wrapped_func, self, ind_to_value_type, vars_dict), vars_dict
---
+++ cvxpy/utilities/torch_utils.py
@@ -0,0 +1,45 @@
+"""
+Copyright 2013 Steven Diamond
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from enum import Enum
+
+VAR_TYPE = Enum("VAR_TYPE", "VARIABLE_PARAMETER CONSTANT EXPRESSION")
+
+class VariablesDict():
+    """ Helper class that contains a dictionary from a non-constant leaf to an index in *args,
+    with a safe add method.
+    If a list is given, will create the dictionary in the order of the list."""
+    def __init__(self, provided_vars_list: list=[]):
+        self.vars_dict = dict()
+        for var in provided_vars_list:
+            self.add_var(var)
+            
+    def add_var(self, var):
+        """
+        var is expected to be either a cp.Variable or a cp.Parameter, but is not enforced.
+        """
+        if var not in self.vars_dict:
+            self.vars_dict[var] = len(self.vars_dict)
+
+    def has_type_in_keys(self, look_type: type):
+        """
+        This function returns True if one of the keys of this variable is of a certain type,
+        and False otherwise
+        """
+        for var in self.vars_dict.keys():
+            if isinstance(var, look_type):
+                return True
+        return False
