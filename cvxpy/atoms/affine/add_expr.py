4902b2e1df95f457c38430611e223918a925629e
Steven Diamond <diamond.po.central@gmail.com>
2023-12-14 15:06:55 -0800

    Update add_expr.py

--- cvxpy/atoms/affine/add_expr.py
+++ cvxpy/atoms/affine/add_expr.py
@@ -42,7 +42,7 @@ class AddExpression(AffAtom):
         """
         return u.shape.sum_shapes([arg.shape for arg in self.args])
 
-    def expand_args(self, expr: List[Expression]) -> List:
+    def expand_args(self, expr: Expression) -> List[Expression]:
         """Helper function to extract the arguments from an AddExpression.
         """
         if isinstance(expr, AddExpression):
