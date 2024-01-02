a4b99afa3538133b919095e94a6761e9a1b9ebd5
Miles Lubin <miles.lubin@gmail.com>
2024-01-01 20:23:35 -0500

    Bump or-tools compat to 9.8

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -27,7 +27,7 @@ elif [[ "$PYTHON_VERSION" == "3.11" ]]; then
 fi
 
 if [[ "$PYTHON_VERSION" == "3.11" ]]; then
-  python -m pip install "ortools>=9.7,<9.8" gurobipy clarabel osqp piqp
+  python -m pip install "ortools>=9.7,<9.9" gurobipy clarabel osqp piqp
   if [[ "$RUNNER_OS" == "Windows" ]]; then
     # SDPA with OpenBLAS backend does not pass LP5 on Windows
     python -m pip install sdpa-multiprecision
