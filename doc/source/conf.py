f4bfe081395254a18b5e7598d5ecf0e8cd130251
Transurgeon <peter.zijie@gmail.com>
2024-02-10 23:00:45 -0500

    adding back daqp doc changes and other minor fixes

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -256,7 +256,7 @@ html_favicon = "_static/favicon.ico"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_css_files = ['css/extra_css.css']
+html_css_files = ['css/styling.css']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
