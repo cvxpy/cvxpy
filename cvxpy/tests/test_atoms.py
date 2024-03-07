6b396e7268d40c225b9cbbd4e70a4f248b038858
Transurgeon <peter.zijie@gmail.com>
2024-03-06 23:34:24 -0500

    removing unnecessary prints in one of the tests

--- cvxpy/tests/test_atoms.py
+++ cvxpy/tests/test_atoms.py
@@ -1270,18 +1270,15 @@ class TestAtoms(BaseTest):
         """Test that norm1 and normInf match definition for matrices.
         """
         A = np.array([[1, 2], [3, 4]])
-        print(A)
         X = Variable((2, 2))
         obj = Minimize(cp.norm(X, 1))
         prob = cp.Problem(obj, [X == A])
         result = prob.solve(solver=cp.SCS)
-        print(result)
         self.assertAlmostEqual(result, cp.norm(A, 1).value, places=3)
 
         obj = Minimize(cp.norm(X, np.inf))
         prob = cp.Problem(obj, [X == A])
         result = prob.solve(solver=cp.SCS)
-        print(result)
         self.assertAlmostEqual(result, cp.norm(A, np.inf).value, places=3)
 
     def test_indicator(self) -> None:
