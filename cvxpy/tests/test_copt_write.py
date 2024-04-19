d316142f2547d6141c45dc883265a803d9e1a1ec
phschiele <phschiele@gmail.com>
2024-04-18 17:58:26 -0700

    Don't write files to local folder

--- cvxpy/tests/test_copt_write.py
+++ cvxpy/tests/test_copt_write.py
@@ -1,5 +1,5 @@
 """
-Copyright 2022, the CVXPY authors
+Copyright, the CVXPY authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,6 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
+
 import os
 import unittest
 
@@ -20,31 +22,27 @@ import numpy as np
 
 import cvxpy as cp
 from cvxpy.reductions.solvers.defines import INSTALLED_SOLVERS
-from cvxpy.tests.base_test import BaseTest
 
 
 @unittest.skipUnless('COPT' in INSTALLED_SOLVERS, 'COPT is not installed.')
-class TestCOPTWrite(BaseTest):
-    def test_write(self) -> None:
-        """Test the COPT model.write().
-        """
-        import py
+def test_write(tmpdir):
+    """Test the COPT model.write().
+    """
 
-        tmpdir = py.path.local()
-        tmpfile = tmpdir.mkdir("outfiles").join("copt_model.bin")
-        tmpname = str(tmpfile)
+    filename = "copt_model.bin"
+    path = os.path.join(tmpdir, filename)
 
-        m = 20
-        n = 15
-        np.random.seed(0)
-        A = np.random.randn(m, n)
-        b = np.random.randn(m)
+    m = 20
+    n = 15
+    np.random.seed(0)
+    A = np.random.randn(m, n)
+    b = np.random.randn(m)
 
-        x = cp.Variable(n)
-        cost = cp.sum_squares(A @ x - b)
-        prob = cp.Problem(cp.Minimize(cost))
-        prob.solve(solver=cp.COPT,
-                   verbose=True,
-                   save_file=tmpname)
+    x = cp.Variable(n)
+    cost = cp.sum_squares(A @ x - b)
+    prob = cp.Problem(cp.Minimize(cost))
+    prob.solve(solver=cp.COPT,
+                verbose=True,
+                save_file=path)
 
-        assert os.path.exists(tmpname)
+    assert os.path.exists(path)
