230d147c2aad3f6530fde04e0debe9a1b6f0b881
PTNobel <PTNobel@users.noreply.github.com>
2024-05-09 05:22:14 +0000

    Deploying to gh-pages from @ cvxpy/cvxpy@44994f81ed16bd63350f1377db0b2c1617a7c8a1 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.3',
+    VERSION: '1.5.1.dev0+0.44994f8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
