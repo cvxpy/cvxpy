48923f253530ccb41a62b6595d1f3ba03e1b4979
Philipp Schiele <44360364+phschiele@users.noreply.github.com>
2024-05-10 20:53:56 +0000

    Fixes typing

--- cvxpy/atoms/affine/add_expr.py
+++ cvxpy/atoms/affine/add_expr.py
@@ -15,13 +15,14 @@ limitations under the License.
 """
 import operator as op
 from functools import reduce
-from typing import List, Tuple
+from typing import Iterable, List, Tuple
 
 import cvxpy.lin_ops.lin_op as lo
 import cvxpy.lin_ops.lin_utils as lu
 import cvxpy.utilities as u
 from cvxpy.atoms.affine.affine_atom import AffAtom
 from cvxpy.constraints.constraint import Constraint
+from cvxpy.expressions.expression import Expression
 
 
 class AddExpression(AffAtom):
