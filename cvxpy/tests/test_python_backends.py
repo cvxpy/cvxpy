79fd4b201b70eee192c9f37ff23dfda857f5db24
William Zijie Zhang <willizz@Williams-MacBook-Pro.local>
2024-06-14 14:46:18 -0700

    moving test to new class

--- cvxpy/tests/test_python_backends.py
+++ cvxpy/tests/test_python_backends.py
@@ -1085,98 +1085,6 @@ class TestBackends:
         assert view.combine_potentially_none(None, a) == a
         assert view.combine_potentially_none(a, b) == view.add_dicts(a, b)
 
-    def test_nd_sum_entries(self, backend):
-        """
-        define x = Variable((2,2,2)) with
-        [[[x111, x112],
-        [x121, x122]],
-
-        [[x211, x212],
-        [x221, x222]]]
-
-        x is represented as eye(8) in the A matrix (in column-major order), i.e.,
-
-        x111 x211 x121 x221 x112 x212 x122 x222
-        [[1   0   0   0   0   0   0   0],
-         [0   1   0   0   0   0   0   0],
-         [0   0   1   0   0   0   0   0],
-         [0   0   0   1   0   0   0   0],
-         [0   0   0   0   1   0   0   0],
-         [0   0   0   0   0   1   0   0],
-         [0   0   0   0   0   0   1   0],
-         [0   0   0   0   0   0   0   1]]
-
-        sum(x, axis = 0) means we only consider entries in a given axis (axes)
-
-        which, when using the same columns as before, now maps to
-        
-        sum(x, axis = 0)
-        x111 x211 x121 x221 x112 x212 x122 x222
-        [[1   1   0   0   0   0   0   0],
-         [0   0   1   1   0   0   0   0],
-         [0   0   0   0   1   1   0   0],
-         [0   0   0   0   0   0   1   1]]
-
-        sum(x, axis = 1)
-        x111 x211 x121 x221 x112 x212 x122 x222
-        [[1   0   1   0   0   0   0   0],
-         [0   1   0   1   0   0   0   0],
-         [0   0   0   0   1   0   1   0],
-         [0   0   0   0   0   1   0   1]]
-
-        sum(x, axis = 2)
-        x111 x211 x121 x221 x112 x212 x122 x222
-        [[1   0   0   0   1   0   0   0],
-         [0   1   0   0   0   1   0   0],
-         [0   0   1   0   0   0   1   0],
-         [0   0   0   1   0   0   0   1]]
-        
-        sum(x, axis = (0,1))
-        x111 x211 x121 x221 x112 x212 x122 x222
-        [[1   1   1   1   0   0   0   0],
-         [0   0   0   0   1   1   1   1]]
-
-        sum(x, axis = (0,2))
-        x111 x211 x121 x221 x112 x212 x122 x222
-        [[1   1   0   0   1   1   0   0],
-         [0   0   1   1   0   0   1   1]]
-
-        sum(x, axis = (1,2))
-        x111 x211 x121 x221 x112 x212 x122 x222
-        [[1   0   1   0   1   0   1   0],
-         [0   1   0   1   0   1   0   1]]
-
-        To reproduce the outputs above, eliminate the given axis (axes)
-        and put ones where the remaining axes (axis) match. 
-
-        Note: sum(x, keepdims=True) is equivalent to sum(x, keepdims=False)
-        with a reshape, which is NO-OP in the backend.
-        """
-
-        variable_lin_op = linOpHelper((2, 2, 2), type="variable", data=1)
-        view = backend.process_constraint(variable_lin_op, backend.get_empty_view())
-
-        # cast to numpy
-        view_A = view.get_tensor_representation(0, 8)
-        view_A = sp.coo_matrix((view_A.data, (view_A.row, view_A.col)), shape=(8, 8)).toarray()
-        assert np.all(view_A == np.eye(8))
-
-        sum_lin_op = linOpHelper(shape=(2, 2, 2), data=[2, True], args=[variable_lin_op])
-        out_view = backend.sum_entries(sum_lin_op, view)
-        A = out_view.get_tensor_representation(0, 4)
-
-        # cast to numpy
-        A = sp.coo_matrix((A.data, (A.row, A.col)), shape=(4, 8)).toarray()
-        expected = np.array([[1, 0, 0, 0, 1, 0, 0, 0],
-                             [0, 1, 0, 0, 0, 1, 0, 0],
-                             [0, 0, 1, 0, 0, 0, 1, 0],
-                             [0, 0, 0, 1, 0, 0, 0, 1]])
-        assert np.all(A == expected)
-
-        # Note: view is edited in-place:
-        assert out_view.get_tensor_representation(0, 4) == view.get_tensor_representation(0, 4)
-
-
 class TestParametrizedBackends:
     @staticmethod
     @pytest.fixture(params=backends)
@@ -1861,6 +1769,116 @@ class TestParametrizedBackends:
         assert out_view.get_tensor_representation(0, 1) == param_var_view.get_tensor_representation(
             0, 1
         )
+
+class TestND_Backends:
+    @staticmethod
+    @pytest.fixture(params=backends)
+    def backend(request):
+        # Not used explicitly in most test cases.
+        # Some tests specify other values as needed within the test case.
+        kwargs = {
+            "id_to_col": {1: 0, 2: 2},
+            "param_to_size": {-1: 1, 3: 1},
+            "param_to_col": {3: 0, -1: 1},
+            "param_size_plus_one": 2,
+            "var_length": 4,
+        }
+
+        backend = CanonBackend.get_backend(request.param, **kwargs)
+        assert isinstance(backend, PythonCanonBackend)
+        return backend
+    
+
+    def test_nd_sum_entries(self, backend):
+        """
+        define x = Variable((2,2,2)) with
+        [[[x111, x112],
+        [x121, x122]],
+
+        [[x211, x212],
+        [x221, x222]]]
+
+        x is represented as eye(8) in the A matrix (in column-major order), i.e.,
+
+        x111 x211 x121 x221 x112 x212 x122 x222
+        [[1   0   0   0   0   0   0   0],
+         [0   1   0   0   0   0   0   0],
+         [0   0   1   0   0   0   0   0],
+         [0   0   0   1   0   0   0   0],
+         [0   0   0   0   1   0   0   0],
+         [0   0   0   0   0   1   0   0],
+         [0   0   0   0   0   0   1   0],
+         [0   0   0   0   0   0   0   1]]
+
+        sum(x, axis = 0) means we only consider entries in a given axis (axes)
+
+        which, when using the same columns as before, now maps to
+        
+        sum(x, axis = 0)
+        x111 x211 x121 x221 x112 x212 x122 x222
+        [[1   1   0   0   0   0   0   0],
+         [0   0   1   1   0   0   0   0],
+         [0   0   0   0   1   1   0   0],
+         [0   0   0   0   0   0   1   1]]
+
+        sum(x, axis = 1)
+        x111 x211 x121 x221 x112 x212 x122 x222
+        [[1   0   1   0   0   0   0   0],
+         [0   1   0   1   0   0   0   0],
+         [0   0   0   0   1   0   1   0],
+         [0   0   0   0   0   1   0   1]]
+
+        sum(x, axis = 2)
+        x111 x211 x121 x221 x112 x212 x122 x222
+        [[1   0   0   0   1   0   0   0],
+         [0   1   0   0   0   1   0   0],
+         [0   0   1   0   0   0   1   0],
+         [0   0   0   1   0   0   0   1]]
+        
+        sum(x, axis = (0,1))
+        x111 x211 x121 x221 x112 x212 x122 x222
+        [[1   1   1   1   0   0   0   0],
+         [0   0   0   0   1   1   1   1]]
+
+        sum(x, axis = (0,2))
+        x111 x211 x121 x221 x112 x212 x122 x222
+        [[1   1   0   0   1   1   0   0],
+         [0   0   1   1   0   0   1   1]]
+
+        sum(x, axis = (1,2))
+        x111 x211 x121 x221 x112 x212 x122 x222
+        [[1   0   1   0   1   0   1   0],
+         [0   1   0   1   0   1   0   1]]
+
+        To reproduce the outputs above, eliminate the given axis (axes)
+        and put ones where the remaining axes (axis) match. 
+
+        Note: sum(x, keepdims=True) is equivalent to sum(x, keepdims=False)
+        with a reshape, which is NO-OP in the backend.
+        """
+
+        variable_lin_op = linOpHelper((2, 2, 2), type="variable", data=1)
+        view = backend.process_constraint(variable_lin_op, backend.get_empty_view())
+
+        # cast to numpy
+        view_A = view.get_tensor_representation(0, 8)
+        view_A = sp.coo_matrix((view_A.data, (view_A.row, view_A.col)), shape=(8, 8)).toarray()
+        assert np.all(view_A == np.eye(8))
+
+        sum_lin_op = linOpHelper(shape=(2, 2, 2), data=[2, True], args=[variable_lin_op])
+        out_view = backend.sum_entries(sum_lin_op, view)
+        A = out_view.get_tensor_representation(0, 4)
+
+        # cast to numpy
+        A = sp.coo_matrix((A.data, (A.row, A.col)), shape=(4, 8)).toarray()
+        expected = np.array([[1, 0, 0, 0, 1, 0, 0, 0],
+                             [0, 1, 0, 0, 0, 1, 0, 0],
+                             [0, 0, 1, 0, 0, 0, 1, 0],
+                             [0, 0, 0, 1, 0, 0, 0, 1]])
+        assert np.all(A == expected)
+
+        # Note: view is edited in-place:
+        assert out_view.get_tensor_representation(0, 4) == view.get_tensor_representation(0, 4)
 
 
 class TestNumPyBackend:
