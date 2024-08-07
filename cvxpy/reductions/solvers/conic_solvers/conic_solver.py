7ab2e56f900679cb5dfee0c2b5556eb6c8e512b0
William Zijie Zhang <willizz@Williams-MacBook-Pro.local>
2024-08-07 16:01:02 -0700

    Revert "changing built-in divmod to numpy and specifying dtype (#2494)"
    This reverts commit eb579b9f76a20a99cb053a2f1d8a6365a2682293.

--- cvxpy/reductions/solvers/conic_solvers/conic_solver.py
+++ cvxpy/reductions/solvers/conic_solvers/conic_solver.py
@@ -261,8 +261,9 @@ class ConicSolver(Solver):
             # this is equivalent to but _much_ faster than:
             #    restruct_mat_rep = sp.block_diag([restruct_mat]*(problem.x.size + 1))
             #    restruct_A = restruct_mat_rep * problem.A
-            unspecified, _ = np.divmod(problem.A.shape[0] * problem.A.shape[1],
-                                        restruct_mat.shape[1], dtype=np.int64)
+            unspecified, remainder = divmod(problem.A.shape[0] *
+                                            problem.A.shape[1],
+                                            restruct_mat.shape[1])
             reshaped_A = problem.A.reshape(restruct_mat.shape[1],
                                            unspecified, order='F').tocsr()
             restructured_A = restruct_mat(reshaped_A).tocoo()
