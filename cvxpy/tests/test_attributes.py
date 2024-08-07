8085fb6b2a2f9b1fb3c5148c3118916ffa9f9c56
William Zijie Zhang <willizz@Williams-MacBook-Pro.local>
2024-08-07 13:11:04 -0700

    adding more tests for multiple attributes and combinations

--- cvxpy/tests/test_attributes.py
+++ cvxpy/tests/test_attributes.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 import cvxpy as cp
+from cvxpy.constraints.nonpos import Inequality
 from cvxpy.constraints.psd import PSD
+from cvxpy.constraints.zero import Equality
 from cvxpy.reductions.cvx_attr2constr import CvxAttr2Constr
 
 
@@ -11,6 +13,10 @@ class Test_Attributes():
         x = cp.Variable(shape=(2,2), symmetric=True, nonneg=True, integer=True)
         target = np.array(np.eye(2) * 5)
         prob = cp.Problem(cp.Minimize(0), [x == target])
+        new_prob = CvxAttr2Constr(prob, reduce_bounds=True)
+        assert type(new_prob.apply(prob)[0].constraints[0]) is Inequality
+        assert type(new_prob.apply(prob)[0].constraints[1]) is Equality
+
         prob.solve()
         assert np.allclose(x.value, target)
 
@@ -18,13 +24,37 @@ class Test_Attributes():
         x = cp.Variable(shape=(2,2), PSD=True, nonneg=True)
         target = np.array(np.eye(2) * 5)
         prob = cp.Problem(cp.Minimize(0), [x == target])
+        new_prob = CvxAttr2Constr(prob, reduce_bounds=True)
+        assert type(new_prob.apply(prob)[0].constraints[0]) is PSD
+        assert type(new_prob.apply(prob)[0].constraints[1]) is Inequality
+        assert type(new_prob.apply(prob)[0].constraints[2]) is Equality
+
         prob.solve()
         assert np.allclose(x.value, target)
 
-    def test_nonneg_NSD(self) -> None:
+    def test_nonpos_NSD(self) -> None:
         x = cp.Variable(shape=(2,2), NSD=True, nonpos=True)
         target = np.array(np.eye(2) * 5)
         prob = cp.Problem(cp.Minimize(0), [x == -target])
 
-        new_prob = CvxAttr2Constr(prob)
+        new_prob = CvxAttr2Constr(prob, reduce_bounds=True)
         assert type(new_prob.apply(prob)[0].constraints[0]) is PSD
+        assert type(new_prob.apply(prob)[0].constraints[1]) is Inequality
+        assert type(new_prob.apply(prob)[0].constraints[2]) is Equality
+
+        prob.solve()
+        assert np.allclose(x.value, -target)
+
+    def test_integer_bounds(self) -> None:
+        x = cp.Variable(shape=(2,2), integer=True, bounds=[0, 2])
+        target = np.array(np.eye(2))
+        prob = cp.Problem(cp.Minimize(0), [x == target])
+
+        new_prob = CvxAttr2Constr(prob, reduce_bounds=True)
+        assert type(new_prob.apply(prob)[0].constraints[0]) is Inequality
+        assert type(new_prob.apply(prob)[0].constraints[1]) is Inequality
+        assert type(new_prob.apply(prob)[0].constraints[2]) is Equality
+
+        prob.solve()
+        assert np.allclose(x.value, target)
+        
