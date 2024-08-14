c135859e11de973085b0087c890c763c6146d9fc
Amit Solomon <as3993@princeton.edu>
2024-08-14 11:01:23 +0300

    Made tensor_reshape_fortran more compact based on Transurgeon's comment.

--- cvxpy/utilities/torch_utils.py
+++ cvxpy/utilities/torch_utils.py
@@ -63,6 +63,9 @@ def gen_tensor(value, dtype=torch.float64) -> torch.Tensor:
 def tensor_reshape_fortran(value: torch.Tensor, shape: tuple) -> torch.Tensor:
     """This function reshapes a tensor in Fortran order (similar to numpy.reshape with order="F").
     This functionality is not included in Pytorch."""
-    reverse_shape = list(shape)
-    reverse_shape.reverse() #reverse a list in place
-    return torch.reshape(value.reshape(reverse_shape).t(), shape=shape)
+    # reverse_shape = list(shape)
+    # reverse_shape.reverse() #reverse a list in place
+    # return torch.reshape(value.reshape(reverse_shape).t(), shape=shape)
+    # A more compact solution based on
+    # https://stackoverflow.com/questions/64433896/pytorch-equivalent-of-numpy-reshape-function.
+    return torch.reshape(value.T, shape[::-1]).T
