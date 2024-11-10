131ea95086661733f83be6078599e6b5f9331125
William Zijie Zhang <89562186+Transurgeon@users.noreply.github.com>
2024-11-10 03:12:01 +0000

    release branch for 1.6.x

--- setup/versioning.py
+++ setup/versioning.py
@@ -34,8 +34,8 @@ import subprocess
 MAJOR = 1
 MINOR = 6
 MICRO = 0
-IS_RELEASED = False
-IS_RELEASE_BRANCH = False
+IS_RELEASED = True
+IS_RELEASE_BRANCH = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
