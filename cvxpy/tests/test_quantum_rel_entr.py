d4bbea7e9244930ff65082d1149aa266ab134204
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-21 10:07:30 +0400

    skipping test_1 for the Windows, Python 3.13 combination

--- cvxpy/tests/test_quantum_rel_entr.py
+++ cvxpy/tests/test_quantum_rel_entr.py
@@ -151,7 +151,7 @@ class TestQuantumRelEntr:
         return sth
 
     @pytest.mark.skipif(
-        platform.system() == "windows-2022" and sys.version_info == (3, 13),
+        platform.system() == "Windows" and sys.version_info == (3, 13),
         reason="This test is skipped on Windows with Python version 3.13",
     )
     def test_1(self):
