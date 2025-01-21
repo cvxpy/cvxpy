d18e224178459ac5cb6ce15c8dd2830ec1381fb6
Parth Nobel <ptnobel@stanford.edu>
2025-01-21 14:18:54 -0800

    Fixes regex

--- cvxpy/tests/test_attributes.py
+++ cvxpy/tests/test_attributes.py
@@ -89,7 +89,8 @@ class TestAttributes:
             A.value = np.ones((3, 3))
         with pytest.raises(
             ValueError,
-            match=r'Invalid sparsity pattern \(array\(\[0\][, dtype=int32]?\), array\(\[0\][, dtype=int32]?\)\)'
+            match='Invalid sparsity pattern '
+                  r'\(array\(\[0\](, dtype=int32)?\), array\(\[0\](, dtype=int32)?\)\)'
                   ' for Parameter value.'
         ):
             A.value_sparse = sp.coo_array(([1], ([0], [0])), (3, 3))
