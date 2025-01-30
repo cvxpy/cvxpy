0d4ae3307cfcd3986f17fdb17ac39eb0f2b8f78f
William Zijie Zhang <89562186+Transurgeon@users.noreply.github.com>
2025-01-30 12:26:45 -0500

    Updating SciPy dependency to >=1.11.0 and version for wheels to >=1.13.0 (#2690)
    * bumping up scipy dependency
    
    * fixing CI issues
    
    * adding new case for skipping cvxopt on windows
    
    * changing cvxopt to install through conda
    
    * avoid installing cvxopt for python313
    
    ---------
    
    Co-authored-by: William Zijie Zhang <william@gridmatic.com>

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -12,7 +12,7 @@ conda config --set remote_read_timeout_secs 120.0
 conda install mkl pip pytest pytest-cov hypothesis openblas "setuptools>65.5.1"
 
 if [[ "$PYTHON_VERSION" != "3.13" ]]; then
-  conda install ecos scs cvxopt proxsuite daqp
+  conda install ecos scs proxsuite daqp
   python -m pip install coptpy==7.1.7 gurobipy piqp clarabel osqp highspy
 else
   # only install the essential solvers for Python 3.13.
@@ -20,17 +20,11 @@ else
   python -m pip install clarabel osqp
 fi
 
-if [[ "$PYTHON_VERSION" == "3.12" ]]; then
-  # The earliest version of numpy that works is 1.26.4
-  # Given numpy 1.26.4, the earliest version of scipy we can use is 1.11.3.
-  conda install scipy=1.11.3 numpy=1.26.4
-elif [[ "$PYTHON_VERSION" == "3.13" ]]; then
-  # Install newest versions for Python 3.13.
+# Install newest stable versions for Python 3.13.
+if [[ "$PYTHON_VERSION" == "3.13" ]]; then
   conda install scipy numpy
 else
-  # Lower versions of Python all install scipy 1.9.3 and numpy 1.23.4.
-  # This change is necessary for compatibility with the scipy.array interface.
-  conda install scipy=1.9.3 numpy=1.23.4
+  conda install scipy=1.13.0 numpy=1.26.4
 fi
 
 if [[ "$PYTHON_VERSION" == "3.11" ]]; then
@@ -42,9 +36,13 @@ if [[ "$RUNNER_OS" == "Windows" ]] && [[ "$PYTHON_VERSION" != "3.13" ]]; then
   python -m pip install sdpa-multiprecision
 fi
 
+if [[ "$RUNNER_OS" != "Windows" ]] && [[ "$PYTHON_VERSION" != "3.13" ]]; then
+  conda install cvxopt
+fi
+
 if [[ "$PYTHON_VERSION" == "3.12" ]] && [[ "$RUNNER_OS" != "Windows" ]]; then
   # cylp has no wheels for Windows
-  python -m pip install cylp pyscipopt==5.2.1 diffcp
+  python -m pip install cylp pyscipopt==5.2.1
 fi
 
 if [[ "$PYTHON_VERSION" == "3.10" ]] && [[ "$RUNNER_OS" != "Windows" ]]; then
