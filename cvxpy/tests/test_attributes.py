6fe957cbdd7a7aaf6d8b5ccd0f149bb27881b478
William Zijie Zhang <william@gridmatic.com>
2025-01-30 22:46:45 -0500

    adding previous test for multiple attributes

--- cvxpy/tests/test_attributes.py
+++ cvxpy/tests/test_attributes.py
@@ -3,9 +3,14 @@ import pytest
 import scipy.sparse as sp
 
 import cvxpy as cp
+from cvxpy.constraints.nonpos import Inequality
+from cvxpy.constraints.psd import PSD
+from cvxpy.constraints.zero import Equality
+from cvxpy.reductions.cvx_attr2constr import CvxAttr2Constr
 
 
 class TestAttributes:
+
     @pytest.mark.parametrize("sparsity", [[np.array([0, 0]), np.array([0, 1])], [(0, 1), (0, 2)]])
     def test_sparsity_pattern(self, sparsity):
         X = cp.Variable((3, 3), sparsity=sparsity)
@@ -128,3 +133,55 @@ class TestAttributes:
             ValueError, match="np.inf is not feasible as a lower bound."
         ):
             x = cp.Variable((2, 2), name="x", bounds=bounds)
+
+
+class TestMultipleAttributes:
+
+    def test_multiple_attributes(self) -> None:
+        x = cp.Variable(shape=(2,2), symmetric=True, nonneg=True, integer=True)
+        target = np.array(np.eye(2) * 5)
+        prob = cp.Problem(cp.Minimize(0), [x == target])
+        new_prob = CvxAttr2Constr(prob, reduce_bounds=True)
+        assert type(new_prob.apply(prob)[0].constraints[0]) is Inequality
+        assert type(new_prob.apply(prob)[0].constraints[1]) is Equality
+
+        prob.solve()
+        assert np.allclose(x.value, target)
+
+    def test_nonneg_PSD(self) -> None:
+        x = cp.Variable(shape=(2,2), PSD=True, nonneg=True)
+        target = np.array(np.eye(2) * 5)
+        prob = cp.Problem(cp.Minimize(0), [x == target])
+        new_prob = CvxAttr2Constr(prob, reduce_bounds=True)
+        assert type(new_prob.apply(prob)[0].constraints[0]) is PSD
+        assert type(new_prob.apply(prob)[0].constraints[1]) is Inequality
+        assert type(new_prob.apply(prob)[0].constraints[2]) is Equality
+
+        prob.solve()
+        assert np.allclose(x.value, target)
+
+    def test_nonpos_NSD(self) -> None:
+        x = cp.Variable(shape=(2,2), NSD=True, nonpos=True)
+        target = np.array(np.eye(2) * 5)
+        prob = cp.Problem(cp.Minimize(0), [x == -target])
+
+        new_prob = CvxAttr2Constr(prob, reduce_bounds=True)
+        assert type(new_prob.apply(prob)[0].constraints[0]) is PSD
+        assert type(new_prob.apply(prob)[0].constraints[1]) is Inequality
+        assert type(new_prob.apply(prob)[0].constraints[2]) is Equality
+
+        prob.solve()
+        assert np.allclose(x.value, -target)
+
+    def test_integer_bounds(self) -> None:
+        x = cp.Variable(shape=(2,2), integer=True, bounds=[0, 2])
+        target = np.array(np.eye(2))
+        prob = cp.Problem(cp.Minimize(0), [x == target])
+
+        new_prob = CvxAttr2Constr(prob, reduce_bounds=True)
+        assert type(new_prob.apply(prob)[0].constraints[0]) is Inequality
+        assert type(new_prob.apply(prob)[0].constraints[1]) is Inequality
+        assert type(new_prob.apply(prob)[0].constraints[2]) is Equality
+
+        prob.solve()
+        assert np.allclose(x.value, target)
