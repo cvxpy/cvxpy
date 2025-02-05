adding some initial ideas to fix broadcasting

cvxpy/expressions/expression.py
@@ -17,7 +17,7 @@ limitations under the License.
 import abc
 import warnings
 from functools import wraps
-from typing import List, Literal, Tuple
+from typing import List, Literal, Tuple, Union
 
 import numpy as np
 
@@ -109,7 +109,7 @@ __BINARY_EXPRESSION_UFUNCS__ = {
 }
 
 
-ExpressionLike = "Expression | np.typing.ArrayLike"
+ExpressionLike = Union["Expression", np.typing.ArrayLike]
 
 
 class Expression(u.Canonical):
@@ -591,19 +591,13 @@ class Expression(u.Canonical):
         elif rh_expr.is_scalar() and not lh_expr.is_scalar():
             rh_expr = cvxtypes.promote()(rh_expr, lh_expr.shape)
         # Broadcasting.
-        if lh_expr.ndim == 2 and rh_expr.ndim == 2:
+        if lh_expr.ndim >= 2 and rh_expr.ndim >= 2:
             # Replicate dimensions of size 1.
-            dims = [max(lh_expr.shape[i], rh_expr.shape[i]) for i in range(2)]
-            # Broadcast along dim 0.
-            if lh_expr.shape[0] == 1 and lh_expr.shape[0] < dims[0]:
-                lh_expr = np.ones((dims[0], 1)) @ lh_expr
-            if rh_expr.shape[0] == 1 and rh_expr.shape[0] < dims[0]:
-                rh_expr = np.ones((dims[0], 1)) @ rh_expr
-            # Broadcast along dim 1.
-            if lh_expr.shape[1] == 1 and lh_expr.shape[1] < dims[1]:
-                lh_expr = lh_expr @ np.ones((1, dims[1]))
-            if rh_expr.shape[1] == 1 and rh_expr.shape[1] < dims[1]:
-                rh_expr = rh_expr @ np.ones((1, dims[1]))
+            output_shape = np.broadcast_shapes(lh_expr.shape, rh_expr.shape)
+            if lh_expr.shape != output_shape:
+                lh_expr = np.ones(np.prod(output_shape)/np.prod(lh_expr.shape)) @ lh_expr
+            if rh_expr.shape != output_shape:
+                rh_expr = np.ones(np.prod(output_shape)/np.prod(rh_expr.shape)) @ rh_expr
         return lh_expr, rh_expr
 
     @_cast_other

cvxpy/tests/test_expressions.py
@@ -20,7 +20,12 @@ import numpy as np
 import pytest
 import scipy.sparse as sp
 from hypothesis import assume, given
-from hypothesis.extra.numpy import arrays, basic_indices, integer_array_indices
+from hypothesis.extra.numpy import (
+    arrays,
+    basic_indices,
+    broadcastable_shapes,
+    integer_array_indices,
+)
 
 import cvxpy as cp
 import cvxpy.interface.matrix_utilities as intf
@@ -1653,7 +1658,7 @@ class TestND_Expressions():
         assert np.allclose(expr.value, y)
 
     @given(axis=basic_indices(shape=(2,2,2), allow_newaxis=True))
-    def test_nd__basic_index(self, axis) -> None:
+    def test_nd_basic_index(self, axis) -> None:
         # Skip examples with 0-d output. TODO allow 0-d expressions in cvxpy.
         def is_zero_dim_output(axis):
             return 0 in self.target[axis].shape
@@ -1713,3 +1718,12 @@ class TestND_Expressions():
         prob = cp.Problem(self.obj, [expr == y])
         prob.solve(canon_backend=cp.SCIPY_CANON_BACKEND)
         assert np.allclose(expr.value, y)
+
+    @given(shapes=broadcastable_shapes(shape=(2, 1, 1)))
+    def test_nd_multiply_broadcast(self, shapes) -> None:
+        var = cp.Variable((2, 1, 1))
+        for shape in shapes:
+            data = np.random.randn(shape)
+            var = cp.multiply(var, data)
+        prob = cp.Problem(cp.Minimize(cp.sum(var)), [])
+        prob.solve()