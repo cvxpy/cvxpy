ab88082247b6589754f770a6836f19c408b4c9d5
govindchari <govindchari1@gmail.com>
2025-02-12 20:49:29 -0800

    Added workflow to install and test optional sovlers

--- continuous_integration/install_dependencies.sh
+++ continuous_integration/install_dependencies.sh
@@ -11,52 +11,14 @@ conda config --set remote_backoff_factor 2
 conda config --set remote_read_timeout_secs 120.0
 conda install mkl pip pytest pytest-cov hypothesis openblas "setuptools>65.5.1"
 
-if [[ "$PYTHON_VERSION" != "3.13" ]]; then
-  conda install ecos scs proxsuite daqp
-  python -m pip install coptpy==7.1.7 gurobipy piqp clarabel osqp highspy
-else
-  # only install the essential solvers for Python 3.13.
-  conda install scs
-  python -m pip install clarabel osqp
-fi
+conda install scs
+python -m pip install clarabel osqp
 
 # Install newest stable versions for Python 3.13.
 if [[ "$PYTHON_VERSION" == "3.13" ]]; then
   conda install scipy numpy
 else
   conda install scipy=1.13.0 numpy=1.26.4
-fi
-
-if [[ "$PYTHON_VERSION" == "3.11" ]]; then
-  python -m pip install cplex "ortools>=9.7,<9.12"
-fi
-
-if [[ "$RUNNER_OS" == "Windows" ]] && [[ "$PYTHON_VERSION" != "3.13" ]]; then
-  # SDPA with OpenBLAS backend does not pass LP5 on Windows
-  python -m pip install sdpa-multiprecision
-fi
-
-if [[ "$RUNNER_OS" != "Windows" ]] && [[ "$PYTHON_VERSION" != "3.13" ]]; then
-  conda install cvxopt
-fi
-
-if [[ "$PYTHON_VERSION" == "3.12" ]] && [[ "$RUNNER_OS" != "Windows" ]]; then
-  # cylp has no wheels for Windows
-  python -m pip install cylp pyscipopt==5.2.1
-fi
-
-if [[ "$PYTHON_VERSION" == "3.10" ]] && [[ "$RUNNER_OS" != "Windows" ]]; then
-  # SDPA didn't pass LP5 on Ubuntu for Python 3.9 and 3.12
-  python -m pip install sdpa-python
-fi
-
-if [[ "$PYTHON_VERSION" == "3.11" ]] && [[ "$RUNNER_OS" != "macOS" ]]; then
-  python -m pip install xpress==9.4.3
-fi
-
-# Only install Mosek if license is available (secret is not copied to forks)
-if [[ -n "$MOSEK_CI_BASE64" ]] && [[ "$PYTHON_VERSION" != "3.13" ]]; then
-    python -m pip install mosek
 fi
 
 if [[ "$USE_OPENMP" == "True" ]]; then
---
+++ continuous_integration/install_optional_solvers.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+conda config --set remote_connect_timeout_secs 30.0
+conda config --set remote_max_retries 10
+conda config --set remote_backoff_factor 2
+conda config --set remote_read_timeout_secs 120.0
+
+if [[ "$PYTHON_VERSION" != "3.13" ]]; then
+  pip install ecos scs proxsuite daqp
+  python -m pip install coptpy==7.1.7 gurobipy piqp clarabel osqp highspy
+else
+  # only install the essential solvers for Python 3.13.
+  pip install scs
+  python -m pip install clarabel osqp
+fi
+
+if [[ "$PYTHON_VERSION" == "3.11" ]]; then
+  python -m pip install cplex "ortools>=9.7,<9.12"
+fi
+
+if [[ "$RUNNER_OS" != "Windows" ]]; then
+  # qoco has no wheels for Windows
+  python -m pip install qoco
+fi
+
+if [[ "$RUNNER_OS" == "Windows" ]] && [[ "$PYTHON_VERSION" != "3.13" ]]; then
+  # SDPA with OpenBLAS backend does not pass LP5 on Windows
+  python -m pip install sdpa-multiprecision
+fi
+
+if [[ "$RUNNER_OS" != "Windows" ]] && [[ "$PYTHON_VERSION" != "3.13" ]]; then
+  python -m pip install cvxopt
+fi
+
+if [[ "$PYTHON_VERSION" == "3.12" ]] && [[ "$RUNNER_OS" != "Windows" ]]; then
+  # cylp has no wheels for Windows
+  python -m pip install cylp pyscipopt==5.2.1
+fi
+
+if [[ "$PYTHON_VERSION" == "3.10" ]] && [[ "$RUNNER_OS" != "Windows" ]]; then
+  # SDPA didn't pass LP5 on Ubuntu for Python 3.9 and 3.12
+  python -m pip install sdpa-python
+fi
+
+if [[ "$PYTHON_VERSION" == "3.11" ]] && [[ "$RUNNER_OS" != "macOS" ]]; then
+  python -m pip install xpress==9.4.3
+fi
+
+# Only install Mosek if license is available (secret is not copied to forks)
+if [[ -n "$MOSEK_CI_BASE64" ]] && [[ "$PYTHON_VERSION" != "3.13" ]]; then
+    python -m pip install mosek
+fi
