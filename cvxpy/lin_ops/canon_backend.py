fixing implementation for broadcast_to atom, and cleaning up tests

cvxpy/lin_ops/canon_backend.py
@@ -780,14 +780,11 @@ class NumPyCanonBackend(PythonCanonBackend):
         """
         Broadcast view by repeating along axis 1 (rows).
         """
-        num_entries = int(np.prod(lin.shape))
-        current_dim = int(np.prod(lin.args[0].shape))
-        times = num_entries // current_dim
-
-        def func(x):
-            return np.tile(x, (1, times, 1))
-
-        view.apply_all(func)
+        broadcast_shape = lin.shape
+        original_shape = lin.args[0].shape
+        rows = np.arange(np.prod(original_shape, dtype=int)).reshape(original_shape, order='F')
+        rows = np.broadcast_to(rows, broadcast_shape).flatten(order="F")
+        view.select_rows(rows)
         return view
     
     def mul_elem(self, lin: LinOp, view: NumPyTensorView) -> NumPyTensorView:
@@ -1237,15 +1234,8 @@ class SciPyCanonBackend(PythonCanonBackend):
         """
         broadcast_shape = lin.shape
         original_shape = lin.args[0].shape
-        if len(broadcast_shape) != len(original_shape):
-            diff = len(broadcast_shape) - len(original_shape)
-            original_shape = (1,) * diff + original_shape
-        rows = np.arange(np.prod(original_shape, dtype=int)).astype(int)
-        for a,b in zip(original_shape[:-1], broadcast_shape[:-1]):
-            if a != b:
-                rows = np.repeat(rows, b)
-        if original_shape[-1] != broadcast_shape[-1]:
-            rows = np.tile(rows, broadcast_shape[-1])
+        rows = np.arange(np.prod(original_shape, dtype=int)).reshape(original_shape, order='F')
+        rows = np.broadcast_to(rows, broadcast_shape).flatten(order="F")
         view.select_rows(rows)
         return view
     

cvxpy/tests/test_expressions.py
@@ -768,13 +768,6 @@ class TestExpressions(BaseTest):
             q = self.A .__matmul__(self.B)
             self.assertTrue(q.is_quadratic())
 
-        # # Nonaffine times nonconstant raises error
-        # with warnings.catch_warnings():
-        #     warnings.simplefilter("ignore")
-        #     with self.assertRaises(Exception) as cm:
-        #         (self.A.__matmul__(self.B).__matmul__(self.A))
-        #     self.assertEqual(str(cm.exception), "Cannot multiply UNKNOWN and AFFINE.")
-
         # Constant expressions
         T = Constant([[1, 2, 3], [3, 5, 5]])
         exp = (T + T) .__matmul__(self.B)
@@ -1738,18 +1731,17 @@ class TestND_Expressions():
             assert y.shape == shapes[1]
 
     def test_no_segfault_multiply(self) -> None:
-        error_str = "Cannot multiply expressions with dimensions"
-        with pytest.raises(Exception, match=error_str):
-            x = cp.Variable(5)
-            a = np.array([1,2,3]).reshape(-1, 1)
-            b = np.array([1,2,3]).reshape(-1, 1)        
-            obj = cp.sum(cp.max(cp.multiply(a, x) + b, axis=0))
-            prob = cp.Problem(cp.Minimize(obj), [])
-            prob.solve(canon_backend=cp.SCIPY_CANON_BACKEND)
+        x = cp.Variable(5)
+        a = np.array([1,2,3]).reshape(-1, 1)
+        b = np.array([1,2,3]).reshape(-1, 1)        
+        obj = cp.sum(cp.max(cp.multiply(a, x) + b, axis=0))
+        prob = cp.Problem(cp.Minimize(obj), [])
+        prob.solve(canon_backend=cp.SCIPY_CANON_BACKEND)
 
-    @pytest.mark.parametrize("shapes", [((3),(253, 253, 3)),
+    @pytest.mark.parametrize("shapes", [((3),(252, 253, 3)),
                                         ((7, 1, 5),(8, 7, 6, 5)),
-                                        ((1),(5, 4)),
+                                        ((3), (2, 1)),
+                                        ((2, 1, 2), (2, 3, 2)),
                                         ((15, 1, 5), (15, 3, 5)),
                                         ((3, 5), (15, 3, 5)),
                                         ((3, 1), (15, 3, 5))])
@@ -1757,15 +1749,7 @@ class TestND_Expressions():
         x = cp.Variable(shapes[0])
         y = np.arange(np.prod(shapes[1])).reshape(shapes[1])
         expr = cp.multiply(x, y)
-        prob = cp.Problem(cp.Minimize(cp.sum(expr)), [x == 1])
+        target = np.arange(np.prod(shapes[0])).reshape(shapes[0])
+        prob = cp.Problem(cp.Minimize(cp.sum(expr)), [expr == target * y])
         prob.solve(canon_backend=cp.SCIPY_CANON_BACKEND)
-        assert np.allclose(expr.value, y)
-
-    def test_nd_multiply_broadcast_error(self) -> None:
-        error_str = "inconsistent shapes"
-        with pytest.raises(Exception, match=error_str):
-            x = cp.Variable(4)
-            y = np.arange(20).reshape(5, 4)
-            expr = cp.multiply(x, y)
-            prob = cp.Problem(cp.Minimize(cp.sum(expr)), [x == 1])
-            prob.solve(canon_backend=cp.SCIPY_CANON_BACKEND)
+        assert np.allclose(x.value, target)