42db31a0a6cfeb656149cd80111dae644f46c01f
Steven Diamond <steven@gridmatic.com>
2025-03-20 14:39:51 -0700

    try broadcasting constraint inputs

--- cvxpy/constraints/constraint.py
+++ cvxpy/constraints/constraint.py
@@ -41,7 +41,7 @@ class Constraint(u.Canonical):
     def __init__(self, args, constr_id=None) -> None:
         # TODO cast constants.
         # self.args = [cvxtypes.expression().cast_to_const(arg) for arg in args]
-        self.args = args
+        self.args = cvxtypes.expression().broadcast_list(args)
         if constr_id is None:
             self.constr_id = lu.get_id()
         else:
