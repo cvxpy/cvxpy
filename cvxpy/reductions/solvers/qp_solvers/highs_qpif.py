3f14812baeb47490aa3f03d96afe6663a46103d9
Steven Diamond <steven@gridmatic.com>
2025-06-25 17:20:21 -0700

    fix dotsort canonicalization

--- cvxpy/reductions/solvers/qp_solvers/highs_qpif.py
+++ cvxpy/reductions/solvers/qp_solvers/highs_qpif.py
@@ -38,6 +38,7 @@ class HIGHS(QpSolver):
 
     # Note that HiGHS does not support MIQP but supports MILP
     MIP_CAPABLE = False
+    BOUNDED_VARIABLES = True
 
     # Map of HiGHS status to CVXPY status.
     STATUS_MAP = {
@@ -171,8 +172,14 @@ class HIGHS(QpSolver):
         lp.a_matrix_.value_ = A.data
 
         # Define Variable bounds
-        lp.col_lower_ = -inf * np.ones(shape=lp.num_col_, dtype=q.dtype)
-        lp.col_upper_ = inf * np.ones(shape=lp.num_col_, dtype=q.dtype)
+        if data[s.LOWER_BOUNDS] is None:
+            lp.col_lower_ = -inf * np.ones(shape=lp.num_col_, dtype=q.dtype)
+        else:
+            lp.col_lower_ = data[s.LOWER_BOUNDS]
+        if data[s.UPPER_BOUNDS] is None:
+            lp.col_upper_ = inf * np.ones(shape=lp.num_col_, dtype=q.dtype)
+        else:
+            lp.col_upper_ = data[s.UPPER_BOUNDS]
 
         # note that we count actual nonzeros because
         # parameter values could make the problem linear
