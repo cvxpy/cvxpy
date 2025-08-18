ee9e26abb42b3c7ad26f484115a2dcbff366e9f0
William Zijie Zhang <89562186+Transurgeon@users.noreply.github.com>
2025-08-18 23:13:38 +0000

    dont do uv run pip

--- continuous_integration/install_optional_solvers.sh
+++ continuous_integration/install_optional_solvers.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 set -e
+uv venv
+
+if [[ "$RUNNER_OS" == "Windows" ]]; then
+  . .venv/Scripts/activate
+else
+  . .venv/bin/activate
+fi
 
 uv pip install ecos scs proxsuite daqp gurobipy piqp clarabel osqp highspy qoco qpalm
 
