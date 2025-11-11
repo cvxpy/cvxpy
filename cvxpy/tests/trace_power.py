d287971d13cc3163f1d57e349d77379ce37e96e3
Nika Zahedi <99093984+Ni2002ka@users.noreply.github.com>
2025-11-11 20:30:17 +0000

    power tests

--- cvxpy/tests/trace_power.py
+++ cvxpy/tests/trace_power.py
@@ -6,7 +6,7 @@ import cvxpy as cp
 x = cp.Variable(5)
 # TODO: support multiple powers
 # prob = cp.Problem(cp.Minimize(cp.power(x,4)), [x >= 1])
-constr = [cp.power(x, 4.2, _approx=False) <= np.ones(5)]
+constr = [cp.power(x, 4.2, _approx=True) <= np.ones(5)]
 prob = cp.Problem(cp.Minimize(x[0]+x[1]-x[2]), constr)
 prob.solve()
 print("status:", prob.status)
