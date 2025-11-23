b717231847d5e87775c5940afc91e2323e7c17f0
Nika Zahedi <99093984+Ni2002ka@users.noreply.github.com>
2025-11-23 12:19:18 -0800

    PowerCone Canonicalization of the Power Atom (#2999)
    * dev container + powconeND template
    
    * working on supporting powconeND
    
    * added more support for powconeND
    
    * trying to add support for ND cone
    
    * tested passing generalized power cones to clarabel
    
    * continuing working on nd, new example
    
    * preliminary version, pre-dual handling. Need to check axis
    
    * fixed axis handling, starting tests for general variable orderin
    
    * tested variable switching for powconeND
    
    * fix shape problems for power cone nd dual vars
    
    * fix shape problems for power cone nd dual vars
    
    * found bug in conversion of nd->3d cones
    
    * clean-up changes, removed stale TODOs, more testing
    
    * "cleaning up"
    
    * moved checking for exotic constraints to within each solver loop
    
    * slight logic improvement
    
    * removed stale TODOs and cleaned up code
    
    * fixed powconeND shape property when axis=1
    
    * implemented additional checks in solving_chain, made powconeND tests compatible with cvxpy test set
    
    * improved testing depth and readability
    
    * suggested changes by Steven and William
    
    * added explanation in power.py
    
    * fixed bug causing unnecessary constraints to be added to the problem
    
    * removed redundant axis checks in cone_matrix_stuffing
    
    * power v2 elementwise tests
    
    * power tests
    
    * added nonnegativity constraints
    
    * Update cvxpy/reductions/dcp2cone/canonicalizers/power_canon.py
    
    Co-authored-by: Parth Nobel <parthnobel@berkeley.edu>
    
    * refactoring tests for pytest compatability
    
    * removed stale todos
    
    * added approx to get_data method of power class
    
    * removed extra lines
    
    * removed stale notes
    
    ---------
    
    Co-authored-by: Parth Nobel <parthnobel@berkeley.edu>

---
+++ cvxpy/tests/test_power_atom.py
@@ -0,0 +1,118 @@
+"""
+Copyright 2025 CVXPY developers  
+
+Licensed under the Apache License, Version 2.0 (the "License");  
+you may not use this file except in compliance with the License.  
+You may obtain a copy of the License at  
+
+    http://www.apache.org/licenses/LICENSE-2.0  
+
+Unless required by applicable law or agreed to in writing, software  
+distributed under the License is distributed on an "AS IS" BASIS,  
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+See the License for the specific language governing permissions and  
+limitations under the License.
+"""
+
+import numpy as np
+
+import cvxpy as cp
+from cvxpy.tests.base_test import BaseTest
+
+
+class TestPowerAtom(BaseTest):
+    """Unit tests for power atom."""
+
+    def test_power_approx(self) -> None:
+        """Test power atom with approximation."""
+        x = cp.Variable(3)
+        constr = [cp.power(x, 3.3, _approx=True) <= np.ones(3)]
+        prob = cp.Problem(cp.Minimize(x[0] + x[1] - x[2]), constr)
+        prob.solve(solver=cp.SCS)
+        self.assertIn(prob.status, [cp.OPTIMAL, cp.OPTIMAL_INACCURATE])
+        self.assertAlmostEqual(prob.value, -1.0, places=3)
+        expected_x = np.array([0.0, 0.0, 1.0])
+        self.assertItemsAlmostEqual(x.value, expected_x, places=3)
+
+    def test_power_no_approx(self) -> None:
+        """Test power atom without approximation."""
+        x = cp.Variable(3)
+        constr = [cp.power(x, 3.3, _approx=False) <= np.ones(3)]
+        prob = cp.Problem(cp.Minimize(x[0] + x[1] - x[2]), constr)
+        prob.solve(solver=cp.SCS)
+        self.assertIn(prob.status, [cp.OPTIMAL, cp.OPTIMAL_INACCURATE])
+        self.assertAlmostEqual(prob.value, -1.0, places=3)
+        expected_x = np.array([0.0, 0.0, 1.0])
+        self.assertItemsAlmostEqual(x.value, expected_x, places=3)
+
+    def test_power_with_and_without_approx_low(self) -> None:
+        """Compare answers with and without approximation on the same problem."""
+        x = cp.Variable(3)
+        constr = [
+            cp.power(x, -1.5, _approx=True) <= np.ones(3),
+        ]
+        prob = cp.Problem(cp.Minimize(x[0] + x[1] - x[2] + (x[1] + x[2])**2), constr)
+        prob.solve(solver=cp.CLARABEL)
+        x_approx = x.value 
+
+        constr = [
+            cp.power(x, -1.5, _approx=False) <= np.ones(3),
+        ]
+        prob = cp.Problem(cp.Minimize(x[0] + x[1] - x[2] + (x[1] + x[2])**2), constr)
+        prob.solve(solver=cp.CLARABEL)
+        self.assertIn(prob.status, [cp.OPTIMAL, cp.OPTIMAL_INACCURATE])
+        self.assertItemsAlmostEqual(x.value, x_approx, places=3)
+
+    def test_power_with_and_without_approx_mid(self) -> None:
+        """Compare answers with and without approximation on the same problem."""
+        x = cp.Variable(3)
+        constr = [
+            cp.power(x, 0.8, _approx=True) >= np.ones(3),
+        ]
+        prob = cp.Problem(cp.Minimize(x[0] + x[1] - x[2] + (x[1] + x[2])**2), constr)
+        prob.solve(solver=cp.CLARABEL)
+        x_approx = x.value 
+
+        constr = [
+            cp.power(x, 0.8, _approx=False) >= np.ones(3),
+        ]
+        prob = cp.Problem(cp.Minimize(x[0] + x[1] - x[2] + (x[1] + x[2])**2), constr)
+        prob.solve(solver=cp.CLARABEL)
+        self.assertIn(prob.status, [cp.OPTIMAL, cp.OPTIMAL_INACCURATE])
+        self.assertItemsAlmostEqual(x.value, x_approx, places=3)
+
+    def test_power_with_and_without_approx_high(self) -> None:
+        """Compare answers with and without approximation on the same problem."""
+        x = cp.Variable(3)
+        constr = [
+            cp.power(x, 4.5, _approx=True) <= np.ones(3),
+        ]
+        prob = cp.Problem(cp.Minimize(x[0] + x[1] - x[2] + (x[1] + x[2])**2), constr)
+        prob.solve(solver=cp.CLARABEL)
+        x_approx = x.value 
+
+        constr = [
+            cp.power(x, 4.5, _approx=False) <= np.ones(3),
+        ]
+        prob = cp.Problem(cp.Minimize(x[0] + x[1] - x[2] + (x[1] + x[2])**2), constr)
+        prob.solve(solver=cp.CLARABEL)
+        self.assertIn(prob.status, [cp.OPTIMAL, cp.OPTIMAL_INACCURATE])
+        self.assertItemsAlmostEqual(x.value, x_approx, places=3)
+
+    def test_power_with_and_without_approx_even(self) -> None:
+        """Compare answers with and without approximation on the same problem."""
+        x = cp.Variable(3)
+        constr = [
+            cp.power(x, 8, _approx=True) <= np.ones(3),
+        ]
+        prob = cp.Problem(cp.Minimize(x[0] + x[1] - x[2] + (x[1] + x[2])**2), constr)
+        prob.solve(solver=cp.CLARABEL)
+        x_approx = x.value 
+
+        constr = [
+            cp.power(x, 8, _approx=False) <= np.ones(3),
+        ]
+        prob = cp.Problem(cp.Minimize(x[0] + x[1] - x[2] + (x[1] + x[2])**2), constr)
+        prob.solve(solver=cp.CLARABEL)
+        self.assertIn(prob.status, [cp.OPTIMAL, cp.OPTIMAL_INACCURATE])
+        self.assertItemsAlmostEqual(x.value, x_approx, places=3)
