7206dcc5d116db571774ff5736afc39f26435497
Parth Nobel <ptnobel@stanford.edu>
2025-12-04 15:50:45 -0800

    Adds the moreau solver (#3024)
    * Adds moreau
    
    * Adds interface file
    
    * Responds to reviews

--- cvxpy/reductions/solvers/solving_chain.py
+++ cvxpy/reductions/solvers/solving_chain.py
@@ -29,6 +29,7 @@ from cvxpy.reductions.cone2cone.exotic2common import (
     Exotic2Common,
 )
 from cvxpy.reductions.cone2cone.soc2psd import SOC2PSD
+from cvxpy.reductions.cone2cone.soc_dim3 import SOCDim3
 from cvxpy.reductions.cvx_attr2constr import CvxAttr2Constr
 from cvxpy.reductions.dcp2cone.cone_matrix_stuffing import ConeMatrixStuffing
 from cvxpy.reductions.dcp2cone.dcp2cone import Dcp2Cone
@@ -361,6 +362,10 @@ def construct_solving_chain(problem, candidates,
                 CvxAttr2Constr(reduce_bounds=not solver_instance.BOUNDED_VARIABLES),
             ]
             if all(c in supported_constraints for c in cones):
+                # Check if solver only supports dim-3 SOC cones
+                if solver_instance.SOC_DIM3_ONLY and SOC in cones:
+                    # Add SOCDim3 reduction to convert n-dim SOC to 3D SOC
+                    reductions.append(SOCDim3())
                 # Return the reduction chain.
                 reductions += [
                     ConeMatrixStuffing(quad_obj=quad_obj, canon_backend=canon_backend),
