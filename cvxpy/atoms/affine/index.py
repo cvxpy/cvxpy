Adds sparsity for powerflow and fix hessian indexing bug (#107)
* adds change for powerflow example

* adds hessian tests for idx

* reformat imports in problem

* remove remove_later.py

* remove pdf that was committed accidently

* fix hess index test

* adds back original formulation

cvxpy/atoms/affine/index.py
@@ -253,7 +253,7 @@ class special_index(AffAtom):
 
     def _hess_vec(self, vec):
         """ See the docstring of the hess_vec method of the atom class. """
-        idx = self.key
+        idx = np.reshape(self._select_mat, self._select_mat.size, order='F')
         e = np.zeros(self.args[0].size)
         e[idx] = vec
         return self.args[0].hess_vec(e)

cvxpy/tests/NLP_tests/hess_tests/test_hess_idx.py
@@ -1,10 +1,10 @@
 import numpy as np
+import pytest
 
 import cvxpy as cp
 
 
-class TestHessAdd():
-
+class TestHessIndex():
 
     def test_single_idx(self):
         n = 3 
@@ -34,7 +34,6 @@ class TestHessAdd():
         computed_hess[rows, cols] = vals
         assert(np.allclose(computed_hess, correct_matrix))
 
-    
     def test_slice_two_other_idx(self):
         n = 3 
         x = cp.Variable((n, ), name='x')
@@ -46,6 +45,42 @@ class TestHessAdd():
         correct_matrix = 2 * (-np.diag(np.array([1 / 2.25, 0, 0]))) + \
                              4 * (-np.diag(np.array([0, 0, 1/9])))
         computed_hess = np.zeros((n, n))
+        rows, cols, vals = result_dict[(x, x)]
+        computed_hess[rows, cols] = vals
+        assert(np.allclose(computed_hess, correct_matrix))
+
+    def test_special_index_matrix(self):
+        """
+        This test was failing because hess_vec was not properly handling
+        matrix indexing.
+        """
+        x = cp.Variable((2, 2), name='x')
+        x.value = np.array([[1.0, 2.0], [3.0, 4.0]])
+        vec = np.array([5, 6])
+        rows, cols = np.array([0, 0]), np.array([0, 1])
+        logx = cp.log(x)[rows, cols]
+        result_dict = logx.hess_vec(vec)
+        correct_matrix = 5 * (-np.diag(np.array([1.0, 0, 0, 0]))) + \
+                            6 * (-np.diag(np.array([0, 0, 1/4, 0])))
+        computed_hess = np.zeros((4, 4))
+        rows, cols, vals = result_dict[(x, x)]
+        computed_hess[rows, cols] = vals
+        assert(np.allclose(computed_hess, correct_matrix))
+
+    @pytest.mark.skip(reason="TODO fix this test for duplicate indices")
+    def test_special_index_duplicate_matrix(self):
+        """
+        TODO fix this test
+        """
+        x = cp.Variable((2, 2), name='x')
+        x.value = np.array([[1.0, 2.0], [3.0, 4.0]])
+        vec = np.array([5, 6])
+        rows, cols = np.array([0, 0]), np.array([0, 0])
+        logx = cp.log(x)[rows, cols]
+        result_dict = logx.hess_vec(vec)
+        correct_matrix = 5 * (-np.diag(np.array([1.0, 0, 0, 0]))) + \
+                            6 * (-np.diag(np.array([1.0, 0, 0, 0])))
+        computed_hess = np.zeros((4, 4))
         rows, cols, vals = result_dict[(x, x)]
         computed_hess[rows, cols] = vals
         assert(np.allclose(computed_hess, correct_matrix))