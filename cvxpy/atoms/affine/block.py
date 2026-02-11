7426bfc16d5da0d42cf83daedf410bd6185952d4
prasu-baran <prasubaran@gmail.com>
2026-02-11 22:43:33 +0530

    now solving like numpy

---
+++ cvxpy/atoms/affine/block.py
@@ -0,0 +1,37 @@
+"""
+NumPy-like block matrix constructor for CVXPY.
+"""
+
+from cvxpy.atoms.affine.hstack import hstack
+from cvxpy.atoms.affine.vstack import vstack
+from cvxpy.atoms.affine.affine_atom import AffAtom
+
+
+def _atleast_2d(expr):
+    expr = AffAtom.cast_to_const(expr)
+
+    # Promote scalars to (1,1)
+    if expr.ndim == 0:
+        return expr.reshape((1, 1), order="F")
+
+    # Promote 1D to row vector (1,n) like numpy.block
+    if expr.ndim == 1:
+        return expr.reshape((1, expr.shape[0]), order="F")
+
+    return expr
+
+
+def block(block_lists):
+    """
+    NumPy-like block matrix assembly.
+
+    Scalars become (1,1).
+    1D arrays become row vectors (1,n).
+    """
+
+    promoted = []
+    for row in block_lists:
+        promoted.append([_atleast_2d(elem) for elem in row])
+
+    row_blocks = [hstack(row) for row in promoted]
+    return vstack(row_blocks)
---
+++ cvxpy/tests/test_block.py
@@ -0,0 +1,20 @@
+import cvxpy as cp
+import numpy as np
+
+
+def test_block_scalar_promotion():
+    U = cp.Variable((2,1))
+    M = cp.block([[4, U.T],
+                  [U, np.eye(2)]])
+    assert M.shape == (3,3)
+
+
+def test_block_numpy_behavior():
+    v = np.array([1,2,3])
+    M = cp.block([[v]])
+    assert M.shape == (1,3)
+
+
+def test_block_pure_scalars():
+    M = cp.block([[1,2],[3,4]])
+    assert M.shape == (2,2)
